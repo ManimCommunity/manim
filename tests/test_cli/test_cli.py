11dcac888f9a374eed80b28a0ce22f50e122e23a
Devin Neal <devin@eulertour.com>
2020-07-11 00:26:25 -0700

    Use an emoji heart

--- tests/test_cli/test_cli.py
+++ tests/test_cli/test_cli.py
@@ -8,6 +8,7 @@ def capture(command):
     proc = subprocess.Popen(command,
                             stdout=subprocess.PIPE,
                             stderr=subprocess.PIPE,
+                            encoding="utf-8",
                             )
     out, err = proc.communicate()
     return out, err, proc.returncode
