f3651f477770075ef788fe9a281664a99a5e512b
Devin Neal <devin@eulertour.com>
2020-07-25 16:25:16 -0700

    Enforce formatting with CI

--- example_scenes/customtex.py
+++ example_scenes/customtex.py
@@ -10,10 +10,11 @@ from manim import *
 # 3. manim customtex.py ExampleClassScene -pl
 #       --> should succeed as the package esvect is included in template object
 
+
 class ExampleFileScene(Scene):
     def construct(self):
         text = TexMobject(r"\vv{vb}")
-        #text=TextMobject(r"$\vv{vb}$")
+        # text=TextMobject(r"$\vv{vb}$")
         self.play(Write(text))
 
 
@@ -21,8 +22,8 @@ class ExampleClassScene(Scene):
     def construct(self):
         tpl = TexTemplate()
         tpl.append_package(["esvect", ["f"]])
-        config['tex_template'] = tpl
+        config["tex_template"] = tpl
 
-        #text=TextMobject(r"$\vv{vb}$")
+        # text=TextMobject(r"$\vv{vb}$")
         text = TexMobject(r"\vv{vb}")
         self.play(Write(text))
