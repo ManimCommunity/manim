210d35ee55496eecd2e5174aaea9f3966d28eea6
Devin Neal <devin@eulertour.com>
2020-07-29 19:00:23 -0700

    Add --diff flag to black, format examples

--- example_scenes/customtex.py
+++ example_scenes/customtex.py
@@ -9,6 +9,7 @@ class TexTemplateFromCLI(Scene):
     This scene will fail to render if a tex_template.tex that doesn't
     import esvect is passed, and will throw a LaTeX error in that case.
     """
+
     def construct(self):
         text = TexMobject(r"\vv{vb}")
         # text=TextMobject(r"$\vv{vb}$")
@@ -19,6 +20,7 @@ class InCodeTexTemplate(Scene):
     """This example scene demonstrates how to modify the tex template
     for a particular scene from the code for the scene itself.
     """
+
     def construct(self):
         tpl = TexTemplate()
         tpl.append_package(["esvect", ["f"]])
