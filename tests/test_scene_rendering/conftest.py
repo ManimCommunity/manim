c77db55897e942eced0a2fa6b3350cfc9a4ae742
Hugues Devimeux <36239975+huguesdevimeux@users.noreply.github.com>
2020-08-23 14:42:10 +0200

    added small video tests (provisionnal)

---
+++ tests/test_scene_rendering/conftest.py
@@ -0,0 +1,16 @@
+import os
+import pytest
+import tempfile
+from tempfile import TemporaryDirectory
+
+
+from manim import file_writer_config
+
+@pytest.fixture
+def manim_cfg_file():
+    return os.path.join(os.path.dirname(__file__), "manim.cfg")
+
+@pytest.fixture
+def simple_scenes_path():
+    return os.path.join(os.path.dirname(__file__), "simple_scenes.py")
+    
---
+++ tests/test_scene_rendering/simple_scenes.py
@@ -0,0 +1,7 @@
+from manim import *
+
+class SquareToCircle(Scene):
+    def construct(self):
+        square = Square()
+        circle = Circle()
+        self.play(Transform(square, circle))
