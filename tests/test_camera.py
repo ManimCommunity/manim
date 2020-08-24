c742f8fcdf3f76e9dd0c43a10cd34cadc63dac8e
leotrs <leo@leotrs.com>
2020-08-24 09:47:32 -0400

    thanks again, black

--- tests/test_camera.py
+++ tests/test_camera.py
@@ -5,12 +5,12 @@ from manim import Camera, tempconfig, config
 def test_camera():
     """Test that Camera instances initialize to the correct config."""
     # by default, use the config
-    assert Camera().frame_width == config['frame_width']
+    assert Camera().frame_width == config["frame_width"]
     # init args override config
     assert Camera(frame_width=10).frame_width == 10
 
     # if config changes, reflect those changes
-    with tempconfig({'frame_width': 100}):
+    with tempconfig({"frame_width": 100}):
         assert Camera().frame_width == 100
         # ..init args still override new config
         assert Camera(frame_width=10).frame_width == 10
