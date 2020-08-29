d5e5f528b95e2cc0d284bb5f4845b0c8c233bae7
leotrs <leo@leotrs.com>
2020-08-29 16:51:32 -0400

    add test for shift

--- tests/test_family.py
+++ tests/test_family.py
@@ -1,4 +1,6 @@
+import numpy as np
 from manim import *
+
 
 def test_family():
     """Check that the family is gathered correclty."""
@@ -44,3 +46,28 @@ def test_overlapping_family():
     assert mob in family
     assert len(family) == 6
     assert family.count(gchild_common) == 1
+
+
+def test_shift_family():
+    """Check that each member of the family is shifted along with the parent.
+
+    Importantly, here we add a common grandchild to each of the children.  So
+    this test will fail if the grandchild moves twice as much as it should.
+
+    """
+    # Note Mobject.shift() needs the mobject to have a non-empty `points`
+    # attribute, so we cannot use Mobject.  We use Circle instead.
+    mob, child1, child2, = Circle(), Circle(), Circle(),
+    gchild1, gchild2, gchild_common = Circle(), Circle(), Circle()
+
+    child1.add(gchild1, gchild_common)
+    child2.add(gchild2, gchild_common)
+    mob.add(child1, child2)
+    family = mob.get_family()
+
+    positions_before = {m: m.get_center() for m in family}
+    mob.shift(RIGHT)
+    positions_after = {m: m.get_center() for m in family}
+
+    for m in family:
+        assert np.allclose(positions_before[m] + RIGHT, positions_after[m])
