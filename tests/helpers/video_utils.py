6ea98a80dc6f69768d4d8441c836c305b5831bc8
Hugues Devimeux <hugues.devimeux@gmail.com>
2020-08-31 21:27:14 +0200

    updated helpers

--- tests/helpers/video_utils.py
+++ tests/helpers/video_utils.py
@@ -5,35 +5,18 @@ import os
 import subprocess
 import json
 
-from manim import logger
+from manim.logger import logger
 
 
 def capture(command):
     proc = subprocess.Popen(
-        command, stdout=subprocess.PIPE, stderr=subprocess.PIPE, encoding="utf8",
+        command,
+        stdout=subprocess.PIPE,
+        stderr=subprocess.PIPE,
+        encoding="utf8",
     )
     out, err = proc.communicate()
     return out, err, proc.returncode
-
-
-def get_hash_from_video(path_to_video):
-    command = [
-        "ffmpeg",
-        "-i",
-        path_to_video,
-        "-f",
-        "hash",
-        "-hash",
-        "md5",
-        "-",
-        "-format_whitelist",
-        "hash",
-        "-loglevel",
-        "error",
-    ]
-    hash_video, err, exitcode = capture(command)
-    assert exitcode == 0, err
-    return hash_video.replace("\n", "")
 
 
 def get_config_from_video(path_to_video):
@@ -55,32 +38,24 @@ def get_config_from_video(path_to_video):
 
 
 def save_control_data_from_video(path_to_video, name):
-    """Helper used to set up a new test that will compare videos. This will create a new .json file in control_data/videos_data that contains 
-    informations tested of the video, including its hash. Refer to the wiki for more informations. 
+    """Helper used to set up a new test that will compare videos. This will create a new .json file in control_data/videos_data that contains
+    informations tested of the video, including its hash. Refer to the wiki for more informations.
 
     Parameters :
     ------------
     path_to_video : :class:`str`
         Path to the video to extract  informations from.
     name : :class:`str`
-        Name of the test. The .json file will be named with it. 
-    
+        Name of the test. The .json file will be named with it.
+
     """
     tests_directory = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
     path_control_data = os.path.join(tests_directory, "control_data", "videos_data")
-    hash_video = get_hash_from_video(path_to_video)
     config_video = get_config_from_video(path_to_video)
     data = {
         "name": name,
-        "hash": hash_video,
         "config": json.loads(config_video)["streams"][0],
     }
     path_saved = os.path.join(path_control_data, f"{name}.json")
     json.dump(data, open(path_saved, "w"), indent=4)
     logger.info(f"Data for {name} saved in {path_saved}")
-
-
-save_control_data_from_video(
-    r"C:\Users\User\OneDrive\Bureau\Programmation\PYTHON\MANIM-DEV\media\videos\simple_scenes\480p15\SquareToCircle.mp4",
-    "SquareToCircleWithPFlag",
-)
