71c27ea48cb5976f0aaaa3ff32f08368e41761fb
Benjamin Hackl <devel@benjamin-hackl.at>
2020-09-09 18:06:24 +0200

    autoformat

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -12,7 +12,8 @@
 
 import os
 import sys
-sys.path.insert(0, os.path.abspath('.'))
+
+sys.path.insert(0, os.path.abspath("."))
 
 
 # -- Project information -----------------------------------------------------
@@ -69,7 +70,7 @@ extensions.append("guzzle_sphinx_theme")
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['_static']
+html_static_path = ["_static"]
 
 # This specifies any additional css files that will override the theme's
-html_css_files = ['custom.css']
+html_css_files = ["custom.css"]
