804a565bb2a812791f24ce11271441b8fb6cdd70
Benjamin Hackl <devel@benjamin-hackl.at>
2020-10-08 08:33:34 +0200

    Update manim/mobject/svg/text_mobject.py
    Co-authored-by: Leo Torres <dleonardotn@gmail.com>

--- manim/mobject/svg/text_mobject.py
+++ manim/mobject/svg/text_mobject.py
@@ -301,7 +301,7 @@ class Text(SVGMobject):
         line_spacing = self.line_spacing * 10
 
         if self.font == "":
-            if NOT_SETTING_FONT_MSG != "":
+            if NOT_SETTING_FONT_MSG:
                 logger.warning(NOT_SETTING_FONT_MSG)
 
         dir_name = file_writer_config["text_dir"]
