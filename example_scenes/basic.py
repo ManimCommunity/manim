5a7846d88ac0c1ab1f7140329d17b12d2d8910ae
Devin Neal <devin@eulertour.com>
2020-10-17 15:55:01 -0700

    Update example_scenes/basic.py
    Co-authored-by: kolibril13 <44469195+kolibril13@users.noreply.github.com>

--- example_scenes/basic.py
+++ example_scenes/basic.py
@@ -14,6 +14,16 @@ from manim import *
 # for a 1920x1080 video)
 
 from manim import *
+# To watch one of these scenes, run the following:
+# python --quality m manim example_scenes.py SquareToCircle -p
+#
+# Use the flag --quality l for a faster rendering at a lower quality.
+# Use -s to skip to the end and just save the final frame
+# Use the -p to have a preview of the animation (or image, if -s was
+# used) pop up once done.
+# Use -n <number> to skip ahead to the n'th animation of a scene.
+# Use -r <number> to specify a resolution (for example, -r 1080
+# for a 1920x1080 video)```
 
 # To watch one of these scenes, run the following:
 # python --quality m manim example_scenes.py SquareToCircle -p
