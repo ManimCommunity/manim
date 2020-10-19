8d946e5c5f7a429fd1994b6799c5dd5aede0c1b6
kolibril13 <jan-hendrik.mueller@gmx.net>
2020-10-19 15:13:52 +0200

    some more info to the template

--- tests/template_generate_graphical_units_data.py
+++ tests/template_generate_graphical_units_data.py
@@ -1,6 +1,10 @@
 from manim import *
 from tests.helpers.graphical_units import set_test_scene
 
+# Note: DO NOT COMMIT THIS FILE. The purpose of this template is to produce control data for graphical_units_data. As
+# soon as the test data is produced, please revert all changes you made to this file, so this template file will be
+# still available for others :)
+
 
 class YourClassHere(Scene):
     def construct(self):
@@ -8,4 +12,6 @@ class YourClassHere(Scene):
         self.play(Animation(circle))
 
 
-set_test_scene(YourClassHere, "<whereitbelongs>")
+set_test_scene(
+    YourClassHere, "<module_name>"
+)  # <module_name> can be e.g.  "geometry" or "movements"
