c57bd04629eda302146052b27158b5dba9aa3022
kolibril13 <jan-hendrik.mueller@gmx.net>
2020-10-19 15:25:33 +0200

    some more info

--- tests/template_generate_graphical_units_data.py
+++ tests/template_generate_graphical_units_data.py
@@ -4,7 +4,7 @@ from tests.helpers.graphical_units import set_test_scene
 # Note: DO NOT COMMIT THIS FILE. The purpose of this template is to produce control data for graphical_units_data. As
 # soon as the test data is produced, please revert all changes you made to this file, so this template file will be
 # still available for others :)
-
+# More about graphical unit tests: https://github.com/ManimCommunity/manim/wiki/Testing#graphical-unit-test
 
 class YourClassHere(Scene):
     def construct(self):
@@ -15,3 +15,5 @@ class YourClassHere(Scene):
 set_test_scene(
     YourClassHere, "<module_name>"
 )  # <module_name> can be e.g.  "geometry" or "movements"
+
+
