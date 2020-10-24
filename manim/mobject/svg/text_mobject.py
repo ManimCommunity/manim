e5b5185551f416df095bc0321e7513918a7f8d0e
Benjamin Hackl <devel@benjamin-hackl.at>
2020-10-24 19:11:30 +0200

    move examples from old to new Text class

--- manim/mobject/svg/text_mobject.py
+++ manim/mobject/svg/text_mobject.py
@@ -551,6 +551,49 @@ class Text(SVGMobject):
 
     Examples
     ---------
+
+    .. manim:: Example1Text
+        :save_last_frame:
+
+        class Example1Text(Scene):
+            def construct(self):
+                text = Text('Hello world').scale(3)
+                self.add(text)
+
+    .. manim:: TextColorExample
+        :save_last_frame:
+
+        class TextColorExample(Scene):
+            def construct(self):
+                text1 = Text('Hello world', color=BLUE).scale(3)
+                text2 = Text('Hello world', gradient=(BLUE, GREEN)).scale(3).next_to(text1, DOWN)
+                self.add(text1, text2)
+
+    .. manim:: TextItalicAndBoldExample
+        :save_last_frame:
+
+        class TextItalicAndBoldExample(Scene):
+            def construct(self):
+                text0 = Text('Hello world', slant=ITALIC)
+                text1 = Text('Hello world', t2s={'world':ITALIC})
+                text2 = Text('Hello world', weight=BOLD)
+                text3 = Text('Hello world', t2w={'world':BOLD})
+                self.add(text0,text1, text2,text3)
+                for i,mobj in enumerate(self.mobjects):
+                    mobj.shift(DOWN*(i-1))
+
+
+    .. manim:: TextMoreCustomization
+            :save_last_frame:
+
+            class TextMoreCustomization(Scene):
+                def construct(self):
+                    text1 = Text(
+                        'Google',
+                        t2c={'[:1]': '#3174f0', '[1:2]': '#e53125',
+                             '[2:3]': '#fbb003', '[3:4]': '#3174f0',
+                             '[4:5]': '#269a43', '[5:]': '#e53125'}, size=1.2).scale(3)
+                    self.add(text1)
     .. manim:: MultipleFonts
         :save_last_frame:
 
@@ -877,69 +920,3 @@ class Text(SVGMobject):
         surface.finish()
         return file_name
 
-
-class Text(CairoText):
-    """Display (non-LaTeX) text.
-
-    Text objects behave like a :class:`.VGroup`-like iterable of all characters
-    in the given text. In particular, slicing is possible.
-
-    Examples
-    --------
-    .. manim:: Example1Text
-        :save_last_frame:
-
-        class Example1Text(Scene):
-            def construct(self):
-                text = Text('Hello world').scale(3)
-                self.add(text)
-
-    .. manim:: TextColorExample
-        :save_last_frame:
-
-        class TextColorExample(Scene):
-            def construct(self):
-                text1 = Text('Hello world', color=BLUE).scale(3)
-                text2 = Text('Hello world', gradient=(BLUE, GREEN)).scale(3).next_to(text1, DOWN)
-                self.add(text1, text2)
-
-    .. manim:: TextItalicAndBoldExample
-        :save_last_frame:
-
-        class TextItalicAndBoldExample(Scene):
-            def construct(self):
-                text0 = Text('Hello world', slant=ITALIC)
-                text1 = Text('Hello world', t2s={'world':ITALIC})
-                text2 = Text('Hello world', weight=BOLD)
-                text3 = Text('Hello world', t2w={'world':BOLD})
-                self.add(text0,text1, text2,text3)
-                for i,mobj in enumerate(self.mobjects):
-                    mobj.shift(DOWN*(i-1))
-
-
-    .. manim:: TextMoreCustomization
-            :save_last_frame:
-
-            class TextMoreCustomization(Scene):
-                def construct(self):
-                    text1 = Text(
-                        'Google',
-                        t2c={'[:1]': '#3174f0', '[1:2]': '#e53125',
-                             '[2:3]': '#fbb003', '[3:4]': '#3174f0',
-                             '[4:5]': '#269a43', '[5:]': '#e53125'}, size=1.2).scale(3)
-                    self.add(text1)
-
-    .. WARNING::
-
-        Using a :class:`.Transform` on text with leading whitespace can look
-        `weird <https://github.com/3b1b/manim/issues/1067>`_. Consider using
-        :meth:`remove_invisible_chars` to resolve this issue.
-
-    """
-
-    def __init__(self, text, **config):
-        logger.warning(
-            "Using Text uses Cairo Toy API to Render Text."
-            "Using PangoText is recommended and soon Text would point to PangoText"
-        )
-        CairoText.__init__(self, text, **config)
