fd41a55834660bb9602c63129ee0343f8493bcda
Devin Neal <devin@eulertour.com>
2020-10-25 16:59:24 -0700

    Add test

--- tests/test_graphical_units/test_transform.py
+++ tests/test_graphical_units/test_transform.py
@@ -113,10 +113,14 @@ class ApplyMatrixTest(Scene):
 
 class BecomeMethodWithTextTest(Scene):
     def construct(self):
+        square = Square(color=BLUE).shift(2 * UP)
         annotation = Text("Fade In", height=0.8)
+        self.add(annotation)
+        self.play(FadeIn(square))
+
         annotation.become(Text("Fade Out", height=0.8))
         self.add(annotation)
-        self.wait()
+        self.play(FadeOut(square))
 
 
 class CyclicReplaceTest(Scene):
