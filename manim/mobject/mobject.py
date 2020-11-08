7cac498613b919abfb28370e17fcb0023bf67f1d
Benjamin Hackl <devel@benjamin-hackl.at>
2020-11-08 17:01:32 +0100

    examples in animations.rst

--- manim/mobject/mobject.py
+++ manim/mobject/mobject.py
@@ -245,6 +245,31 @@ class Mobject(Container):
         return list(it.chain(*[sm.get_updaters() for sm in self.get_family()]))
 
     def add_updater(self, update_function, index=None, call_updater=True):
+        """Add an update function to this mobject.
+
+        Examples
+        --------
+
+        .. manim:: RotationUpdater
+
+            class RotationUpdater(Scene):
+                def construct(self):
+                    def updater_forth(mobj, dt):
+                        mobj.rotate_about_origin(dt)
+                    def updater_back(mobj, dt):
+                        mobj.rotate_about_origin(-dt)
+                    line_reference = Line(ORIGIN, LEFT).set_color(WHITE)
+                    line_moving = Line(ORIGIN, LEFT).set_color(YELLOW)
+                    line_moving.add_updater(updater_forth)
+                    self.add(line_reference, line_moving)
+                    self.wait(2)
+                    line_moving.remove_updater(updater_forth)
+                    line_moving.add_updater(updater_back)
+                    self.wait(2)
+                    line_moving.remove_updater(updater_back)
+                    self.wait(0.5)
+
+        """
         if index is None:
             self.updaters.append(update_function)
         else:
