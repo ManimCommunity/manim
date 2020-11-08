64e8625b4e411c786525997f4e28fd14963fb0f2
Benjamin Hackl <devel@benjamin-hackl.at>
2020-11-08 14:39:49 +0100

    examples in formulas.rst

--- docs/source/examples/formulas.rst
+++ docs/source/examples/formulas.rst
@@ -1,8 +1,5 @@
 Formulas
-=================================
-
-
-
+========
 
 .. manim:: MovingFrameBox
 
@@ -22,30 +19,4 @@ Formulas
             self.play(
                 ReplacementTransform(framebox1,framebox2),
             )
-            self.wait()
-
-.. manim:: MovingBraces
-
-    class MovingBraces(Scene):
-        def construct(self):
-            text=MathTex(
-                "\\frac{d}{dx}f(x)g(x)=",       #0
-                "f(x)\\frac{d}{dx}g(x)",        #1
-                "+",                            #2
-                "g(x)\\frac{d}{dx}f(x)"         #3
-            )
-            self.play(Write(text))
-            brace1 = Brace(text[1], UP, buff=SMALL_BUFF)
-            brace2 = Brace(text[3], UP, buff=SMALL_BUFF)
-            t1 = brace1.get_text("$g'f$")
-            t2 = brace2.get_text("$f'g$")
-            self.play(
-                GrowFromCenter(brace1),
-                FadeIn(t1),
-                )
-            self.wait()
-            self.play(
-                ReplacementTransform(brace1,brace2),
-                ReplacementTransform(t1,t2)
-                )
             self.wait()
--- manim/mobject/svg/tex_mobject.py
+++ manim/mobject/svg/tex_mobject.py
@@ -30,16 +30,34 @@ Anything enclosed in ``$`` signs is interpreted as maths-mode:
             tex = Tex(r'$\xrightarrow{Hello}$ \LaTeX').scale(3)
             self.add(tex)
 
-Whereas in a :class:`~.MathTex` mobject everything is math-mode by default and you would use ``\text{}`` to
-insert regular text:
+Whereas in a :class:`~.MathTex` mobject everything is math-mode by default.
 
-.. manim:: HelloMathTex
-    :save_last_frame:
+.. manim:: MovingBraces
 
-    class HelloMathTex(Scene):
+    class MovingBraces(Scene):
         def construct(self):
-            tex = MathTex(r'\xrightarrow{Hello}\text{ \LaTeX}').scale(3)
-            self.add(tex)
+            text=MathTex(
+                "\\frac{d}{dx}f(x)g(x)=",       #0
+                "f(x)\\frac{d}{dx}g(x)",        #1
+                "+",                            #2
+                "g(x)\\frac{d}{dx}f(x)"         #3
+            )
+            self.play(Write(text))
+            brace1 = Brace(text[1], UP, buff=SMALL_BUFF)
+            brace2 = Brace(text[3], UP, buff=SMALL_BUFF)
+            t1 = brace1.get_text("$g'f$")
+            t2 = brace2.get_text("$f'g$")
+            self.play(
+                GrowFromCenter(brace1),
+                FadeIn(t1),
+                )
+            self.wait()
+            self.play(
+                ReplacementTransform(brace1,brace2),
+                ReplacementTransform(t1,t2)
+                )
+            self.wait()
+
 
 LaTeX commands and keyword arguments
 ++++++++++++++++++++++++++++++++++++
