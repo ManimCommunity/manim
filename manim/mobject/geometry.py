c618c12005cf1612015b861cad171fc0c85efe1e
XorUnison <2881103+XorUnison@users.noreply.github.com>
2020-11-14 13:38:52 +0100

    docs fix

--- manim/mobject/geometry.py
+++ manim/mobject/geometry.py
@@ -909,18 +909,18 @@ class ArcPolygon(VMobject):
 
             class ArcPolygonExample(Scene):
                 def construct(self):
-                arc_conf = {"stroke_width":0}
-                poly_conf = {"stroke_width":10,"stroke_color":BLUE,
+                    arc_conf = {"stroke_width":0}
+                    poly_conf = {"stroke_width":10,"stroke_color":BLUE,
                       "fill_opacity":1,"color": PURPLE}
-                a=[-1,0,0]
-                b=[1,0,0]
-                c=[0,np.sqrt(3),0]
-                arc0=ArcBetweenPoints(a,b,radius=2,**arc_conf)
-                arc1=ArcBetweenPoints(b,c,radius=2,**arc_conf)
-                arc2=ArcBetweenPoints(c,a,radius=2,**arc_conf)
-                reuleaux_triangle=ArcPolygon(arc0,arc1,arc2,**poly_conf)
-                self.play(FadeIn(reuleaux_triangle))
-                self.wait(2)
+                    a=[-1,0,0]
+                    b=[1,0,0]
+                    c=[0,np.sqrt(3),0]
+                    arc0=ArcBetweenPoints(a,b,radius=2,**arc_conf)
+                    arc1=ArcBetweenPoints(b,c,radius=2,**arc_conf)
+                    arc2=ArcBetweenPoints(c,a,radius=2,**arc_conf)
+                    reuleaux_tri=ArcPolygon(arc0,arc1,arc2,**poly_conf)
+                    self.play(FadeIn(reuleaux_tri))
+                    self.wait(2)
 
     >>> print(reuleaux_triangle.arcs)
     '[ArcBetweenPoints, ArcBetweenPoints, ArcBetweenPoints]'
@@ -931,18 +931,18 @@ class ArcPolygon(VMobject):
 
             class ArcPolygonExample2(Scene):
                 def construct(self):
-                arc_conf = {"stroke_width":3,"stroke_color":RED,
+                    arc_conf = {"stroke_width":3,"stroke_color":RED,
                       "fill_opacity":0.5,"color": GREEN}
-                poly_conf = {"color":None}
-                a=[-1,0,0]
-                b=[1,0,0]
-                c=[0,np.sqrt(3),0]
-                arc0=ArcBetweenPoints(a,b,radius=2,**arc_conf)
-                arc1=ArcBetweenPoints(b,c,radius=2,**arc_conf)
-                arc2=ArcBetweenPoints(c,a,radius=2,**arc_conf)
-                reuleaux_triangle=ArcPolygon(arc0,arc1,arc2,**poly_conf)
-                self.play(FadeIn(reuleaux_triangle))
-                self.wait(2)
+                    poly_conf = {"color":None}
+                    a=[-1,0,0]
+                    b=[1,0,0]
+                    c=[0,np.sqrt(3),0]
+                    arc0=ArcBetweenPoints(a,b,radius=2,**arc_conf)
+                    arc1=ArcBetweenPoints(b,c,radius=2,**arc_conf)
+                    arc2=ArcBetweenPoints(c,a,radius=2,**arc_conf)
+                    reuleaux_tri=ArcPolygon(arc0,arc1,arc2,**poly_conf)
+                    self.play(FadeIn(reuleaux_tri))
+                    self.wait(2)
     """
 
     def __init__(self, *arcs, **kwargs):
