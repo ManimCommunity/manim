7ef9e644b8f2a259605113aea5f17d90b98bbdbe
Benjamin Hackl <devel@benjamin-hackl.at>
2020-11-16 14:25:32 +0100

    test background color with colors defined in test

--- tests/test_color.py
+++ tests/test_color.py
@@ -14,15 +14,15 @@ def test_background_color():
     import manim.utils.color as C
 
     S = Scene()
-    S.camera.background_color = C.RED
+    S.camera.background_color = "#ff0000"
     S.renderer.update_frame(S)
-    assert np.all(S.renderer.get_frame()[0, 0] == np.array([252, 98, 85, 255]))
+    assert np.all(S.renderer.get_frame()[0, 0] == np.array([255, 0, 0, 255]))
 
-    S.camera.background_color = C.BLUE
+    S.camera.background_color = "#436f80"
     S.renderer.update_frame(S)
-    assert np.all(S.renderer.get_frame()[0, 0] == np.array([88, 196, 221, 255]))
+    assert np.all(S.renderer.get_frame()[0, 0] == np.array([67, 111, 128, 255]))
 
-    S.camera.background_color = C.GREEN
+    S.camera.background_color = "#bbffbb"
     S.camera.background_opacity = 0.5
     S.renderer.update_frame(S)
-    assert np.all(S.renderer.get_frame()[0, 0] == np.array([131, 193, 103, 127]))
+    assert np.all(S.renderer.get_frame()[0, 0] == np.array([187, 255, 187, 127]))
