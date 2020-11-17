5a002f0534e06e8112221627e816ffea620bc8da
XorUnison <2881103+XorUnison@users.noreply.github.com>
2020-11-17 01:14:51 +0100

    Visual adjustment to second example

--- manim/mobject/geometry.py
+++ manim/mobject/geometry.py
@@ -935,7 +935,7 @@ class ArcPolygon(VMobject):
 
         class ArcPolygonExample2(Scene):
             def construct(self):
-                arc_conf = {"stroke_width":3,"stroke_color":RED,
+                arc_conf = {"stroke_width":3,"stroke_color":BLUE,
                     "fill_opacity":0.5,"color": GREEN}
                 poly_conf = {"color":None}
                 a=[-1,0,0]
@@ -943,7 +943,7 @@ class ArcPolygon(VMobject):
                 c=[0,np.sqrt(3),0]
                 arc0=ArcBetweenPoints(a,b,radius=2,**arc_conf)
                 arc1=ArcBetweenPoints(b,c,radius=2,**arc_conf)
-                arc2=ArcBetweenPoints(c,a,radius=2,**arc_conf)
+                arc2=ArcBetweenPoints(c,a,radius=2,{"stroke_color":RED})
                 reuleaux_tri=ArcPolygon(arc0,arc1,arc2,**poly_conf)
                 self.play(FadeIn(reuleaux_tri))
                 self.wait(2)
