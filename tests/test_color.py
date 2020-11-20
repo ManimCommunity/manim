207afdb4730dba64c5730f01134ed69b32a24fcb
leotrs <leo@leotrs.com>
2020-11-19 23:22:39 -0500

    update tests

--- tests/test_color.py
+++ tests/test_color.py
@@ -13,14 +13,14 @@ def test_import_color():
 def test_background_color():
     S = Scene()
     S.camera.background_color = "#ff0000"
-    S.renderer.update_frame(S)
+    S.renderer.update_frame(S, 0)
     assert np.all(S.renderer.get_frame()[0, 0] == np.array([255, 0, 0, 255]))
 
     S.camera.background_color = "#436f80"
-    S.renderer.update_frame(S)
+    S.renderer.update_frame(S, 0)
     assert np.all(S.renderer.get_frame()[0, 0] == np.array([67, 111, 128, 255]))
 
     S.camera.background_color = "#bbffbb"
     S.camera.background_opacity = 0.5
-    S.renderer.update_frame(S)
+    S.renderer.update_frame(S, 0)
     assert np.all(S.renderer.get_frame()[0, 0] == np.array([187, 255, 187, 127]))
