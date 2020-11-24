b9d8f465bc8c465e078e5a111ec1f8f56583ba0b
leotrs <leo@leotrs.com>
2020-11-23 19:02:54 -0500

    change test to use colors, not Text

--- tests/test_graphical_units/test_last_frame.py
+++ tests/test_graphical_units/test_last_frame.py
@@ -1,27 +1,27 @@
 import pytest
 
 import manim as mn
+from manim import color as C
 from ..utils.testing_utils import get_scenes_to_test
 from ..utils.GraphicalUnitTester import GraphicalUnitTester
 
 
-def make_lbl(num):
-    text = mn.Text(f"{num}").scale(7)
-    text.num = num
-    return text
-
-
-def update_lbl(lbl, dt):
-    lbl.num += 1
-    lbl.become(make_lbl(lbl.num))
-
-
 class LastFrame(mn.Scene):
     def construct(self):
-        text = make_lbl(0).add_updater(update_lbl)
-        self.add(text)
-        sq = mn.Square(stroke_width=30).scale(3)
-        self.play(mn.ShowCreation(sq), run_time=1)
+        tick_start = 1.0
+        tick_end = 3.0
+        val_tracker = mn.ValueTracker(tick_start)
+        square = mn.Square(fill_opacity=1).set_stroke(width=0)
+        self.add(square)
+        num_colors = 1000
+        cols = mn.color_gradient([C.RED, C.WHITE, C.BLUE], num_colors)
+
+        def col_uptater(mob):
+            integ = int((val_tracker.get_value() - tick_start) / (tick_end - tick_start) * (num_colors - 1))
+            mob.set_color(cols[integ])
+
+        square.add_updater(col_uptater)
+        self.play(val_tracker.set_value, tick_end, rate_func=mn.linear, run_time=3)
 
 
 MODULE_NAME = "last_frame"
