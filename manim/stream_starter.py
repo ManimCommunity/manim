9dc7e16c5abc651a5ab610183e26f71a69263616
NeoPlato <67624028+NeoPlato@users.noreply.github.com>
2020-11-29 18:33:59 +0300

    Revert "Revert "Merge branch 'master' of https://github.com/NeoPlato/manim""
    This reverts commit 6c9a140b90172b2764f882fad102b26996ce87ac.

--- manim/stream_starter.py
+++ manim/stream_starter.py
@@ -51,8 +51,9 @@ def livestream():
 def stream():
     """For a quick import and livestream eg:
 
-    >>> from manim import `stream, stuff and other stuff`
+    >>> from manim import stream, Circle, ShowCreation
     >>> manim = stream()
-    >>> manim.play(...)
+    >>> circ = Circle()
+    >>> manim.play(ShowCreation(circ))
     """
     return BasicStreamer()
