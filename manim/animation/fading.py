e31b6519d5fd48b6495254201a9a06001147cc0c
Harry-Lees <harry.lees@gmail.com>
2020-12-01 01:51:31 +0000

    fixed Transform import

--- manim/animation/fading.py
+++ manim/animation/fading.py
@@ -17,7 +17,8 @@ __all__ = [
 
 
 from .. import logger
-from ..animation.animation import DEFAULT_ANIMATION_LAG_RATIO, Animation, Transform
+from ..animation.animation import DEFAULT_ANIMATION_LAG_RATIO, Animation
+from ..animation.transform import Transform
 from ..constants import DOWN
 from ..mobject.types.vectorized_mobject import VMobject
 from ..utils.bezier import interpolate
