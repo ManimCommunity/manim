4e33544a9ae356ad4f0dd764e77368e22bfd4282
NeoPlato <allanmuruha@gmail.com>
2020-12-10 14:59:55 +0300

    Popup ffplay window

--- manim/stream_starter.py
+++ manim/stream_starter.py
@@ -1,4 +1,5 @@
 import code
+import functools
 import os
 import readline
 import rlcompleter
@@ -9,7 +10,11 @@ from colorama import Fore, Style
 from . import config, logger
 from .scene.streaming_scene import get_streamer, play_scene
 
-__all__ = ["livestream", "stream"]
+
+__all__ = ["livestream", "stream", "open_client"]
+
+
+globals().update(config.streaming_config)
 
 
 info = """
@@ -26,6 +31,12 @@ another scene class, e.g. MovingCameraScene, create it with the syntax:
 >>> manim2 = get_streamer(MovingCameraScene)
 >>> 
 
+Want to render the animation of an entire pre-baked scene? Here's an example:
+
+>>> from example_scenes import basic
+>>> play_scene(basic.WarpSquare)
+>>> play_scene(basic.OpeningManimExample, start=0, end=5)
+
 To view an image of the current state of the scene or mobject, use: 
 
 >>> manim.show_frame()        #For Scene
@@ -34,9 +45,9 @@ To view an image of the current state of the scene or mobject, use:
 """
 
 
-def open_ffplay(streaming_client=None, sdp_path=None, **kwargs):
+def open_client(client=None):
     command = [
-        streaming_client,
+        client or streaming_client,
         "-x",
         "1280",
         "-y",
@@ -53,19 +64,36 @@ def open_ffplay(streaming_client=None, sdp_path=None, **kwargs):
     subprocess.Popen(command)
 
 
-def _hide_wait_output(sdp_path=None, **kwargs):
+def _disable_logging(func):
+    """Decorator for running trigger Wait() animations without showing the
+    usual output expected from this action
+    """
+    functools.wraps(func)
+    def action(*args, **kwargs):
+        logger.disabled = True
+        func(*args, **kwargs)
+        logger.disabled = False
+
+    return action
+
+
+@_disable_logging
+def _guarantee_sdp_file(*args):
     """Ensures, if required, that the sdp file exists,
     while supressing the loud info message given out by this process
     """
-    # TODO: Bound for improvement later
-    # Also, OCD motivated
-    logger.debug("Ensuring sdp file exists: Running Wait() animation")
-    logger.disabled = True
     if not os.path.exists(sdp_path):
         kicker = get_streamer()
         kicker.wait()
         del kicker
-    logger.disabled = False
+
+
+@_disable_logging
+def _popup_window(shell):
+    """Triggers the opening of the window. May lack utility for a streaming
+    client like vlc
+    """
+    shell.push("get_streamer().wait(0.5)")
 
 
 def livestream():
@@ -76,25 +104,35 @@ def livestream():
         "manim": get_streamer(),
         "get_streamer": get_streamer,
         "play_scene": play_scene,
+        "open_client": open_client
     }
     readline.set_completer(rlcompleter.Completer(variables).complete)
     readline.parse_and_bind("tab: complete")
     shell = code.InteractiveConsole(variables)
     shell.push("from manim import *")
-    _hide_wait_output(**config.streaming_config)
-    open_ffplay(**config.streaming_config)
+
+    logger.debug("Ensuring sdp file exists: Running Wait() animation")
+    _guarantee_sdp_file()
+
+    open_client()
+
+    logger.debug(
+        "Triggering streaming client window: Running Wait() animation")
+    _popup_window(shell)
+
     shell.interact(banner=f"{Fore.GREEN}{info}{Style.RESET_ALL}")
 
 
 def stream():
     """For a quick import and livestream eg:
 
-    >>> from manim import stream, Circle, ShowCreation
+    >>> from manim import stream, open_client, Circle, ShowCreation
     >>> manim = stream()
+    >>> open_client()
     >>> circ = Circle()
     >>> manim.play(ShowCreation(circ))
     """
-    _hide_wait_output(**config["streaming_config"])
+    _guarantee_sdp_file()
     streamer = get_streamer()
-    open_ffplay(**config["streaming_config"])
+    open_client()
     return streamer
