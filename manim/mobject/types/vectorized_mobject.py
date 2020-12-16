8c29c6a235f0015b054df0b1cb02e72fcb666ef4
kolibril13 <44469195+kolibril13@users.noreply.github.com>
2020-12-16 20:56:50 +0100

    Update manim/mobject/types/vectorized_mobject.py

--- manim/mobject/types/vectorized_mobject.py
+++ manim/mobject/types/vectorized_mobject.py
@@ -1397,10 +1397,10 @@ class CurvesAsSubmobjects(VGroup):
 
         class LineGradientExample(Scene):
             def construct(self):
-          curve = ParametricFunction(lambda t: [t, np.sin(t), 0], t_min = -PI, t_max=PI,stroke_width=10)
-          new_curve = CurvesAsSubmobjects(curve)
-          new_curve.set_color_by_gradient(BLUE, RED)
-          self.add(new_curve.shift(UP), curve)
+                                curve = ParametricFunction(lambda t: [t, np.sin(t), 0], t_min = -PI, t_max=PI,stroke_width=10)
+                                new_curve = CurvesAsSubmobjects(curve)
+                                new_curve.set_color_by_gradient(BLUE, RED)
+self.add(new_curve.shift(UP), curve)
 
     """
 
