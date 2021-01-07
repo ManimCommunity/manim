f045e4d29bf10fda48b52fe9f2bcea6fdf0f3662
kolibril13 <44469195+kolibril13@users.noreply.github.com>
2021-01-07 18:18:47 +0100

    Update manim/communitycolors.py

--- manim/communitycolors.py
+++ manim/communitycolors.py
@@ -1,7 +1,7 @@
 """The colormap of manim community"""
 
-<span class="x x-first x-last">LOGO_WHITE</span> = "#ece6e2"
-<span class="x x-first x-last">LOGO_GREEN</span> = "#87c2a5"
-<span class="x x-first x-last">LOGO_BLUE</span> = "#525893"
-<span class="x x-first x-last">LOGO_RED</span> = "#e07a5f"
-<span class="x x-first x-last">LOGO_BLACK</span> = "#343434"
+LOGO_WHITE = "#ece6e2"
+LOGO_GREEN = "#87c2a5"
+LOGO_BLUE = "#525893"
+LOGO_RED = "#e07a5f"
+LOGO_BLACK =  "#343434"
