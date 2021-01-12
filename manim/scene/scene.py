98e07a6c2f1c90b17c24e867a5e49ad63f067b95
kolibril13 <44469195+kolibril13@users.noreply.github.com>
2021-01-12 10:09:31 +0100

    Update scene.py

--- manim/scene/scene.py
+++ manim/scene/scene.py
@@ -887,13 +887,13 @@ class Scene(Container):
                 # Source of sound under Creative Commons 0 License. https://freesound.org/people/Druminfected/sounds/250551/
                 def construct(self):
                     dot = Dot().set_color(GREEN)
-                    self.add_sound("/docs/source/_static/click.wav")
+                    self.add_sound("click.wav")
                     self.add(dot)
                     self.wait()
-                    self.add_sound("/docs/source/_static/click.wav")
+                    self.add_sound("click.wav")
                     dot.set_color(BLUE)
                     self.wait()
-                    self.add_sound("/docs/source/_static/click.wav")
+                    self.add_sound("click.wav")
                     dot.set_color(RED)
                     self.wait()
         """
