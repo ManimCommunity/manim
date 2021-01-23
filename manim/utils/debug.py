cb5757ab662ce6c93e882fe6da5f68dfcf4394a1
kilacoda <kilacoda@gmail.com>
2021-01-23 10:11:16 +0530

    add deprecation notice

--- manim/utils/debug.py
+++ manim/utils/debug.py
@@ -1,10 +1,11 @@
 """Debugging utilities."""
 
-__all__ = ["print_family", "index_labels"]
+__all__ = ["print_family", "index_labels","get_submobject_index_labels"]
 
 from .color import BLACK
 from ..mobject.numbers import Integer
 from ..mobject.types.vectorized_mobject import VGroup
+import logging
 
 
 def print_family(mobject, n_tabs=0):
@@ -23,3 +24,10 @@ def index_labels(mobject, label_height=0.15):
         label.set_stroke(BLACK, 5, background=True)
         labels.add(label)
     return labels
+
+
+def get_submobject_index_labels(mobject, label_height=0.15):
+    logging.getLogger("manim").warning(
+        "get_submobject_index_labels has been deprecated and has been replaced by index_labels, which does the same thing, and will thus be removed in a future release."
+    )
+    return index_labels(mobject,label_height)
