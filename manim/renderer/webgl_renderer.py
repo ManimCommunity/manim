9d9cdd7bed00ccc63e1ca741b3ed9f10728966e8
Devin Neal <devin@eulertour.com>
2021-01-26 23:02:12 -0800

    Step through animations using updaters with fixed frame rate

--- manim/renderer/webgl_renderer.py
+++ manim/renderer/webgl_renderer.py
@@ -1,4 +1,5 @@
 import copy
+from ..utils.family import extract_mobject_family_members
 
 
 class WebGLRenderer:
@@ -16,11 +17,19 @@ class WebGLRenderer:
 
     def play(self, scene, *args, **kwargs):
         self.num_plays += 1
+        # If the scene contains an updater it must be updated frame by frame.
+        for mob in extract_mobject_family_members(scene.mobjects):
+            if len(mob.updaters) > 0:
+                self.skip_animations = False
+                break
         s = scene.compile_animation_data(*args, skip_rendering=True, **kwargs)
+        self.skip_animations = True
+
         scene_copy = copy.deepcopy(scene)
         scene_copy.renderer = self
         self.frame_server.keyframes.append(scene_copy)
         if s is None:
+            # Nothing happens in this animation, so there's no need to update it.
             scene_copy.is_static = True
         else:
             scene_copy.is_static = False
@@ -49,3 +58,4 @@ class WebGLRenderer:
 class WebGLCamera:
     def __init__(self, use_z_index=True):
         self.use_z_index = use_z_index
+        self.frame_rate = 15
