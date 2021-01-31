2ee02f41838c390cb48414e7ae1513e190117af1
WampyCakes <superpie@usa.com>
2021-01-31 15:20:12 -0500

    Update scene_file_writer.py

--- manim/scene/scene_file_writer.py
+++ manim/scene/scene_file_writer.py
@@ -291,11 +291,10 @@ class SceneFileWriter(object):
         image : np.array
             The pixel array of the image to save.
         """
-        file_path = (
-            self.image_file_path
-            if config["output_file"]
-            else add_version_before_extension(self.image_file_path)
-        )
+        file_path = self.image_file_path
+        if not config["output_file"]:
+            file_path = add_version_before_extension(file_path)
+        
         image.save(file_path)
         self.print_file_ready_message(file_path)
 
