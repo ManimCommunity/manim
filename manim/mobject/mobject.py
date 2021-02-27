60b240a49d9af2d4fe987ba42934a2cb461cd286
friedkeenan <friedkeenan@protonmail.com>
2021-02-27 03:48:01 -0600

    More descriptive error when accessing an unhandled mobject attribute (#1059)
    * More descriptive error when accessing an unhandled mobject attribute
    
    * Fix tests, stop taking advantage of mobject representations

--- manim/mobject/mobject.py
+++ manim/mobject/mobject.py
@@ -352,7 +352,7 @@ class Mobject(Container):
             return types.MethodType(setter, self)
 
         # Unhandled attribute, therefore error
-        raise AttributeError
+        raise AttributeError(f"{type(self).__name__} object has no attribute '{attr}'")
 
     @property
     def width(self):
