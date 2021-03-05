e635d3ab5c644ad1be1a5f90388c1b97f46efedf
Naveen M K <naveen@syrusdark.website>
2021-03-06 00:36:24 +0530

    Cleanup __main__.py - remove stray print statements use rich's print traceback instead
    fix double printing of version when --version
    is called
    
    Add some tests for the subcommands
    
    Signed-off-by: Naveen M K <naveen@syrusdark.website>

---
+++ tests/test_commands.py
@@ -0,0 +1,77 @@
+import subprocess
+import sys
+
+from .test_plugins.test_plugins import function_like_plugin
+
+try:
+    import importlib.metadata as importlib_metadata
+except ModuleNotFoundError:
+    import importlib_metadata
+
+import manim
+
+
+def call_command(command, cwd=None, env=None):
+    a = subprocess.run(
+        command,
+        stdout=subprocess.PIPE,
+        stderr=subprocess.PIPE,
+        check=True,
+        text=True,
+        cwd=cwd,
+        env=env,
+    )
+    return a
+
+
+def test_manim_version_from_command_line():
+    a = call_command(
+        [
+            sys.executable,
+            "-m",
+            "manim",
+            "--version",
+        ]
+    )
+    version = importlib_metadata.version("manim")
+    assert version in a.stdout
+    assert a.stdout.strip() == f"Manim Community v{version}"
+    assert manim.__version__ == version
+
+
+def test_manim_cfg_subcommand_no_subcommand():
+    a = call_command(
+        [
+            sys.executable,
+            "-m",
+            "manim",
+            "cfg",
+        ]
+    )
+    assert "No subcommand provided; Exiting..." in a.stdout
+
+
+def test_manim_plugis_subcommand_no_subcommand():
+    a = call_command(
+        [
+            sys.executable,
+            "-m",
+            "manim",
+            "plugins",
+        ]
+    )
+    assert "No flag provided; Exiting..." in a.stdout
+
+def test_manim_plugis_subcommand_listing(function_like_plugin):
+    # Check whether `test_plugin` is in plugins list
+    a = call_command(
+        [
+            sys.executable,
+            "-m",
+            "manim",
+            "plugins",
+            "--list"
+        ]
+    )
+    assert "test_plugin" in a.stdout
+
