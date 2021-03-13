8b4b077de32d82df8f7d69e0071eb765ab1c3618
Anton Ballmaier <aballmaier@posteo.de>
2021-03-13 19:20:25 +0100

    remove old example in add_upader

--- manim/mobject/mobject.py
+++ manim/mobject/mobject.py
@@ -644,29 +644,7 @@ class Mobject(Container):
 
                     self.play(Rotating(dot, about_point=ORIGIN, angle=TAU, run_time=TAU, rate_func=linear))
         
-
         """
-
-        # .. manim:: RotationUpdater
-
-        #     class RotationUpdater(Scene):
-        #         def construct(self):
-        #             def updater_forth(mobj, dt):
-        #                 mobj.rotate_about_origin(dt)
-        #             def updater_back(mobj, dt):
-        #                 mobj.rotate_about_origin(-dt)
-        #             line_reference = Line(ORIGIN, LEFT).set_color(WHITE)
-        #             line_moving = Line(ORIGIN, LEFT).set_color(YELLOW)
-        #             line_moving.add_updater(updater_forth)
-        #             self.add(line_reference, line_moving)
-        #             self.wait(2)
-        #             line_moving.remove_updater(updater_forth)
-        #             line_moving.add_updater(updater_back)
-        #             self.wait(2)
-        #             line_moving.remove_updater(updater_back)
-        #             self.wait(0.5)
-
-        # """
         if index is None:
             self.updaters.append(update_function)
         else:
