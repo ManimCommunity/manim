8f9fa3d8928feaf3926fb0f8033b298a3f7adcaa
TRoboto <m4bh@hotmail.com>
2021-03-22 09:50:22 +0200

    fix typo

--- manim/mobject/types/opengl_surface.py
+++ manim/mobject/types/opengl_surface.py
@@ -42,7 +42,7 @@ class OpenGLSurface(OpenGLMobject):
         shader_folder=None,
         **kwargs
     ):
-        self.passes_uv_func = uv_func
+        self.passed_uv_func = uv_func
         self.u_range = u_range if u_range is not None else (0, 1)
         self.v_range = v_range if v_range is not None else (0, 1)
         # Resolution counts number of points sampled, which for
@@ -68,8 +68,8 @@ class OpenGLSurface(OpenGLMobject):
 
     def uv_func(self, u, v):
         # To be implemented in subclasses
-        if self.passes_uv_func:
-            return self.passes_uv_func(u, v)
+        if self.passed_uv_func:
+            return self.passed_uv_func(u, v)
         return (u, v, 0.0)
 
     def init_points(self):
