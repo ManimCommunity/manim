b6eadf4bec9fb4bc2edeb606914d21eea2353a75
MrMallIronmaker <725mrm@gmail.com>
2021-03-24 08:41:36 -0700

    minor edits

--- tests/test_graphical_units/test_opengl.py
+++ tests/test_graphical_units/test_opengl.py
@@ -4,7 +4,6 @@ from manim import *
 from manim.opengl import *
 from ..utils.testing_utils import get_scenes_to_test
 from ..utils.GraphicalUnitTester import GraphicalUnitTester
-from tests.helpers.graphical_units import set_test_scene
 
 with tempconfig({"use_opengl_renderer": True}):
 
@@ -21,7 +20,7 @@ MODULE_NAME = "opengl"
 @pytest.mark.parametrize("scene_to_test", get_scenes_to_test(__name__), indirect=False)
 def test_scene(scene_to_test, tmpdir, show_diff):
     with tempconfig({"use_opengl_renderer": True}):
-        # allow 2/255 differences with opengl because of system dependencies
+        # allow 1/255 RGB value differences with opengl tests because of differences across platforms
         GraphicalUnitTester(scene_to_test[1], MODULE_NAME, tmpdir, rgb_atol=1.01).test(
             show_diff=show_diff
         )
