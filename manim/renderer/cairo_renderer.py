e4049da00a8e4fed065d08f5abd99aa1b677e311
Hugues Devimeux <thespourieye@gmail.com>
2021-03-30 12:15:16 +0200

    fixed logic

--- manim/renderer/cairo_renderer.py
+++ manim/renderer/cairo_renderer.py
@@ -84,21 +84,23 @@ class CairoRenderer:
 
         scene.compile_animation_data(*args, **kwargs)
 
-        if config["disable_caching"]:
-            hash_current_animation = f"uncached_{self.num_plays:05}"
         if self.skip_animations:
             logger.debug(f"Skipping animation {self.num_plays}")
             hash_current_animation = None
         else:
-            hash_current_animation = get_hash_from_play_call(
-                scene, self.camera, scene.animations, scene.mobjects
-            )
-            if self.file_writer.is_already_cached(hash_current_animation):
-                logger.info(
-                    f"Animation {self.num_plays} : Using cached data (hash : %(hash_current_animation)s)",
-                    {"hash_current_animation": hash_current_animation},
+            if config["disable_caching"]:
+                logger.debug("Caching disabled.")
+                hash_current_animation = f"uncached_{self.num_plays:05}"
+            else:
+                hash_current_animation = get_hash_from_play_call(
+                    scene, self.camera, scene.animations, scene.mobjects
                 )
-                self.skip_animations = True
+                if self.file_writer.is_already_cached(hash_current_animation):
+                    logger.info(
+                        f"Animation {self.num_plays} : Using cached data (hash : %(hash_current_animation)s)",
+                        {"hash_current_animation": hash_current_animation},
+                    )
+                    self.skip_animations = True
         # adding None as a partial movie file will make file_writer ignore the latter.
         self.file_writer.add_partial_movie_file(hash_current_animation)
         self.animations_hashes.append(hash_current_animation)
