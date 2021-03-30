158c45cead1f95d9d87d72aa82ee34e17c4ae93b
kolibril13 <44469195+kolibril13@users.noreply.github.com>
2021-03-30 11:56:52 +0200

    Removed SmallDot from example (#1195)
    * Update geometry.py
    
    * Update geometry.py

--- manim/mobject/geometry.py
+++ manim/mobject/geometry.py
@@ -8,14 +8,15 @@ Examples
 
     class UsefulAnnotations(Scene):
         def construct(self):
-            m0 = SmallDot()
+            m0 = Dot()
             m1 = AnnotationDot()
             m2 = LabeledDot("ii")
             m3 = LabeledDot(MathTex(r"\alpha").set_color(ORANGE))
-            m4 = CurvedArrow(ORIGIN, 2*LEFT)
-            m5 = CurvedDoubleArrow(ORIGIN, 2*RIGHT)
+            m4 = CurvedArrow(2*LEFT, 2*RIGHT, radius= -5)
+            m5 = CurvedArrow(2*LEFT, 2*RIGHT, radius= 8)
+            m6 = CurvedDoubleArrow(ORIGIN, 2*RIGHT)
 
-            self.add(m0, m1, m2, m3, m4, m5)
+            self.add(m0, m1, m2, m3, m4, m5, m6)
             for i, mobj in enumerate(self.mobjects):
                 mobj.shift(DOWN * (i-3))
 
