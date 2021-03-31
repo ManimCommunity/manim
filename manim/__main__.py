f65493c480e61e670ba54581e8b08e04070bcf37
Jason Villanueva <a@jsonvillanueva.com>
2021-03-30 23:30:09 -0700

    print colored version always

--- manim/__main__.py
+++ manim/__main__.py
@@ -1,11 +1,20 @@
+import sys
 import click
 from click_default_group import DefaultGroup
-from . import __version__
+from . import __version__, console
 from .constants import EPILOG
 from .constants import CONTEXT_SETTINGS
 from .cli.cfg.commands import cfg
 from .cli.plugins.commands import plugins
 from .cli.render.commands import render
+
+
+def exit_early(ctx, param, value):
+    if value:
+        sys.exit()
+
+
+console.print(f"Manim Community [green]v{__version__}[/green]")
 
 
 @click.group(
@@ -16,8 +25,14 @@ from .cli.render.commands import render
     help="Animation engine for explanatory math videos",
     epilog=EPILOG,
 )
-@click.version_option(
-    version=__version__, prog_name="Manim Community", message="%(prog)s v%(version)s"
+@click.option(
+    "-v",
+    "--version",
+    is_flag=True,
+    help="Show version and exit.",
+    callback=exit_early,
+    is_eager=True,
+    expose_value=False,
 )
 @click.pass_context
 def main(ctx):
