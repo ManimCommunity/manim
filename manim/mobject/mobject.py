f4d34ac0677d1051e6418e4d7176d9b4ec094cca
kolibril13 <44469195+kolibril13@users.noreply.github.com>
2021-04-01 10:38:35 +0200

    Update manim/mobject/mobject.py

--- manim/mobject/mobject.py
+++ manim/mobject/mobject.py
@@ -1766,7 +1766,7 @@ class Mobject(Container):
         return self.get_coord(2, direction)
 
     def get_start(self):
-        """Returns the point, where the stroke that surrounds the :class:`~.Mobject` starts. 
+        """Returns the point, where the stroke that surrounds the :class:`~.Mobject` starts."""
         self.throw_error_if_no_points()
         if config["use_opengl_renderer"]:
             return np.array(self.data["points"][0])
