7132833085e9e93d00eaedc8f078faa443313845
Ricky Chon <rickychon99@gmail.com>
2021-04-07 11:04:02 -0400

    Remove argument check for outdated version
    Too verbose, value should be configured via config

--- manim/cli/render/commands.py
+++ manim/cli/render/commands.py
@@ -137,7 +137,7 @@ def render(
             except Exception:
                 console.print_exception()
 
-    if config.notify_outdated_version or args["notify_outdated_version"]:
+    if config.notify_outdated_version:
         manim = requests.get("https://pypi.org/pypi/manim/json")
         releases = manim.json()["releases"].keys()
         stable = max(releases)
