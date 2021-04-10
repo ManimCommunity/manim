618f5e9f6bf78104b6965ca46a2f677dcdcc61d7
Anton Ballmaier <aballmaier@posteo.de>
2021-04-10 15:22:44 +0200

    Finishing touches - Slide back correctly aligned - documentation

--- manim/mobject/logo.py
+++ manim/mobject/logo.py
@@ -2,21 +2,18 @@
 
 __all__ = ["ManimBanner"]
 
-from os import scandir
-from typing import Union
-from manim.utils.color import RED
-from manim.animation.animation import Animation
-import numpy as np
-
 from ..animation.update import UpdateFromAlphaFunc
 from ..animation.composition import AnimationGroup, Succession
 from ..animation.fading import FadeIn
-from ..animation.transform import ApplyMethod
 from ..constants import DOWN, LEFT, ORIGIN, RIGHT, TAU, UP
 from ..mobject.geometry import Circle, Square, Triangle
 from ..mobject.svg.tex_mobject import MathTex, Tex
 from ..mobject.types.vectorized_mobject import VGroup
-from ..utils.rate_functions import ease_in_out_bounce, ease_in_out_circ, ease_in_out_cubic, ease_out_elastic, ease_out_sine, rush_from, smooth
+from ..utils.rate_functions import (
+    ease_in_out_cubic,
+    ease_out_sine,
+    smooth,
+)
 from ..utils.tex_templates import TexFontTemplates
 
 
@@ -34,27 +31,24 @@ class ManimBanner(VGroup):
 
     Examples
     --------
-
-    .. manim:: BannerDarkBackground
-
-        class BannerDarkBackground(Scene):
+    .. manim:: DarkThemeBanner
+        class DarkThemeBanner(Scene):
             def construct(self):
-                banner = ManimBanner().scale(0.5).to_corner(DR)
-                self.play(FadeIn(banner))
+                banner = ManimBanner()
+                self.play(banner.create())
                 self.play(banner.expand())
-                self.play(FadeOut(banner))
+                self.wait()
+                self.play(Unwrite(banner))
 
-    .. manim:: BannerLightBackground
-
-        class BannerLightBackground(Scene):
+    .. manim:: LightThemeBanner
+        class LightThemeBanner(Scene):
             def construct(self):
                 self.camera.background_color = "#ece6e2"
-                banner_large = ManimBanner(dark_theme=False).scale(0.7)
-                banner_small = ManimBanner(dark_theme=False).scale(0.35)
-                banner_small.next_to(banner_large, DOWN)
-                self.play(banner_large.create(), banner_small.create())
-                self.play(banner_large.expand(), banner_small.expand())
-                self.play(FadeOut(banner_large), FadeOut(banner_small))
+                banner = ManimBanner(dark_theme=False)
+                self.play(banner.create())
+                self.play(banner.expand())
+                self.wait()
+                self.play(Unwrite(banner))
 
     """
 
@@ -116,7 +110,7 @@ class ManimBanner(VGroup):
             self.anim.scale(scale_factor, **kwargs)
         return super().scale(scale_factor, **kwargs)
 
-    def create(self, run_time: float = 2):
+    def create(self, run_time: float = 2) -> AnimationGroup:
         """The creation animation for Manim's logo.
 
         Parameters
@@ -147,12 +141,14 @@ class ManimBanner(VGroup):
                 shape.shift((shape.final_position - shape.initial_position) * alpha)
                 shape.rotate(TAU * alpha, about_point=shape_center)
                 shape.rotate(-TAU * alpha, about_point=shape.get_center_of_mass())
-                shape.set_opacity(min(1, alpha*3))
+                shape.set_opacity(min(1, alpha * 3))
 
         return AnimationGroup(
-            UpdateFromAlphaFunc(self.shape, spiral_updater, run_time=run_time, rate_func=ease_out_sine),
+            UpdateFromAlphaFunc(
+                self.shape, spiral_updater, run_time=run_time, rate_func=ease_out_sine
+            ),
             FadeIn(self.M, run_time=run_time / 2),
-            lag_ratio=0.1
+            lag_ratio=0.1,
         )
 
     def expand(self, run_time: float = 1.5, direction="center") -> Succession:
@@ -175,11 +171,25 @@ class ManimBanner(VGroup):
         ----------
         run_time
             The run time of the animation.
+        direction
+            The direction in which the logo is expanded.
 
         Returns
         -------
         :class:`~.Succession`
             An animation to be used in a :meth:`.Scene.play` call.
+
+        Examples
+        --------
+        .. manim:: ExpandDirections
+            class ExpandDirections(Scene):
+                def construct(self):
+                    banners = [ManimBanner().scale(0.5).shift(UP*x) for x in [-2, 0, 2]]
+                    self.play(
+                        banners[0].expand(direction = "right"),
+                        banners[1].expand(direction = "center"),
+                        banners[2].expand(direction = "left"),
+                    )
 
         """
         if direction not in ["left", "right", "center"]:
@@ -188,67 +198,65 @@ class ManimBanner(VGroup):
         m_shape_offset = 6.25 * self.scale_factor
         shape_sliding_overshoot = self.scale_factor * 0.8
         m_anim_buff = 0.06
-        self.anim.next_to(self.M, buff=m_anim_buff)\
-                 .align_to(self.M, DOWN)\
-                 .set_opacity(0)
+        self.anim.next_to(self.M, buff=m_anim_buff).align_to(self.M, DOWN).set_opacity(
+            0
+        )
         self.shape.save_state()
         m_clone = self.anim[-1].copy()
         self.add(m_clone)
         m_clone.move_to(self.shape)
 
-        def slide_and_uncover(mob, alpha):
-            mob.shape.restore()
-            
-            # Add a slight upwards curve to the motion.
-            # This is needed to let the quare fully cover the "i"
-            mob.shape.shift(((0.5-alpha)**2 - 0.25) * DOWN * 0.25)
+        self.M.save_state()
+        left_group = VGroup(self.M, self.anim, m_clone)
 
-            side_shift = alpha * (m_shape_offset + shape_sliding_overshoot) * RIGHT
-            left_group = VGroup(self.M, self.anim, m_clone)
-
+        def shift(vector):
+            self.shape.restore()
+            left_group.align_to(self.M.saved_state, LEFT)
             if direction == "right":
-                mob.shape.shift(side_shift)
+                self.shape.shift(vector)
             elif direction == "center":
-                mob.shape.shift(side_shift/2)
-                left_group.align_to(m_start, LEFT)
-                left_group.shift(-side_shift/2)
+                self.shape.shift(vector / 2)
+                left_group.shift(-vector / 2)
             elif direction == "left":
-                left_group.align_to(m_start, LEFT)
-                left_group.shift(-side_shift)
-                
+                left_group.shift(-vector)
 
-            # if direction == "center": 
-            #     VGroup(mob, self.anim, m_clone).shift(mob.fixed_position-mob.get_center()[0]*RIGHT)
-            # elif direction == "left":
-            #     VGroup(mob, self.anim, m_clone).shift(mob.fixed_position-mob.get_center()[0]*RIGHT*2)
+        def slide_and_uncover(mob, alpha):
+            shift(alpha * (m_shape_offset + shape_sliding_overshoot) * RIGHT)
 
-
+            # Add letters when they are covered
             for letter in mob.anim:
                 if mob.square.get_center()[0] > letter.get_center()[0]:
                     letter.set_opacity(1)
                     self.add_to_back(letter)
+
+            # Finish animation
             if alpha == 1:
                 self.remove(*[self.anim])
                 self.add_to_back(self.anim)
                 mob.shape.set_z_index(0)
                 mob.shape.save_state()
-            
+                mob.M.save_state()
 
         def slide_back(mob, alpha):
-            m_clone.set_opacity(1)
-            m_clone.move_to(mob.anim[-1])         
-            mob.shape.restore()
-            mob.shape.shift(alpha * shape_sliding_overshoot * LEFT)
-            mob.anim.set_opacity(1)
+            if alpha == 0:
+                m_clone.set_opacity(1)
+                m_clone.move_to(mob.anim[-1])
+                mob.anim.set_opacity(1)
+
+            shift(alpha * shape_sliding_overshoot * LEFT)
+
             if alpha == 1:
                 mob.remove(m_clone)
                 mob.add_to_back(mob.shape)
 
-        m_start = self.M.copy()
-        
-
         return Succession(
-            UpdateFromAlphaFunc(self, slide_and_uncover, run_time=run_time * 2/3, rate_func=ease_in_out_cubic),
-            UpdateFromAlphaFunc(self, slide_back, run_time=run_time * 1/3, rate_func=smooth),
+            UpdateFromAlphaFunc(
+                self,
+                slide_and_uncover,
+                run_time=run_time * 2 / 3,
+                rate_func=ease_in_out_cubic,
+            ),
+            UpdateFromAlphaFunc(
+                self, slide_back, run_time=run_time * 1 / 3, rate_func=smooth
+            ),
         )
-        
