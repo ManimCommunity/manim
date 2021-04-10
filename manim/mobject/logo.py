14067709b790c1494e8a31dae142645bb2370e10
Anton Ballmaier <aballmaier@posteo.de>
2021-04-10 23:35:55 +0200

    Update manim/mobject/logo.py
    Co-authored-by: Benjamin Hackl <devel@benjamin-hackl.at>

--- manim/mobject/logo.py
+++ manim/mobject/logo.py
@@ -185,9 +185,9 @@ class ManimBanner(VGroup):
                 def construct(self):
                     banners = [ManimBanner().scale(0.5).shift(UP*x) for x in [-2, 0, 2]]
                     self.play(
-                        banners[0].expand(direction = "right"),
-                        banners[1].expand(direction = "center"),
-                        banners[2].expand(direction = "left"),
+                        banners[0].expand(direction="right"),
+                        banners[1].expand(direction="center"),
+                        banners[2].expand(direction="left"),
                     )
 
         """
