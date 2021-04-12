c6eb3e61becd6371ae3cfe89f173f43ad8f9e841
Benjamin Hackl <devel@benjamin-hackl.at>
2021-04-13 01:00:29 +0200

    add tests for adding/removing vertices

--- tests/test_graph.py
+++ tests/test_graph.py
@@ -1,4 +1,4 @@
-from manim import Graph
+from manim import Graph, Text
 
 
 def test_graph_creation():
@@ -9,3 +9,28 @@ def test_graph_creation():
     assert str(G_manual) == "Graph on 4 vertices and 4 edges"
     G_spring = Graph(vertices=vertices, edges=edges)
     assert str(G_spring) == "Graph on 4 vertices and 4 edges"
+
+def test_graph_add_vertices():
+    G = Graph([1, 2, 3], [(1, 2), (2, 3)])
+    G.add_vertex(4)
+    assert str(G) == "Graph on 4 vertices and 2 edges"
+    G.add_vertex(5, label=Text("5"))
+    assert str(G) == "Graph on 5 vertices and 2 edges"
+    assert 5 in G._labels
+    assert 5 in G._vertex_config
+    G.add_vertices([6, 7, 8])
+    assert len(G.vertices) == 8
+    assert len(G._graph.nodes()) == 8 
+
+def test_graph_remove_vertices():
+    G = Graph([1, 2, 3, 4, 5], [(1, 2), (2, 3), (3, 4), (4, 5)])
+    removed_mobjects = G.remove_vertex(3)
+    assert len(removed_mobjects) == 3
+    assert str(G) == "Graph on 4 vertices and 2 edges"
+    assert list(G.vertices.keys()) == [1, 2, 4, 5]
+    assert list(G.edges.keys()) == [(1, 2), (4, 5)]
+    removed_mobjects = G.remove_vertices(4, 5)
+    assert len(removed_mobjects) == 3
+    assert str(G) == "Graph on 2 vertices and 1 edges"
+    assert list(G.vertices.keys()) == [1, 2]
+    assert list(G.edges.keys()) == [(1, 2)]
