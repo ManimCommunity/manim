ec18fcd1cfb428300b8b9fc46015d8ab3fb77b1f
kolibril13 <44469195+kolibril13@users.noreply.github.com>
2021-04-15 09:51:03 +0200

    Update manim/mobject/geometry.py

--- manim/mobject/geometry.py
+++ manim/mobject/geometry.py
@@ -2275,17 +2275,21 @@ class Angle(Arc, Elbow):
 
         class FilledAngle(Scene):
             def construct(self):
-                l1 = Line(ORIGIN, 2*UP+RIGHT).set_color(GREEN)
-                l2 = Line(ORIGIN, 2*UP+RIGHT).set_color(GREEN).rotate(-20*DEGREES, about_point=ORIGIN)
-                norm=np.linalg.norm(l1.get_start() - l1.get_end())
-                a1=Angle(l1,l2, other_angle=True, radius= norm-0.5).set_color(GREEN)
-                a2=Angle(l1,l2, other_angle=True, radius= norm).set_color(GREEN)
-                q1=a1.get_points()
-                q2= a2.reverse_direction().get_points()
-                pnts= np.concatenate([q1,q2, q1[0].reshape(1,3)])
+                l1 = Line(ORIGIN, 2 * UP + RIGHT).set_color(GREEN)
+                l2 = (
+                    Line(ORIGIN, 2 * UP + RIGHT)
+                    .set_color(GREEN)
+                    .rotate(-20 * DEGREES, about_point=ORIGIN)
+                )
+                norm = l1.get_length()
+                a1 = Angle(l1, l2, other_angle=True, radius=norm - 0.5).set_color(GREEN)
+                a2 = Angle(l1, l2, other_angle=True, radius=norm).set_color(GREEN)
+                q1 = a1.get_points() #  save all coordinates of points of angle a1
+                q2 = a2.reverse_direction().get_points()  #  save all coordinates of points of angle a1 (in reversed direction)
+                pnts = np.concatenate([q1, q2, q1[0].reshape(1, 3)])  # adds points and ensures that path starts and ends at same point
                 mfill = VMobject().set_color(ORANGE)
-                mfill.set_points_as_corners([q for q in pnts]).set_fill(GREEN, opacity=1)
-                self.add(l1,l2)
+                mfill.set_points_as_corners(pnts).set_fill(GREEN, opacity=1)
+                self.add(l1, l2)
                 self.add(mfill)
 
     """
