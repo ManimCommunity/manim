3c9a1805479e7c97d393a5d968a2d6496d67ecbd
Hugues Devimeux <thespourieye@gmail.com>
2021-04-16 18:53:17 +0200

    Added test

--- tests/test_graphical_units/test_graphscene.py
+++ tests/test_graphical_units/test_graphscene.py
@@ -5,6 +5,7 @@ from manim import *
 from ..utils.GraphicalUnitTester import GraphicalUnitTester
 from ..utils.testing_utils import get_scenes_to_test
 
+EPSILON = 1e-5
 
 class PlotFunctions(GraphScene):
     def __init__(self, **kwargs):
@@ -29,9 +30,20 @@ class PlotFunctions(GraphScene):
 
         self.add(f)
 
+class PlotFunctionWithDiscontinuitiesTest(GraphScene):
+    def construct(self):
+        self.x_axis_label, self.y_axis_label = None, None
+        self.setup_axes()
+        func_graph = self.get_graph(
+            lambda t: t % 1.0,
+            x_min=-2 + EPSILON,
+            x_max=2 - EPSILON,
+            discontinuities=range(-2, 2),
+        )
+        self.play(Animation(func_graph))
+        
 
 MODULE_NAME = "plot"
-
 
 @pytest.mark.slow
 @pytest.mark.parametrize("scene_to_test", get_scenes_to_test(__name__), indirect=False)
