de332e9b8d2636aa5ff4fb3801b8469e483aa74f
e4coder <e4coder@gmail.com>
2021-04-25 04:02:03 +0500

    added select_resolution() and project command

--- manim/cli/project/commands.py
+++ manim/cli/project/commands.py
@@ -4,7 +4,7 @@ init -  The init subcommand is a quick and easy way to initialize a project
         Ite copies 2 files and pastes them in the current working dir
 
 new  -  The new command group has 2 commands. these commands handle project creation
-        and scene creation
+        and scene creation or insertion
 
         project -   The project subcommand is used for project creation
                     This command is similar to init but different in a way
@@ -25,31 +25,35 @@ from shutil import copyfile
 import click
 
 from ... import console
-from ...constants import CONTEXT_SETTINGS, EPILOG
+from ...constants import CONTEXT_SETTINGS, EPILOG, QUALITIES
 
-cfg_vars = [
-    "frame_rate",
-    "resolution",
-    "background_color",
-    "background_opacity",
-    "scene_names",
-]
+cfg_vars = {
+    "frame_rate": 60,
+    "resolution": (1920, 1200),
+    "background_color": "BLACK",
+    "background_opacity": 1,
+    "scene_names": "DefaultScene",
+}
+
+cfg_resolutions = []
 
 
-def update_cfg(vars, values, project_cfg_path):
+def update_cfg(cfg_vars, project_cfg_path):
     """Updates the manim.cfg file in the newly created project
     Parameters
     ----------
-    vars : list
-        list of variables that are to be edited after files are copied.
-    values : list
-        list of values that will be edited in the manim.cfg file
+    values : dictionary : Look at `cfg_vars`
+        dictionary of values that will be edited in the manim.cfg file
     """
     config = configparser.ConfigParser()
     config.read(project_cfg_path)
     cli_config = config["CLI"]
-    for num, var in enumerate(vars, start=0):
-        cli_config[var] = values[num]
+    for key, value in cfg_vars.items():
+        if key == "resolution":
+            cli_config["pixel_height"] = value[0]
+            cli_config["pixel_width"] = value[1]
+        else:
+            cli_config[key] = value
 
     with open(project_cfg_path, "w") as conf:
         config.write(conf)
@@ -67,15 +71,95 @@ def copy_template_files(project_dir=Path("."), template_name="default"):
         In the future templates may be downloaded over the internet
         and users may be able to create and share templates
     """
-    init_default_cfg_path = Path.resolve(
-        Path(__file__).parent / "templates/template.cfg"
-    )
-    default_template = Path.resolve(
+    template_cfg_path = Path.resolve(Path(__file__).parent / "templates/template.cfg")
+    template_scene_path = Path.resolve(
         Path(__file__).parent / f"templates/{template_name}.py"
     )
 
-    copyfile(init_default_cfg_path, Path.resolve(project_dir / "manim.cfg"))
+    if not template_scene_path.exists():
+        template_scene_path = Path.resolve(
+            Path(__file__).parent / f"templates/{template_name}.py"
+        )
+
+    copyfile(template_cfg_path, Path.resolve(project_dir / "manim.cfg"))
     console.print("...copied [green]manim.cfg[/green]\n")
 
-    copyfile(default_template, Path.resolve(project_dir / "main.py"))
+    copyfile(template_scene_path, Path.resolve(project_dir / "main.py"))
     console.print("...copied [green]main.py[/green]\n")
+
+
+def select_resolution(stdscr):
+    for key, quality in QUALITIES.items():
+        cfg_resolutions.append((key, quality))
+
+    attributes = {}
+    curses.init_pair(1, curses.COLOR_WHITE, curses.COLOR_BLACK)
+    attributes["normal"] = curses.color_pair(1)
+
+    curses.init_pair(2, curses.COLOR_BLACK, curses.COLOR_WHITE)
+    attributes["highlighted"] = curses.color_pair(2)
+
+    c = 0  # last character read
+    option = 0  # the current option that is marked
+    while c != 10:  # Enter in ascii
+        stdscr.erase()
+        stdscr.addstr("Select rendring quality?\n", curses.A_UNDERLINE)
+        for i in range(len(cfg_resolutions)):
+            if i == option:
+                attr = attributes["highlighted"]
+            else:
+                attr = attributes["normal"]
+            stdscr.addstr("{0}. ".format(i + 1))
+            stdscr.addstr(cfg_resolutions[i][0] + "\n", attr)
+        c = stdscr.getch()
+        if c == curses.KEY_UP and option > 0:
+            option -= 1
+        elif c == curses.KEY_DOWN and option < len(cfg_resolutions) - 1:
+            option += 1
+
+    stdscr.addstr("You chose {0}".format(cfg_resolutions[option][0]))
+    stdscr.getch()
+
+
+@click.command(
+    context_settings=CONTEXT_SETTINGS,
+    no_args_is_help=False,
+    epilog=EPILOG,
+    help="Initialize New Project",
+)
+@click.argument("project_name", type=Path, required=False)
+@click.argument("template_name", required=False)
+@click.option(
+    "-d",
+    "--default",
+    "default_settings",
+    is_flag=True,
+    help="Default settings for Project Initialization",
+    nargs=1,
+)
+def project(default_settings, **args):
+    if args["project_name"]:
+        project_name = args["project_name"]
+    else:
+        project_name = click.prompt("Project Name", type=Path)
+
+    if project_name.is_dir():
+        console.print("Project Name Exists")
+    else:
+        project_name.mkdir()
+        new_cfg_vars = dict()
+
+        if not default_settings:
+            for key, value in cfg_vars.items():
+                if key == "scene_names":
+                    pass
+                elif key == "resolution":
+                    pass
+                else:
+                    new_cfg_vars[key] = click.prompt(f"{key}")
+
+            copy_template_files(project_name)
+            update_cfg(new_cfg_vars, Path.resolve(project_name / "manim.cfg"))
+
+        else:
+            console.print(default_settings)
