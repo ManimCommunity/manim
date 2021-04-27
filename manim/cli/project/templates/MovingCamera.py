38ca3e50c979ab44a246b5a9a83b261522dfd4c8
e4coder <e4coder@gmail.com>
2021-04-27 15:18:24 +0500

    added moving camera scene template

---
+++ manim/cli/project/templates/MovingCamera.py
@@ -0,0 +1,11 @@
+from manim import *
+
+
+class MovingCamer(MovingCameraScene):
+    def construct(self):
+        text = Text("Hello World").set_color(BLUE)
+        self.add(text)
+        self.camera.frame.save_state()
+        self.play(self.camera.frame.animate.set(width=text.width * 1.2))
+        self.wait(0.3)
+        self.play(Restore(self.camera.frame))
