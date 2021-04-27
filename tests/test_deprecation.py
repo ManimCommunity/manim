5700ff1e286ee927d410ad29f7062b0b522903e4
Ricky Chon <rickychon99@gmail.com>
2021-04-27 16:22:36 -0400

    Add tests for since and until arguments

--- tests/test_deprecation.py
+++ tests/test_deprecation.py
@@ -14,6 +14,18 @@ class Foo:
         pass
 
 
+@deprecated(since="v0.6.0")
+class Bar:
+    def __init__(self):
+        pass
+
+
+@deprecated(until="06/01/2021")
+class Baz:
+    def __init__(self):
+        pass
+
+
 def test_deprecate_class_no_args(caplog):
     """Test the deprecation of a class (decorator with no arguments)."""
     f = Foo()
@@ -22,4 +34,26 @@ def test_deprecate_class_no_args(caplog):
     assert (
         message
         == "The class Foo has been deprecated and may be removed in a later version."
+    )
+
+
+def test_deprecate_class_since(caplog):
+    """Test the deprecation of a class (decorator with since argument)."""
+    b = Bar()
+    assert len(caplog.record_tuples) == 1
+    logger_name, level, message = caplog.record_tuples[0]
+    assert (
+        message
+        == "The class Bar has been deprecated since v0.6.0 and may be removed in a later version."
+    )
+
+
+def test_deprecate_class_until(caplog):
+    """Test the deprecation of a class (decorator with until argument)."""
+    bz = Baz()
+    assert len(caplog.record_tuples) == 1
+    logger_name, level, message = caplog.record_tuples[0]
+    assert (
+        message
+        == "The class Baz has been deprecated and is expected to be removed after 06/01/2021."
     )
