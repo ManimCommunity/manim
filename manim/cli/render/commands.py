d787b44826e9b9bc316ee808b1ff5fa0852fe4cb
CallMeMisterOwl <denizhasler@outlook.de>
2021-04-28 12:12:48 +0200

    Move version check into previous catch block Instead of checking if the request was successful, the version check gets executed when 'request.raise_for_status()' doesn't throw an exception

--- manim/cli/render/commands.py
+++ manim/cli/render/commands.py
@@ -153,28 +153,26 @@ def render(
         try:
             req_info = requests.get(manim_info_url)
             req_info.raise_for_status()
+
+            stable = req_info.json()["info"]["version"]
+            if stable != __version__:
+                console.print(
+                    f"You are using manim version [red]v{__version__}[/red], but version [green]v{stable}[/green] is available."
+                )
+                console.print(
+                    "You should consider upgrading via [yellow]pip install -U manim[/yellow]"
+                )
         except requests.exceptions.HTTPError:
             logger.debug(f"HTTP Error: {warn_prompt}")
         except requests.exceptions.ConnectionError:
             logger.debug(f"Connection Error: {warn_prompt}")
         except requests.exceptions.Timeout:
             logger.debug(f"Timed Out: {warn_prompt}")
+        except json.JSONDecodeError:
+            logger.debug(warn_prompt)
+            logger.debug(f"Error decoding JSON from {manim_info_url}")
         except Exception:
             logger.debug(f"Something went wrong: {warn_prompt}")
 
-        if req_info.status_code == requests.codes.ok:
-            try:
-                stable = req_info.json()["info"]["version"]
-
-                if stable != __version__:
-                    console.print(
-                        f"You are using manim version [red]v{__version__}[/red], but version [green]v{stable}[/green] is available."
-                    )
-                    console.print(
-                        "You should consider upgrading via [yellow]pip install -U manim[/yellow]"
-                    )
-            except json.JSONDecodeError:
-                logger.debug(warn_prompt)
-                logger.debug(f"Error decoding JSON from {manim_info_url}")
 
     return args
