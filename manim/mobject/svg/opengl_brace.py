64d9576c28487778fdd4418cebab3e5d1c8da580
TRoboto <m4bh@hotmail.com>
2021-04-30 11:52:47 +0300

    update docstring

--- manim/mobject/svg/opengl_brace.py
+++ manim/mobject/svg/opengl_brace.py
@@ -212,7 +212,7 @@ class OpenGLBraceBetweenPoints(OpenGLBrace):
                 def construct(self):
                     p1 = [0,0,0]
                     p2 = [1,2,0]
-                    brace = BraceBetweenPoints(p1,p2)
+                    brace = OpenGLBraceBetweenPoints(p1,p2)
                     self.play(Create(NumberPlane()))
                     self.play(Create(brace))
                     self.wait(2)
