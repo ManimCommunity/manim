ab49ba648315fbf7fbb869db1880cd93e02027fc
Anton Ballmaier <aballmaier@posteo.de>
2021-05-03 02:52:07 +0200

    fix example indentation

--- manim/animation/fading.py
+++ manim/animation/fading.py
@@ -3,12 +3,12 @@
 .. manim:: Fading
 
     class Fading(Scene):
-    def construct(self):
-        tex_in = Tex("Fade", "In").scale(3)
-        tex_out = Tex("Fade", "Out").scale(3)
-        self.play(FadeIn(tex_in, shift=DOWN, scale=0.66))
-        self.play(ReplacementTransform(tex_in, tex_out))
-        self.play(FadeOut(tex_out, shift=DOWN * 2, scale=1.5))
+        def construct(self):
+            tex_in = Tex("Fade", "In").scale(3)
+            tex_out = Tex("Fade", "Out").scale(3)
+            self.play(FadeIn(tex_in, shift=DOWN, scale=0.66))
+            self.play(ReplacementTransform(tex_in, tex_out))
+            self.play(FadeOut(tex_out, shift=DOWN * 2, scale=1.5))
 
 """
 
