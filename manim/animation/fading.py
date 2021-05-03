7b58941bf4fd6a62fb4ee947e5bd2d27421105ec
Anton Ballmaier <aballmaier@posteo.de>
2021-05-03 02:33:55 +0200

    Add docs

--- manim/animation/fading.py
+++ manim/animation/fading.py
@@ -79,6 +79,23 @@ from ..utils.rate_functions import there_and_back
 
 
 class _Fade(Transform):
+    """Fade in a :class:`~.Mobject` in or out.
+
+    Parameters
+    ----------
+    mobject
+        The mobject to be faded in.
+    shift
+        The vector by which the mobject shifts while beeing faded.
+    target_position
+        The position to/from which the mobject moves while beeing faded in. In case
+        another mobject is given as target position, its center is used.
+    scale
+        The factor by which the mobject is scaled initially before beeing rescaling to
+        its original size while beeing faded in.
+
+    """
+
     def __init__(
         self,
         mobject: Mobject,
@@ -101,7 +118,19 @@ class _Fade(Transform):
         self.scale_factor = scale
         super().__init__(mobject, **kwargs)
 
-    def _create_faded_mobject(self, fadeIn: bool):
+    def _create_faded_mobject(self, fadeIn: bool) -> Mobject:
+        """Create a faded, shifted and scaled copy of the mobject.
+
+        Parameters
+        ----------
+        fadeIn
+            Whether the faded mobject is used to fade in.
+
+        Returns
+        -------
+        Mobject
+            The faded, shifted and scaled copy of the mobject.
+        """
         faded_mobject = self.mobject.copy()
         faded_mobject.set_opacity(0)
         direction_modifier = -1 if fadeIn and not self.point_target else 1
@@ -111,6 +140,23 @@ class _Fade(Transform):
 
 
 class FadeIn(_Fade):
+    """Fade in a :class:`~.Mobject`.
+
+    Parameters
+    ----------
+    mobject
+        The mobject to be faded in.
+    shift
+        The vector by which the mobject shifts while beeing faded in.
+    target_position
+        The position from which the mobject starts while beeing faded in. In case
+        another mobject is given as target position, its center is used.
+    scale
+        The factor by which the mobject is scaled initially before beeing rescaling to
+        its original size while beeing faded in.
+
+    """
+
     def create_target(self):
         return self.mobject
 
@@ -119,6 +165,22 @@ class FadeIn(_Fade):
 
 
 class FadeOut(_Fade):
+    """Fade out a :class:`~.Mobject`.
+
+    Parameters
+    ----------
+    mobject
+        The mobject to be faded out.
+    shift
+        The vector by which the mobject shifts while beeing faded out.
+    target_position
+        The position to which the mobject moves while beeing faded out. In case another
+        mobject is given as target position, its center is used.
+    scale
+        The factor by which the mobject is scaled while beeing faded out.
+
+    """
+
     def __init__(self, mobject: Mobject, **kwargs) -> None:
         super().__init__(mobject, remover=True, **kwargs)
 
