6d009b27f47149feceed064948b895ba5a4ff900
Hugues Devimeux <thespourieye@gmail.com>
2021-05-04 20:48:57 +0200

    Fixed ytpo

--- tests/test_scene_rendering/test_cairo_renderer.py
+++ tests/test_scene_rendering/test_cairo_renderer.py
@@ -37,8 +37,8 @@ def test_skipping_status_with_from_to_and_up_to(using_temp_config, disabling_cac
 
 def test_when_animation_is_cached(using_temp_config):
     for _ in range(3):
-        # Renders several times to generate a cache.
-        # THis is run three times because in some edgy cases and some OS, a same scene can produce
+        # Render several times to generate a cache.
+        # This is ran three times because in some edgy cases and on some OS, a same scene can produce
         # a finite number of different hash. In this case, the scene wouldn't be detected as cached, making the test fail.
         scene = SquareToCircle()
         scene.render()
