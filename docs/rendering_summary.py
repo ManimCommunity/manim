82f77c3eef54e1b42d3269e11a7b1ef9ea4f55b9
Anton Ballmaier <aballmaier@posteo.de>
2021-05-05 01:59:00 +0200

    fix makefile issues

--- docs/rendering_summary.py
+++ docs/rendering_summary.py
@@ -1,16 +1,24 @@
 import itertools as it
 
+lining_char = "."
+
 print("\nRendering Summary\n-----------------\n")
 with open("rendering_times.csv") as file:
     data = [line.split(",") for line in file.read().splitlines()]
     # print(data)
     max_file_length = max([len(row[0]) for row in data])
     for key, group in it.groupby(data, key=lambda row: row[0]):
+        key = key.ljust(max_file_length + 1, lining_char)
         group = list(group)
+        if len(group) == 1:
+            print(f"{key}{row[2].rjust(7, lining_char)}s {row[1]}")
+            continue
         time_sum = sum([float(row[2]) for row in group])
-        print(f"{key.ljust(max_file_length)} {f'{time_sum:.3f}'.rjust(7)}")
+        print(
+            f"{key}{f'{time_sum:.3f}'.rjust(7, lining_char)}s  => {len(group)} EXAMPLES"
+        )
         for row in group:
-            print(f"{' '*max_file_length} {row[2].rjust(7)} {row[1]}")
+            print(f"{' '*(max_file_length)} {row[2].rjust(7)}s {row[1]}")
 
     # print(data)
 
