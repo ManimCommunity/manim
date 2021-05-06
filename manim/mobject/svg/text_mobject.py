8886892c82810eddc2c92d41afbdff0b04bb71a8
kolibril13 <44469195+kolibril13@users.noreply.github.com>
2021-05-06 17:14:36 +0200

    updated example once more

--- manim/mobject/svg/text_mobject.py
+++ manim/mobject/svg/text_mobject.py
@@ -322,14 +322,14 @@ class Text(SVGMobject):
 
         class TextItalicAndBoldExample(Scene):
             def construct(self):
-                text0 = Text('Hello world', t2c={'world':YELLOW})
                 text1 = Text('Hello world', slant=ITALIC)
                 text2 = Text('Hello world', t2s={'world':ITALIC})
                 text3 = Text('Hello world', weight=BOLD)
                 text4 = Text('Hello world', t2w={'world':BOLD})
-                text5 = Text("Visit us at docs.manim.community",t2c={'docs.manim.community':YELLOW}, disable_ligatures=True).scale(1.3).shift(DOWN)
-                self.add(text0, text1, text2, text3, text4,text5)
-                Group(*self.mobjects).arrange(DOWN, buff=.8)
+                text5 = Text('Hello world', t2c={'o':YELLOW}, disable_ligatures=True)
+                text6 = Text("Visit us at docs.manim.community",t2c={'docs.manim.community':YELLOW}, disable_ligatures=True).scale(1.3).shift(DOWN)
+                self.add( text1, text2, text3, text4,text5 , text6)
+                Group(*self.mobjects).arrange(DOWN, buff=.8).set_height(config.frame_height-LARGE_BUFF)
 
     .. manim:: TextMoreCustomization
             :save_last_frame:
