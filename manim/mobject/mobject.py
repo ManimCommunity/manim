ed45e4bfc4353829f7ac857037c89ae1efec23ac
Anton Ballmaier <aballmaier@posteo.de>
2021-05-08 11:54:56 +0200

    Update manim/mobject/mobject.py
    Co-authored-by: Jason Villanueva <a@jsonvillanueva.com>

--- manim/mobject/mobject.py
+++ manim/mobject/mobject.py
@@ -2337,7 +2337,7 @@ class Mobject(Container):
 
         .. manim:: ShuffleSumobjectsExample
 
-            class ShuffleSumobjectsExample(Scene):
+            class ShuffleSubmobjectsExample(Scene):
                 def construct(self):
                     s= VGroup(*[Dot().shift(i*0.1*RIGHT) for i in range(-20,20)])
                     s2= s.copy()
