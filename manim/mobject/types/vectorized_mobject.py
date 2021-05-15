15261ea65b4f81578c088a795d409bb37f98e927
Anton Ballmaier <aballmaier@posteo.de>
2021-05-15 16:23:47 +0200

    Fading module enhancements (#1454)
    * finish draft implementation
    * use @wrapss to remove docs problems
    * reduce complexity and add doc deprecation
    * Docs deprecated_params
    * add decoratos to __init__
    * Add decorators to documentation
    * Finish detailed docs
    * Apply suggestions from code review
    Co-authored-by: Naveen M K <naveen@syrusdark.website>
    
    * change since/until/message param docs
    * Mark get_callable_info and deprecation_text_component as private
    * change how until is included in warning messages
    * Add The to parameter descriptions
    * make params docs more readable
    * make redirections docs clearer
    * update dependency list
    * order dependencies
    * update examples
    * fix dependency problem
    * fix duplicate problem
    * change decorator version
    * updated lock file
    * enhance function / method separation
    * fix typo and double underscores
    * rename message_ and replacement_
    * Change warning to custom admonition
    * remove [] as default parameters
    * Remove "" around type
    * fix bug and move to inspect
    * doc string => docstring
    * rename to deprecate
    * change module docstring
    * change to deprecation.py
    * remove tests again
    * Rename decorators.py to deprecate.py
    * Update poetry.lock
    * Test if deprecated class outputs the correct warning when used
    * Rename deprecate to deprecation
    * Shorten conditional for msg, since, util
    * Specify decorator arguments in test
    * Add tests for since and until as arguments
    * Add test for msg argument for class
    * Add test for replacement argument for class
    * Remove unneeded docstring in deprecated class
    * Add test for deprecate method, no args
    * Update docstring examples, move deprecated method outside class
    * Add test for method in class, since and message args
    * Add test for deprecating nested class
    * Add test for deprecated method in nested class
    * Test deprecation of nested function
    * Test param deprecation, only params argument
    * Test deprecation of single method param
    * Rename single param test method name
    * Fix deprecated_params docstring whitespace
    * Test parameter redirection using tuple
    * Update warning msgs in deprecated_params docstring
    * Test parameter redirection using lambda function
    * Test param redirection from many to one
    * Test param redirection from one to many
    * Update Top.foo to deprecate with message
    * FadeIn & FadeOut overhaul and deprecations
    * Adjust examples
    * Add docs
    * Add simple example
    * handle empty docstrings
    * Update poetry.lock
    * Deprecate ShowCreation using decorator to test the docs
    * Test docstrings
    * add examples
    * Update example_scenes/basic.py
    
    Co-authored-by: Laith Bahodi <70682032+hydrobeam@users.noreply.github.com>
    Co-authored-by: Naveen M K <naveen@syrusdark.website>
    Co-authored-by: Ricky Chon <rickychon99@gmail.com>
    Co-authored-by: Laith Bahodi <70682032+hydrobeam@users.noreply.github.com>

--- manim/mobject/types/vectorized_mobject.py
+++ manim/mobject/types/vectorized_mobject.py
@@ -1619,7 +1619,7 @@ class VDict(VMobject):
                 self.play(FadeOut(my_dict["c"]))
                 self.wait()
 
-                self.play(FadeOutAndShift(my_dict["r"], DOWN))
+                self.play(FadeOut(my_dict["r"], shift=DOWN))
                 self.wait()
 
                 # you can also make a VDict from an existing dict of mobjects
