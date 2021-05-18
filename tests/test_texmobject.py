9c3d0ce987a1bd125f30b3b045b610b2ed0e0461
Naveen M K <naveen@syrusdark.website>
2021-05-18 14:01:32 +0530

    Case-Sensitive linux

--- tests/test_texmobject.py
+++ tests/test_texmobject.py
@@ -5,12 +5,12 @@ from manim import MathTex, SingleStringMathTex, Tex
 
 def test_MathTex(temp_media_dir):
     MathTex("a^2 + b^2 = c^2")
-    assert (temp_media_dir / "tex" / "3879f6b03bc495cd.svg").exists()
+    assert (temp_media_dir / "Tex" / "3879f6b03bc495cd.svg").exists()
 
 
 def test_SingleStringMathTex(temp_media_dir):
     SingleStringMathTex("test")
-    assert (temp_media_dir / "tex" / "79822967f1fa1935.svg").exists()
+    assert (temp_media_dir / "Tex" / "79822967f1fa1935.svg").exists()
 
 
 @pytest.mark.parametrize(
@@ -24,4 +24,4 @@ def test_double_braces_testing(temp_media_dir, text_input, length_sub):
 
 def test_tex(temp_media_dir):
     Tex("The horse does not eat cucumber salad.")
-    assert (temp_media_dir / "tex" / "983949cac5bdd272.svg").exists()
+    assert (temp_media_dir / "Tex" / "983949cac5bdd272.svg").exists()
