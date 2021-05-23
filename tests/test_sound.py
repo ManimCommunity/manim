c693c982874737388efe0230166fafc000bc6c5b
Naveen M K <naveen@syrusdark.website>
2021-05-23 11:31:25 +0530

    wave doesn't support pathlib

--- tests/test_sound.py
+++ tests/test_sound.py
@@ -8,7 +8,7 @@ from pathlib import Path
 def test_add_sound(tmpdir):
     # create sound file
     sound_loc = Path(tmpdir, "noise.wav")
-    f = wave.open(sound_loc, "w")
+    f = wave.open(str(sound_loc), "w")
     f.setparams((2, 2, 44100, 0, "NONE", "not compressed"))
     for _ in range(22050):  # half a second of sound
         packed_value = struct.pack("h", 14242)
