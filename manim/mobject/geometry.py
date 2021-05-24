03757148cc7748d33d9de4c727cb9b4ae1a04218
Philipp Imhof <52650214+PhilippImhof@users.noreply.github.com>
2021-05-24 20:11:14 +0200

    Added example

--- manim/mobject/geometry.py
+++ manim/mobject/geometry.py
@@ -2226,7 +2226,7 @@ class Rectangle(Polygon):
 
         class RectangleExample(Scene):
             def construct(self):
-                rect1 = Rectangle(width=4.0, height=2.0)
+                rect1 = Rectangle(width=4.0, height=2.0, grid_xstep=1.0, grid_ystep=0.5)
                 rect2 = Rectangle(width=1.0, height=4.0)
 
                 rects = Group(rect1,rect2).arrange(buff=1)
