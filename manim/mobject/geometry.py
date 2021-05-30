8ad22e73070886f44e2aacabcd630a36750f6794
Jerónimo Squartini <jerosquartini123@gmail.com>
2021-05-30 12:52:55 -0300

    Update geometry.py

--- manim/mobject/geometry.py
+++ manim/mobject/geometry.py
@@ -768,7 +768,7 @@ class Sector(AnnularSector):
 
 class Annulus(Circle):
     """Region between two concentric :class:`Circle`s.
-        
+
     Parameters
     ----------
     inner_radius : :class:`float`, optional
@@ -777,20 +777,20 @@ class Annulus(Circle):
         The radius of the outer :class:`Circle`.
     kwargs : Any
         Additional arguments to be passed to :class:`Annulus`
-    
+
     Examples
     --------
-    
+
     .. manim:: AnnulusExample
         :save_last_frame:
-        
+
         class AnnulusExample(Scene):
             def construct(self):
                 annulus_1 = Annulus(inner_radius=0.5, outer_radius=1).shift(UP)
                 annulus_2 = Annulus(inner_radius=0.3, outer_radius=0.6, color=RED).next_to(annulus_1, DOWN)
                 self.add(annulus_1, annulus_2)
     """
-    
+
     def __init__(
         self,
         inner_radius=1,
