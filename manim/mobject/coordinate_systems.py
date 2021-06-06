fc1d2f43cd7bd0965eab8f7d687d6b0d2faec671
Iced-Tea3 <83535735+Iced-Tea3@users.noreply.github.com>
2021-06-06 17:13:35 +0800

    applied blacken-docs
    accidentally bypassed precommit

--- manim/mobject/coordinate_systems.py
+++ manim/mobject/coordinate_systems.py
@@ -2060,13 +2060,15 @@ class ComplexPlane(NumberPlane):
             def construct(self):
                 plane = ComplexPlane().add_coordinates()
                 self.add(plane)
-                d1 = Dot(plane.n2p(2+1j),color=YELLOW)
-                d2 = Dot(plane.n2p(-3-2j),color=YELLOW)
-                label1 = MathTex("2+i").next_to(d1,UR,0.1)
-                label2 = MathTex("-3-2i").next_to(d2,UR,0.1)
+                d1 = Dot(plane.n2p(2 + 1j), color=YELLOW)
+                d2 = Dot(plane.n2p(-3 - 2j), color=YELLOW)
+                label1 = MathTex("2+i").next_to(d1, UR, 0.1)
+                label2 = MathTex("-3-2i").next_to(d2, UR, 0.1)
                 self.add(
-                    d1,label1,
-                    d2,label2,
+                    d1,
+                    label1,
+                    d2,
+                    label2,
                 )
 
     """
