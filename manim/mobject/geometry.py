55fdad59cdccf6ce7452aac3a56373820dc7560c
Ujjayanta <aprajit09041996@gmail.com>
2021-06-07 11:21:42 +0530

    Update geometry.py

--- manim/mobject/geometry.py
+++ manim/mobject/geometry.py
@@ -764,6 +764,38 @@ class AnnularSector(Arc):
 
 
 class Sector(AnnularSector):
+    """
+    
+    Examples
+    --------
+    
+    .. manim:: ExampleSector 
+        :save_last_frame:
+
+        class ExampleSector(Scene):
+            def construct(self):
+                sector = Sector(outer_radius=2, inner_radius=1)
+                sector2 = Sector(outer_radius=2.5, inner_radius=0.8).move_to([-3, 0, 0])
+                sector.set_color(RED)
+                sector2.set_color(PINK)
+                self.add(sector)
+                self.add(sector2)
+                self.wait(1)
+                sector.set_color(BLUE)
+                sector2.set_color(GREEN)
+                self.play(Rotate(sector, PI / 2))
+                self.play(Rotate(sector2, PI / 3))
+                self.wait(1)
+                sector.set_color(GREEN)
+                sector2.set_color(WHITE)
+                self.play(Rotate(sector, PI))
+                self.play(Rotate(sector2, PI / 6))
+                self.wait(1)
+                sector.set_color(YELLOW)
+                sector2.set_color(RED)
+                self.play(Rotate(sector, 2 * PI))
+                self.play(Rotate(sector2, 6 * PI))
+    """
     def __init__(self, outer_radius=1, inner_radius=0, **kwargs):
         super().__init__(inner_radius=inner_radius, outer_radius=outer_radius, **kwargs)
 
