ad1742a2f738fd856c2cfb2fc7181df775a1d85c
Ujjayanta <aprajit09041996@gmail.com>
2021-06-08 14:29:24 +0530

    Update manim/mobject/three_dimensions.py
    Co-authored-by: Laith Bahodi <70682032+hydrobeam@users.noreply.github.com>

--- manim/mobject/three_dimensions.py
+++ manim/mobject/three_dimensions.py
@@ -143,19 +143,26 @@ class Sphere(ParametricSurface):
 
         class ExampleSphere(ThreeDScene):
             def construct(self):
-                self.set_camera_orientation(phi = PI/6, theta = PI/6)
-                sphere1 = Sphere(center = (3,0,0),radius = 1,resolution = (30, 40),u_min = 0.001,u_max = PI - 0.001,v_min = 0,v_max = TAU)
+                self.set_camera_orientation(phi=PI / 6, theta=PI / 6)
+                sphere1 = Sphere(
+                    center=(3, 0, 0),
+                    radius=1,
+                    resolution=(30, 40),
+                    u_min=0.001,
+                    u_max=PI - 0.001,
+                    v_min=0,
+                    v_max=TAU
+                )
                 sphere1.set_color(RED)
                 self.add(sphere1)
-                sphere2 = Sphere(center=(0,1,0),radius=2,resolution=(40, 50))
-                sphere2.move_to((-1,-3,0))
+                sphere2 = Sphere(center=(0, 1, 0), radius=2, resolution=(40, 50))
+                sphere2.move_to((-1, -3, 0))
                 sphere2.set_color(GREEN)
                 self.add(sphere2)
-                sphere3 = Sphere(center=(0,1,0),radius=2,resolution=(40, 50))
-                sphere3.move_to((-1,2,0))
+                sphere3 = Sphere(center=(0, 1, 0), radius=2, resolution=(40, 50))
+                sphere3.move_to((-1, 2, 0))
                 sphere3.set_color(BLUE)
                 self.add(sphere3)
-
     """
 
     def __init__(
