3604c84772fa49a8fa3c00049355f18d8ab89172
ryan <ryanmccauley211@gmail.com>
2021-06-12 11:13:00 +0100

    Add tests for jupyter file output

--- tests/test_ipython_magic.py
+++ tests/test_ipython_magic.py
@@ -7,11 +7,23 @@ from manim.utils.ipython_magic import _generate_file_name
 def test_jupyter_file_naming():
     """Check the format of file names for jupyter"""
     scene_name = "SimpleScene"
-    expected_pattern = "[0-9a-zA-Z@_-]+[@_-]\d\d\d\d-\d\d-\d\d[@_-]\d\d-\d\d-\d\d"
-    with tempconfig({"scene_names": ["SimpleScene"]}):
+    expected_pattern = r"[0-9a-zA-Z@_-]+[@_-]\d\d\d\d-\d\d-\d\d[@_-]\d\d-\d\d-\d\d"
+    with tempconfig({"scene_names": [scene_name]}):
         file_name = _generate_file_name()
         match = re.match(expected_pattern, file_name)
         assert scene_name in file_name, (
             "Expected file to contain " + scene_name + " but got " + file_name
         )
         assert match, "file name does not match expected pattern " + expected_pattern
+
+
+def test_jupyter_file_output(tmp_path):
+    """Check the jupyter file naming is valid and can be created"""
+    scene_name = "SimpleScene"
+    with tempconfig({"scene_names": [scene_name]}):
+        file_name = _generate_file_name()
+        actual_path = tmp_path.with_name(file_name)
+        with open(actual_path, "w") as outfile:
+            outfile.write("")
+            assert actual_path.exists()
+            assert actual_path.is_file()
