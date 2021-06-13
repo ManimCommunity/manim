e326a7b6110b7706063cba10df299534a7e66081
Devin Neal <devin@eulertour.com>
2021-06-12 18:53:37 -0700

    Add dearpygui integration (#1553)
    * Add dearpygui integration
    
    * Rename MobjectTest -> GuiTest
    
    * flake8
    
    * Guard against dearpygui import
    
    * Add manim.gui
    
    * Guard against dearpygui import in gui.py
    
    * Add __init__.py
    
    * Add flag for GUI
    
    * Default to boolean
    
    * Fix problem when scene isn't specified
    
    * Add gui_location flag
    
    * poetry lock
    
    * Fix test
    
    * Update manim/gui/gui.py
    
    Co-authored-by: Laith Bahodi <70682032+hydrobeam@users.noreply.github.com>
    
    * Update manim/_config/default.cfg
    
    Co-authored-by: Laith Bahodi <70682032+hydrobeam@users.noreply.github.com>
    
    * Update manim/cli/render/global_options.py
    
    Co-authored-by: Laith Bahodi <70682032+hydrobeam@users.noreply.github.com>
    
    * Review comments
    
    * Update pyproject.toml
    
    Co-authored-by: Naveen M K <naveen@syrusdark.website>
    
    * Add __all__ to shader.py
    
    * Add utils import to opengl/__init__.py
    
    Co-authored-by: Laith Bahodi <70682032+hydrobeam@users.noreply.github.com>
    Co-authored-by: Darylgolden <darylgolden@gmail.com>
    Co-authored-by: Naveen M K <naveen@syrusdark.website>

--- manim/renderer/shaders/vertex_colors/vert.glsl
+++ manim/renderer/shaders/vertex_colors/vert.glsl
@@ -3,11 +3,11 @@
 in vec4 in_vert;
 in vec4 in_color;
 out vec4 v_color;
-uniform mat4 u_model_matrix;
-uniform mat4 u_view_matrix;
-uniform mat4 u_projection_matrix;
+uniform mat4 model_matrix;
+uniform mat4 view_matrix;
+uniform mat4 projection_matrix;
 
 void main() {
     v_color = in_color;
-    gl_Position = u_projection_matrix *  u_view_matrix *  u_model_matrix * in_vert;
+    gl_Position = projection_matrix *  view_matrix *  model_matrix * in_vert;
 }
