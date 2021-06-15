b1ef57df09db6eb0046764e01f6c54f476ec708a
MathInvariance <85428162+MathInvariance@users.noreply.github.com>
2021-06-15 12:18:32 -0500

    Update manim/mobject/shape_matchers.py
    Co-authored-by: Benjamin Hackl <devel@benjamin-hackl.at>

--- manim/mobject/shape_matchers.py
+++ manim/mobject/shape_matchers.py
@@ -21,7 +21,11 @@ class SurroundingRectangle(Rectangle):
         class SurroundingRectExample(Scene):
             def construct(self):
                 title = Title("A Quote from Newton")
-                quote = Text("If I have seen further than others, \\nit is by standing upon the shoulders of giants.", color=BLUE).scale(0.75)
+                quote = Text(
+                    r"If I have seen further than others, \n"
+                    r"it is by standing upon the shoulders of giants.",
+                    color=BLUE
+                ).scale(0.75)
                 box = SurroundingRectangle(quote, color=YELLOW, buff=MED_LARGE_BUFF)
                 self.add(title, box, quote)
 
