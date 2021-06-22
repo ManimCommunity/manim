a7b3f338167279449c83049f68a0d1809d4b9c76
MathInvariance <85428162+MathInvariance@users.noreply.github.com>
2021-06-22 09:53:29 -0500

    Update number_line.py

--- manim/mobject/number_line.py
+++ manim/mobject/number_line.py
@@ -76,22 +76,17 @@ class NumberLine(Line):
         class NumLineExample(Scene):
             def construct(self):
                 l0 = NumberLine(x_range=[-10, 10, 2], length=10, color=BLUE, include_numbers=True, label_direction=UP)
-                l0.shift(2 * UP)
 
                 l1 = NumberLine(x_range=[-10, 10, 2], unit_size=0.5, numbers_with_elongated_ticks=[-2, 4], include_numbers=True, number_scale_value=0.5)
-                l1.shift(UP)
                 num6 = l1.get_number_mobject(6).set_color(RED)
                 l1.add(num6)
 
-                l2 = NumberLine(
-                    x_range=[-2.5, 2.5 + 0.5, 0.5], length=12, decimal_number_config={"num_decimal_places": 2}, unit_size=1.25, include_numbers=True
-                )
-                l2.shift(DOWN)
+                l2 = NumberLine(x_range=[-2.5, 2.5 + 0.5, 0.5], length=12, decimal_number_config={"num_decimal_places": 2}, include_numbers=True)
 
                 l3 = NumberLine(x_range=[-5, 5 + 1, 1], length=6, include_tip=True, include_numbers=True, rotation=10 * DEGREES)
-                l3.shift(2.75 * DOWN)
 
-                self.add(l0, l1, num6, l2, l3)
+                line_group = VGroup(l0, l1, l2, l3).arrange(DOWN, buff=1)
+                self.add(line_group)
 
     Returns
     -------
