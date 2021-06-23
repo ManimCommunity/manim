4ec3877f307d48cfc17a112daca0c4eebcbf930a
Oliver <44864613+PhotonSpheres@users.noreply.github.com>
2021-06-23 16:21:21 +0200

    black/isort/flake8

--- tests/test_graphical_units/test_tabular.py
+++ tests/test_graphical_units/test_tabular.py
@@ -9,44 +9,41 @@ from ..utils.testing_utils import get_scenes_to_test
 class TabularTest(Scene):
     def construct(self):
         t = Tabular(
-            [["1", "2"],
-            ["3","4"]],
+            [["1", "2"], ["3", "4"]],
             row_labels=[Text("R1"), Text("R2")],
             col_labels=[Text("C1"), Text("C2")],
             top_left_entry=Text("TOP"),
-            include_outer_lines=True
+            include_outer_lines=True,
         )
-        t.add(t.get_highlighted_cell((2,2)))
+        t.add(t.get_highlighted_cell((2, 2)))
         self.add(t)
-    
+
+
 class MathTabularTest(Scene):
     def construct(self):
-        t = MathTabular(
-            [[1, 2],
-            [3,4]]
-        )
+        t = MathTabular([[1, 2], [3, 4]])
         self.add(t)
+
 
 class MobjectTabularTest(Scene):
     def construct(self):
         a = Circle().scale(0.5)
-        t = MobjectTabular(
-            [[a.copy(), a.copy()],
-            [a.copy(), a.copy()]]
-        )
+        t = MobjectTabular([[a.copy(), a.copy()], [a.copy(), a.copy()]])
         self.add(t)
+
 
 class IntegerTabularTest(Scene):
     def construct(self):
         t = IntegerTabular(
-            np.arange(1,21).reshape(5,4),
+            np.arange(1, 21).reshape(5, 4),
         )
         self.add(t)
+
 
 class DecimalTabularTest(Scene):
     def construct(self):
         t = DecimalTabular(
-            np.linspace(0,0.9,20).reshape(5,4),
+            np.linspace(0, 0.9, 20).reshape(5, 4),
         )
         self.add(t)
 
