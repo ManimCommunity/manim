d6571a32c9acb6a6fda9bac24452867aad35e000
hydrobeam <laithbahodi@gmail.com>
2021-06-24 16:39:10 -0400

    adjust docs and make remover work

--- manim/animation/specialized.py
+++ manim/animation/specialized.py
@@ -14,25 +14,25 @@ class Broadcast(LaggedStart):
 
     Parameters
     ----------
-    mobject
+    mobject :
         The mobject to be broadcast.
-    focal_point
-        The point from which the mobjects emerge, by default ORIGIN.
-    n_mobs
-        The number of mobjects that originate from the focal point, by default 5.
-    initial_opacity
+    focal_point :
+        The center of the broadcast, by default ORIGIN.
+    n_mobs :
+        The number of mobjects that emerge from the focal point, by default 5.
+    initial_opacity :
         The starting stroke opacity of the mobjects emitted from the broadcast, by default 1.
-    final_opacity
+    final_opacity :
         The final stroke opacity of the mobjects emitted from the broadcast, by default 0.
-    initial_width
+    initial_width :
         The initial width of the mobjects, by default 0.0.
-    remover
-        Whether the given mobject should be removed from the scene after the animation, by default True.
-    lag_ratio
+    remover :
+        Whether the mobjects should be removed from the scene after the animation, by default True.
+    lag_ratio :
         The time between each iteration of the mobject, by default 0.2.
-    run_time
+    run_time :
         The total duration of the animation, by default 3.
-    kwargs
+    kwargs :
         Additional arguments to be passed to :class:`~.LaggedStart`.
 
     Examples
@@ -72,8 +72,6 @@ class Broadcast(LaggedStart):
             mob.set_stroke(opacity=self.final_opacity).move_to(self.focal_point)
             mob.save_state()
             mob.set_width(self.initial_width).set_stroke(opacity=self.initial_opacity)
-            anims.append(Restore(mob))
+            anims.append(Restore(mob, remover=remover))
 
-        super().__init__(
-            *anims, run_time=run_time, lag_ratio=lag_ratio, remover=remover, **kwargs
-        )
+        super().__init__(*anims, run_time=run_time, lag_ratio=lag_ratio, **kwargs)
