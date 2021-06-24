4221fa9cba1b3f6deafcb723c479c09ea469bf42
Jason Villanueva <a@jsonvillanueva.com>
2021-06-24 07:40:41 -0700

    Fix incorrect merge in Mobject

--- manim/mobject/mobject.py
+++ manim/mobject/mobject.py
@@ -94,7 +94,7 @@ class Mobject:
         cls._add_intrinsic_animation_overrides()
 
     def __init__(self, color=WHITE, name=None, dim=3, target=None, z_index=0):
-        self.color = Color(color)
+        self.color = Color(color) if color else None
         self.name = self.__class__.__name__ if name is None else name
         self.dim = dim
         self.target = target
