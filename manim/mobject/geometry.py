91f359f264f0d49964d17ef1585835ca4ee6510f
MathInvariance <85428162+MathInvariance@users.noreply.github.com>
2021-06-29 05:20:58 -0500

    Updated imports in `geometry.py` and added example to :class:`~.Arrow` (#1687)
    * Updated imports in `geometry.py` and added example to :class:`~.Arrow`
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * Update manim/mobject/geometry.py
    
    Co-authored-by: Laith Bahodi <70682032+hydrobeam@users.noreply.github.com>
    
    * Update manim/mobject/geometry.py
    
    Co-authored-by: Laith Bahodi <70682032+hydrobeam@users.noreply.github.com>
    
    * Update manim/mobject/geometry.py
    
    Co-authored-by: Laith Bahodi <70682032+hydrobeam@users.noreply.github.com>
    
    * Update manim/mobject/geometry.py
    
    Co-authored-by: Laith Bahodi <70682032+hydrobeam@users.noreply.github.com>
    
    * Update manim/mobject/geometry.py
    
    Co-authored-by: Laith Bahodi <70682032+hydrobeam@users.noreply.github.com>
    
    * Update manim/mobject/geometry.py
    
    Co-authored-by: Laith Bahodi <70682032+hydrobeam@users.noreply.github.com>
    
    * Update manim/mobject/geometry.py
    
    Co-authored-by: Laith Bahodi <70682032+hydrobeam@users.noreply.github.com>
    
    * Update manim/mobject/geometry.py
    
    Co-authored-by: Laith Bahodi <70682032+hydrobeam@users.noreply.github.com>
    
    * Update geometry.py
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * Update manim/mobject/geometry.py
    
    Co-authored-by: Laith Bahodi <70682032+hydrobeam@users.noreply.github.com>
    
    * Update manim/mobject/geometry.py
    
    Co-authored-by: Laith Bahodi <70682032+hydrobeam@users.noreply.github.com>
    
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>
    Co-authored-by: Laith Bahodi <70682032+hydrobeam@users.noreply.github.com>
    Co-authored-by: Benjamin Hackl <devel@benjamin-hackl.at>

--- manim/mobject/geometry.py
+++ manim/mobject/geometry.py
@@ -59,6 +59,10 @@ __all__ = [
     "Cutout",
     "Angle",
     "RightAngle",
+    "ArrowCircleFilledTip",
+    "ArrowCircleTip",
+    "ArrowSquareTip",
+    "ArrowSquareFilledTip",
 ]
 
 import math
@@ -1263,6 +1267,46 @@ class Arrow(Line):
                 g3 = Group(arrow_5, arrow_6)
 
                 self.add(Group(g1, g2, g3).arrange(buff=2))
+
+
+    .. manim:: ArrowExample
+        :save_last_frame:
+
+        class ArrowExample(Scene):
+            def construct(self):
+                left_group = VGroup()
+                # As buff increases, the size of the arrow decreases.
+                for buff in np.arange(0, 2.2, 0.45):
+                    left_group += Arrow(buff=buff, start=2 * LEFT, end=2 * RIGHT)
+                # Required to arrange arrows.
+                left_group.arrange(DOWN)
+                left_group.move_to(4 * LEFT)
+
+                middle_group = VGroup()
+                # As max_stroke_width_to_length_ratio gets bigger,
+                # the width of stroke increases.
+                for i in np.arange(0, 5, 0.5):
+                    middle_group += Arrow(max_stroke_width_to_length_ratio=i)
+                middle_group.arrange(DOWN)
+
+                UR_group = VGroup()
+                # As max_tip_length_to_length_ratio increases,
+                # the length of the tip increases.
+                for i in np.arange(0, 0.3, 0.1):
+                    UR_group += Arrow(max_tip_length_to_length_ratio=i)
+                UR_group.arrange(DOWN)
+                UR_group.move_to(4 * RIGHT + 2 * UP)
+
+                DR_group = VGroup()
+                DR_group += Arrow(start=LEFT, end=RIGHT, color=BLUE, tip_shape=ArrowSquareTip)
+                DR_group += Arrow(start=LEFT, end=RIGHT, color=BLUE, tip_shape=ArrowSquareFilledTip)
+                DR_group += Arrow(start=LEFT, end=RIGHT, color=YELLOW, tip_shape=ArrowCircleTip)
+                DR_group += Arrow(start=LEFT, end=RIGHT, color=YELLOW, tip_shape=ArrowCircleFilledTip)
+                DR_group.arrange(DOWN)
+                DR_group.move_to(4 * RIGHT + 2 * DOWN)
+
+                self.add(left_group, middle_group, UR_group, DR_group)
+
 
     See Also
     --------
