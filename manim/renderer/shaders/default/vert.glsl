0149480723d63cc2f7fc7d9cd14677f612c71b67
Devin Neal <devin@eulertour.com>
2021-06-30 00:49:55 -0700

    Rewrite stroke and fill shaders (#1716)
    Rewrite vectorized mobject shaders to be compatible with transformation matrices.

--- manim/renderer/shaders/default/vert.glsl
+++ manim/renderer/shaders/default/vert.glsl
@@ -6,5 +6,5 @@ uniform mat4 u_view_matrix;
 uniform mat4 u_projection_matrix;
 
 void main() {
-    gl_Position = u_projection_matrix *  u_view_matrix *  u_model_matrix * vec4(in_vert, 1);
+    gl_Position = u_projection_matrix * u_view_matrix * u_model_matrix * vec4(in_vert, 1.0);
 }
