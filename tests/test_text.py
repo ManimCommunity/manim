6e305214b24fee732636cc3a6d2451ee80d40d0a
Led Me Explain <ledmeexplain@gmail.com>
2021-07-07 13:39:37 -0500

    test: fixed test_tex.py so it works with Ubuntu and MacOS too

--- tests/test_text.py
+++ tests/test_text.py
@@ -8,9 +8,9 @@ def test_t2c():
     text1 = Text("fl ligature", t2c={"f": RED, "e": BLUE}, disable_ligatures=False)
     # If disable_ligatures is set to False, submobjects don't include blank spaces
     assert text1.submobjects[0].stroke_color == RED
-    assert text1.submobjects[9].stroke_color == BLUE
+    assert text1.submobjects[-1].stroke_color == BLUE
 
     text2 = Text("fl ligature", t2c={"f": RED, "e": BLUE}, disable_ligatures=True)
     # If disable_ligatures is set to True, submobjects include blank spaces
     assert text2.submobjects[0].stroke_color == RED
-    assert text2.submobjects[10].stroke_color == BLUE
+    assert text2.submobjects[-1].stroke_color == BLUE
