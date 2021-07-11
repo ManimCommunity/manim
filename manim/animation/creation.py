e55b39897dc9ef1018f495077014956c38dd8a77
TRoboto <m4bh@hotmail.com>
2021-07-11 18:43:07 +0300

    Fix write reverse with OpenGL

--- manim/animation/creation.py
+++ manim/animation/creation.py
@@ -328,9 +328,9 @@ class Write(DrawBorderThenFill):
         super().begin()
 
     def finish(self) -> None:
+        super().finish()
         if self.reverse:
             self.reverse_submobjects()
-        super().finish()
 
 
 class Unwrite(Write):
