352f275cf9306906a50500703254738a0f50fd5d
Led Me Explain <ledmeexplain@gmail.com>
2021-07-11 22:52:49 -0500

    fix: set_z_index little changes in Example

--- manim/mobject/mobject.py
+++ manim/mobject/mobject.py
@@ -2697,18 +2697,18 @@ class Mobject:
         .. manim:: SetZIndex
             :save_last_frame:
 
-            class SetZIndex(Scene):
-                def construct(self):
-                    text = Text('z_index = 3', color = RED).set_z_index(3)
-                    square = Square().set_z_index(2)
-                    tex = Tex('z_index = 1').set_z_index(1)
-                    circle = Circle() # z_index = 0
+        class SetZIndex(Scene):
+            def construct(self):
+                text = Text('z_index = 3', color = PURE_RED).shift(UP).set_z_index(3)
+                square = Square(2, fill_opacity=1).set_z_index(2)
+                tex = Tex(r'zIndex = 1', color = PURE_BLUE).shift(DOWN).set_z_index(1)
+                circle = Circle(radius = 1.7, color = GREEN, fill_opacity = 1) # z_index = 0
 
-                    # Displaying order is now defined by z_index values
-                    self.add(text)
-                    self.add(square)
-                    self.add(tex)
-                    self.add(circle)
+                # Displaying order is now defined by z_index values
+                self.add(text)
+                self.add(square)
+                self.add(tex)
+                self.add(circle)
         """
         if family:
             for submob in self.submobjects:
