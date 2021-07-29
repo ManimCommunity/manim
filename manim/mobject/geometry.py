bc93a2e99afb3f77c18a0c7e56d148aca10683f8
sparshg <43041139+sparshg@users.noreply.github.com>
2021-07-29 21:57:53 +0530

    DashedVMobject reworked (#1823)
    * fix dashedVMobject
    
    * add test
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * also change opengl dashed vmobject
    
    * fix dashedline
    
    * small fix
    
    * remove dash_spacing
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * gitignore
    
    * remove whitespace
    
    * undo gitignore
    
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>

--- manim/mobject/geometry.py
+++ manim/mobject/geometry.py
@@ -1089,10 +1089,8 @@ class DashedLine(Line):
         Arguments to be passed to :class:`Line`
     dash_length : :class:`float`, optional
         The length of each individual dash of the line.
-    dash_spacing : Optional[:class:`float`]
-        The spacing between the dashes.
-    positive_space_ratio : :class:`float`, optional
-        The ratio of empty space to dash space. Range of 0-1.
+    dashed_ratio : :class:`float`, optional
+        The ratio of dash space to empty space. Range of 0-1.
     kwargs : Any
         Additional arguments to be passed to :class:`Line`
 
@@ -1107,8 +1105,8 @@ class DashedLine(Line):
                 dashed_1 = DashedLine(config.left_side, config.right_side, dash_length=2.0).shift(UP*2)
                 # normal
                 dashed_2 = DashedLine(config.left_side, config.right_side)
-                # positive_space_ratio decreased
-                dashed_3 = DashedLine(config.left_side, config.right_side, positive_space_ratio=0.1).shift(DOWN*2)
+                # dashed_ratio decreased
+                dashed_3 = DashedLine(config.left_side, config.right_side, dashed_ratio=0.1).shift(DOWN*2)
                 self.add(dashed_1, dashed_2, dashed_3)
 
     See Also
@@ -1120,18 +1118,16 @@ class DashedLine(Line):
         self,
         *args,
         dash_length=DEFAULT_DASH_LENGTH,
-        dash_spacing=None,
-        positive_space_ratio=0.5,
+        dashed_ratio=0.5,
         **kwargs,
     ):
         self.dash_length = dash_length
-        self.dash_spacing = (dash_spacing,)
-        self.positive_space_ratio = positive_space_ratio
+        self.dashed_ratio = dashed_ratio
         super().__init__(*args, **kwargs)
         dashes = DashedVMobject(
             self,
             num_dashes=self.calculate_num_dashes(),
-            positive_space_ratio=positive_space_ratio,
+            dashed_ratio=dashed_ratio,
         )
         self.clear_points()
         self.add(*dashes)
@@ -1147,14 +1143,10 @@ class DashedLine(Line):
             20
         """
 
-        try:
-            full_length = self.dash_length / self.positive_space_ratio
-            return int(np.ceil(self.get_length() / full_length))
-        except ZeroDivisionError:
-            return 1
-
-    def calculate_positive_space_ratio(self):
-        return fdiv(self.dash_length, self.dash_length + self.dash_spacing)
+        # Minimum number of dashes has to be 2
+        return max(
+            2, int(np.ceil((self.get_length() / self.dash_length) * self.dashed_ratio))
+        )
 
     def get_start(self) -> np.ndarray:
         """Returns the start point of the line.
@@ -1180,7 +1172,7 @@ class DashedLine(Line):
         ::
 
             >>> DashedLine().get_end()
-            array([0.99871795, 0.        , 0.        ])
+            array([1., 0., 0.])
         """
 
         if len(self.submobjects) > 0:
@@ -1209,7 +1201,7 @@ class DashedLine(Line):
         ::
 
             >>> DashedLine().get_last_handle()
-            array([0.98205128, 0.        , 0.        ])
+            array([0.98333333, 0.        , 0.        ])
         """
 
         return self.submobjects[-1].get_points()[-2]
