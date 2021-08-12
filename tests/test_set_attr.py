9f30a79d1d4aa52bb64a6257186f7fcacc47dec9
hydrobeam <laithbahodi@gmail.com>
2021-08-12 05:19:25 -0400

    fix improper usage of .all() in test

--- tests/test_set_attr.py
+++ tests/test_set_attr.py
@@ -8,20 +8,19 @@ from manim.opengl import OpenGLSquare
 def test_Data():
     a = OpenGLSquare().move_to(RIGHT)
     data_bb = a.data["bounding_box"]
-    assert (
-        data_bb.all()
-        == np.array([[0.0, -1.0, 0.0], [1.0, 0.0, 0.0], [2.0, 1.0, 0.0]]).all()
+    assert np.array_equal(
+        data_bb, np.array([[0.0, -1.0, 0.0], [1.0, 0.0, 0.0], [2.0, 1.0, 0.0]])
     )
 
     # test that calling the attribute equals calling it from self.data
-    assert a.bounding_box.all() == data_bb.all()
+    assert np.array_equal(a.bounding_box, data_bb)
 
     # test that the array can be indexed
-    assert (
-        a.bounding_box[1].all()
-        == np.array(
+    assert np.array_equal(
+        a.bounding_box[1],
+        np.array(
             [1.0, 0.0, 0.0],
-        ).all()
+        ),
     )
 
     # test that a value can be set
@@ -30,9 +29,8 @@ def test_Data():
     # test that both the attr and self.data arrays match after adjusting a value
 
     data_bb = a.data["bounding_box"]
-    assert (
-        data_bb.all()
-        == np.array([[0.0, -1.0, 0.0], [300.0, 300.0, 300.0], [2.0, 1.0, 0.0]]).all()
+    assert np.array_equal(
+        data_bb, np.array([[0.0, -1.0, 0.0], [300.0, 300.0, 300.0], [2.0, 1.0, 0.0]])
     )
 
-    assert a.bounding_box.all() == data_bb.all()
+    assert np.array_equal(a.bounding_box, data_bb)
