83306c4a88e467649e6d9890ce817d0563d7f3db
hydrobeam <laithbahodi@gmail.com>
2021-08-20 16:45:07 -0400

    example for riemann rectangles

--- manim/mobject/coordinate_systems.py
+++ manim/mobject/coordinate_systems.py
@@ -670,8 +670,7 @@ class CoordinateSystem:
         dot: bool = False,
         dot_config: Optional[dict] = None,
     ) -> Mobject:
-        """Creates a properly positioned label for the passed graph,
-        styled with parameters and an optional dot.
+        """Creates a properly positioned label for the passed graph, with an optional dot.
 
         Examples
         --------
@@ -759,8 +758,43 @@ class CoordinateSystem:
         blend: bool = False,
         width_scale_factor: float = 1.001,
     ) -> VGroup:
-        """This method returns the :class:`~.VGroup` of the Riemann Rectangles for
+        """Generates a :class:`~.VGroup` of the Riemann Rectangles for
         a particular curve.
+
+        .. manim:: GetRiemannRectanglesExample
+            :save_last_frame:
+
+            class GetRiemannRectanglesExample(Scene):
+                def construct(self):
+                    ax = Axes(y_range=[-2, 10])
+                    quadratic = ax.get_graph(lambda x: 0.5 * x ** 2 - 0.5)
+
+                    # the rectangles are constructed from their top right corner.
+                    # passing an iterable to the color parameter produces a gradient
+                    rects_right = ax.get_riemann_rectangles(
+                        quadratic,
+                        x_range=[-4, -3],
+                        dx=0.25,
+                        color=(TEAL, BLUE_B, DARK_BLUE),
+                        input_sample_type="right",
+                    )
+
+                    # the colour of rectangles below the x-axis is inverted due to show_signed_area
+                    rects_left = ax.get_riemann_rectangles(
+                        quadratic, x_range=[-1.5, 1.5], dx=0.15, color=YELLOW
+                    )
+
+                    bounding_line = ax.get_graph(lambda x: 1.5 * x, color=BLUE_B, x_range=[3.3, 6])
+                    bounded_rects = ax.get_riemann_rectangles(
+                        bounding_line,
+                        bounded_graph=quadratic,
+                        dx=0.15,
+                        x_range=[4, 5],
+                        show_signed_area=False,
+                        color=(MAROON_A, RED_B, PURPLE_D),
+                    )
+
+                    self.add(ax, bounding_line, quadratic, rects_right, rects_left, bounded_rects)
 
         Parameters
         ----------
