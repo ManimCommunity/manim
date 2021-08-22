8b7a815323c8a717321f9e488dbef21870ad0e05
hydrobeam <laithbahodi@gmail.com>
2021-08-22 02:36:20 -0400

    more test errors

--- tests/test_coordinate_system.py
+++ tests/test_coordinate_system.py
@@ -93,7 +93,7 @@ def test_point_to_coords():
 
     # get the coordinates of the circle with respect to the axes
     coords = np.around(ax.point_to_coords(circ.get_right()), decimals=4)
-    assert np.array_equal(coords, (7.0833, 2.6667, 0))
+    assert np.array_equal(coords, (7.0833, 2.6667))
 
 
 def test_coords_to_point():
@@ -110,4 +110,4 @@ def test_input_to_graph_point():
 
     # move a square to PI on the cosine curve.
     position = np.around(ax.input_to_graph_point(x=PI, graph=curve), decimals=4)
-    assert np.array_equal(position, (2.6980, -0.75, 0))
+    assert np.array_equal(position, (2.6928, -0.75, 0))
