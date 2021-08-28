4f7861e80053dd932e189ae4a18a4291a3be3ffc
Ryan McCauley <32387857+ryanmccauley211@users.noreply.github.com>
2021-08-28 08:23:48 +0100

    Update manim/mobject/types/opengl_point_cloud_mobject.py
    Co-authored-by: Laith Bahodi <70682032+hydrobeam@users.noreply.github.com>

--- manim/mobject/types/opengl_point_cloud_mobject.py
+++ manim/mobject/types/opengl_point_cloud_mobject.py
@@ -75,7 +75,7 @@ class OpenGLPMobject(OpenGLMobject):
         return self
 
     def set_color_by_gradient(self, *colors):
-        self.data["rgbas"] = np.array(
+        self.rgbas = np.array(
             list(map(color_to_rgba, color_gradient(*colors, self.get_num_points())))
         )
         return self
