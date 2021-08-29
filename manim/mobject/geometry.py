67b1e26da2b9a81d8e77fd4bd7ad23d46c8a3aad
GameDungeon <gamedungeon@protonmail.com>
2021-08-28 21:38:17 -0500

    Docs Fix

--- manim/mobject/geometry.py
+++ manim/mobject/geometry.py
@@ -2938,7 +2938,7 @@ class Angle(VMobject, metaclass=ConvertToOpenGL):
                 norm = l1.get_length()
                 a1 = Angle(l1, l2, other_angle=True, radius=norm - 0.5).set_color(GREEN)
                 a2 = Angle(l1, l2, other_angle=True, radius=norm).set_color(GREEN)
-                q1 = a1points #  save all coordinates of points of angle a1
+                q1 = a1.points #  save all coordinates of points of angle a1
                 q2 = a2.reverse_direction().points  #  save all coordinates of points of angle a1 (in reversed direction)
                 pnts = np.concatenate([q1, q2, q1[0].reshape(1, 3)])  # adds points and ensures that path starts and ends at same point
                 mfill = VMobject().set_color(ORANGE)
