9735820a2883e2d649aa7d7512165b927fa248fe
hydrobeam <laithbahodi@gmail.com>
2021-08-30 14:51:04 -0400

    add tests

---
+++ tests/test_numbers.py
@@ -0,0 +1,20 @@
+import pytest
+
+from manim.mobject.numbers import DecimalNumber
+
+
+def test_font_size():
+    """Test that DecimalNumber returns the correct font_size value
+    after being scaled."""
+    num = DecimalNumber(0).scale(0.3)
+
+    assert round(num.font_size, 5) == 14.4
+
+
+def test_set_value_size():
+    """Test that the size of DecimalNumber after set_value is correct."""
+    num = DecimalNumber(0).scale(0.3)
+    test_num = num.copy()
+    num.set_value(0)
+
+    assert num.height == test_num.height
