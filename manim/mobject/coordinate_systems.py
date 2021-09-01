5bd6cd875c20efc89984e1ac19b4d20ec9f8f205
Léo Géré <leo.gere@inrae.fr>
2021-09-01 12:44:01 +0200

    Fix the bounds of the binary search

--- manim/mobject/coordinate_systems.py
+++ manim/mobject/coordinate_systems.py
@@ -743,8 +743,8 @@ class CoordinateSystem:
                     0
                 ],
                 target=x,
-                lower_bound=self.x_range[0],
-                upper_bound=self.x_range[1],
+                lower_bound=0,
+                upper_bound=1,
             )
             if alpha is not None:
                 return graph.point_from_proportion(alpha)
