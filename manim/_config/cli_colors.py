cc17a2af5ccc6061fd4e53fd993493ed72bae9c8
Jason Villanueva <a@jsonvillanueva.com>
2021-09-05 09:23:42 -0700

    Adjusted logic for overriding theme

--- manim/_config/cli_colors.py
+++ manim/_config/cli_colors.py
@@ -5,7 +5,7 @@ from cloup import Context, HelpFormatter, HelpTheme, Style
 from manim.constants import HELP_OPTIONS
 
 
-def parse_cli_ctx(parser: configparser.ConfigParser):
+def parse_cli_ctx(parser: configparser.ConfigParser) -> Context:
     formatter_settings = {
         "indent_increment": int(parser["indent_increment"]),
         "width": int(parser["width"]),
@@ -14,23 +14,22 @@ def parse_cli_ctx(parser: configparser.ConfigParser):
         "col_spacing": int(parser["col_spacing"]),
         "row_sep": parser["row_sep"] if parser["row_sep"] else None,
     }
-    theme_settings = {
-        "invoked_command": Style(parser["invoked_command"])
-        if parser["invoked_command"]
-        else None,
-        "command_help": Style(parser["command_help"])
-        if parser["command_help"]
-        else None,
-        "heading": Style(parser["heading"]) if parser["heading"] else None,
-        "constraint": Style(parser["constraint"]) if parser["constraint"] else None,
-        "section_help": Style(parser["section_help"])
-        if parser["section_help"]
-        else None,
-        "col1": Style(parser["col1"]) if parser["col1"] else None,
-        "col2": Style(parser["col2"]) if parser["col2"] else None,
+    theme_settings = {}
+    theme_keys = {
+        "command_help",
+        "invoked_command",
+        "heading",
+        "constraint",
+        "section_help",
+        "col1",
+        "col2",
     }
+    for k, v in parser.items():
+        if k in theme_keys and v:
+            theme_settings.update({k: Style(v)})
+
     formatter = {}
-    theme = parser["theme"]
+    theme = parser["theme"] if parser["theme"] else None
     if theme is None:
         formatter = HelpFormatter().settings(
             theme=HelpTheme(**theme_settings), **formatter_settings
@@ -44,11 +43,10 @@ def parse_cli_ctx(parser: configparser.ConfigParser):
             theme=HelpTheme.light().with_(**theme_settings), **formatter_settings
         )
 
-    ctx_settings = Context.settings(
+    return Context.settings(
         align_option_groups=parser["align_option_groups"].lower() == "true",
         align_sections=parser["align_sections"].lower() == "true",
         show_constraints=True,
         help_option_names=HELP_OPTIONS,
         formatter_settings=formatter,
     )
-    return ctx_settings
