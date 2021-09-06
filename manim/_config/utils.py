87cdbffcdd6e3d5f195fc20eab514e34794f60a7
icedcoffeeee <83535735+icedcoffeeee@users.noreply.github.com>
2021-09-06 13:33:28 +0800

    Updated documentation

--- manim/_config/utils.py
+++ manim/_config/utils.py
@@ -239,18 +239,20 @@ class ManimConfig(MutableMapping):
     Themes
     ------
     There are options to set manim to follow a specific color scheme. You need to
-    make a ``.cfg`` file with which contains the line:
+    make a ``.cfg`` file with which contains the lines:
+
     .. code-block:: cfg
 
         [CLI]
 
         theme = light_mode
 
-    The options of the themes include
-    - dark_mode (default)
-    - light_mode (white background and black mobjects)
-    - sepia (brown-esque)
-    - seagreen
+    The options of the themes include:
+
+    * dark_mode (default)
+    * light_mode (white background and black mobjects)
+    * sepia (brown-esque)
+    * seagreen
 
     .. important::
         The theme cannot be stated from within your manim script.
@@ -267,9 +269,9 @@ class ManimConfig(MutableMapping):
         The theme changes the definition of some colors, namely the greyscale
         family. ``WHITE`` will refer to the newly set "mobject_color" and
         ``BLACK`` will refer to the new "background_color". Every grey in between
-        (GREY_A, GREY_B and so on) will be a gradient between the two ``WHITE``
-        and ``BLACK``. If you wish to refer to the pure colors, you may pass
-        their hex codes instead.
+        (``GREY_A``, ``GREY_B`` and so on) will be a gradient between the two
+        ``WHITE`` and ``BLACK``. If you wish to refer to the pure colors,
+        you may pass their hex codes instead.
 
     """
 
