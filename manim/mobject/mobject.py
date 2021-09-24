7a2f0ff18a208df92c1527291ac510c707169056
Benjamin Hackl <devel@benjamin-hackl.at>
2021-09-24 19:25:03 +0200

    actually fix doctest 🙄

--- manim/mobject/mobject.py
+++ manim/mobject/mobject.py
@@ -197,7 +197,7 @@ class Mobject:
 
         ::
 
-            >>> from manim import Square
+            >>> from manim import Square, GREEN
             >>> Square.change_default(color=GREEN, fill_opacity=0.25)
             >>> s = Square(); s.color, s.fill_opacity
             (<Color #83C167>, 0.25)
