c00a6f0fc076014552c1687cf03c4302037128ec
icedcoffeeee <83535735+icedcoffeeee@users.noreply.github.com>
2021-09-25 21:18:07 +0800

    Make spherical_to_cartesian accept a sequence

--- manim/utils/space_ops.py
+++ manim/utils/space_ops.py
@@ -772,19 +772,22 @@ def cartesian_to_spherical(vec: Sequence[float]) -> Tuple[float, float, float]:
     return r, phi, theta
 
 
-def spherical_to_cartesian(r: float, theta: float, phi: float) -> np.ndarray:
+def spherical_to_cartesian(spherical: Sequence[float]) -> np.ndarray:
     """Returns a numpy array ``[x, y, z]`` based on the spherical
     coordinates given.
 
     Parameters
     ----------
-    r
-        The distance between the point and the origin.
-    theta
-        The azimuthal angle of the point to the positive x-axis.
-    phi
-        The vertical angle of the point to the positive z-axis.
+    spherical
+        A list of three floats that correspond to the following:
+
+        r - The distance between the point and the origin.
+
+        theta - The azimuthal angle of the point to the positive x-axis.
+
+        phi - The vertical angle of the point to the positive z-axis.
     """
+    r, theta, phi = spherical
     return np.array(
         [
             r * np.cos(theta) * np.sin(phi),
