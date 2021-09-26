25ba8176139b2d08f0bf7f2f944f155d7ce747a5
icedcoffeeee <83535735+icedcoffeeee@users.noreply.github.com>
2021-09-26 13:42:52 +0800

    corrected rounding

--- tests/test_space_ops.py
+++ tests/test_space_ops.py
@@ -6,5 +6,8 @@ from manim import cartesian_to_spherical, spherical_to_cartesian
 def test_polar_coords():
     a = np.array([1, 1, 0])
     b = (2, np.pi / 2, np.pi / 2)
-    assert cartesian_to_spherical(a) == (2 ** 0.5, np.pi / 2, np.pi / 4)
-    assert all(np.round(spherical_to_cartesian(b), 6) == np.array([0, 2, 0]))
+    assert all(
+        np.round(cartesian_to_spherical(a), 4)
+        == np.round([2 ** 0.5, np.pi / 2, np.pi / 4], 4),
+    )
+    assert all(np.round(spherical_to_cartesian(b), 4) == np.array([0, 2, 0]))
