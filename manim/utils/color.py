2e11e166d833b8878e4d29e93181078084049a50
GameDungeon <60719255+GameDungeon@users.noreply.github.com>
2021-10-02 14:21:51 -0500

    oops 2

--- manim/utils/color.py
+++ manim/utils/color.py
@@ -478,7 +478,7 @@ def rgb_to_hex(rgb: Iterable[float]) -> str:
 def hex_to_rgb(hex_code: str) -> np.ndarray:
     hex_part = hex_code[1:]
     if len(hex_part) == 3:
-        hex_part: str = "".join([2 * c for c in hex_part])
+        hex_part = "".join([2 * c for c in hex_part])
     return np.array([int(hex_part[i : i + 2], 16) / 255 for i in range(0, 6, 2)])
 
 
