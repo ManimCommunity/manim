f2fc2e946a85515465cc6cc8f734f588e2d1200c
Gustav-Rixon <Gustav.rixon@gmail.com>
2021-10-06 11:14:49 -0400

    feature request #1056
    Added auto_zoom to moving_camera.
    Fixed is_in_frame.

---
+++ tests/test_auto_zoom.py
@@ -0,0 +1,21 @@
+from manim import *
+
+from .simple_zoom_scene import (
+    test_zoom_pan_to_center,
+)
+
+def test_zoom():
+
+    s1 = Square()
+    s1.set_x(-10)
+    s2 = Square()
+    s2.set_x(10)
+
+    scene = test_zoom_pan_to_center()
+
+    scene.construct()
+    
+    assert(
+        scene.camera.frame_width == abs(s1.get_left()[0] - s2.get_right()[0])
+        and scene.camera.frame.get_center()[0] == (abs(s1.get_center()[0] + s2.get_center()[0]) / 2)
+    )
