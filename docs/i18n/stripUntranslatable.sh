1cd24892e58e76486c40a7130baed0eff889d93d
Darylgolden <darylgolden@gmail.com>
2021-10-15 17:51:09 +0800

    Added Crowdin configuration and changed source files to ``.pot`` format (#2165)
    * Create crowdin.yml
    
    * try fix yml
    
    * Move files
    
    * Revert "Move files"
    
    This reverts commit 8d53826ae0d8666fd4a74b88aa3ff90e9561d039.
    
    * Update crowdin.yml
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * Changed to locale with underscore
    
    * Changing folder for .pot files. And removing the useless sphinx-intl part.
    
    * Updated .gitignore and crowdin configuration to follow the directory change.
    
    * Renaming po to pot EFFECTIVELY
    
    * Changed the file with the new (simpler) generation. Also added better display for `make i18n`
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * Change to two letters code
    
    * Correct extension
    
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>
    Co-authored-by: Mysaa <samy.avrillon@ens-lyon.fr>
    Co-authored-by: Mysaa <mysaa@myssian.home>

--- docs/i18n/stripUntranslatable.sh
+++ docs/i18n/stripUntranslatable.sh
@@ -1,17 +1,14 @@
 #!/bin/bash
 rm -f translatable.po
 rm -f untranslatable.po
-rm -rf build-en
-mkdir build-en
-pot_dir_prefix="en/LC_MESSAGES/"
-for i in `find $pot_dir_prefix -name "*.po"`
+pot_dir_prefix="gettext/"
+echo ""
+for srcFile in `find $pot_dir_prefix -name "*.pot" -type f`
 do
-	srcFile=$i
-	destFile="build-$srcFile"
-	mkdir -p `dirname $destFile`
-	awk -f stripUntranslatable.awk $srcFile > $destFile
-	cat $destFile >> translatable.po
-	echo "OK: $srcFile"
+	printf "\r\033[KCleaning file \e[32m$srcFile\e[0m"
+	dstFile="$srcFile.bld"
+	awk -f stripUntranslatable.awk $srcFile | sed '/POT-Creation-Date/d'> $dstFile
+	cat $dstFile >> translatable.po
+	mv $dstFile $srcFile
 done
-rm -rf en
-mv build-en en
+echo ""
