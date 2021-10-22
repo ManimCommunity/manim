dd2e728bdb0a17f287a2f2765b882ff5c1fc22bd
christopher-besch <christopher.besch@gmx.de>
2021-10-22 12:25:49 +0200

    added link to docu

--- manim/scene/section.py
+++ manim/scene/section.py
@@ -31,6 +31,7 @@ class DefaultSectionType(str, Enum):
 
 class Section:
     """A :class:`.Scene` can be segmented into multiple Sections.
+    Refer to :doc:`the documentation</tutorials/a_deeper_look>` for more info.
     It consists of multiple animations.
 
     Attributes
