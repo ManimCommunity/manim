c2ed3b4a033ceebc96ebcc637f38938128806ce8
ryan <ryanmccauley211@gmail.com>
2021-10-31 15:24:03 +0000

    Add abstract renderer

---
+++ manim/renderer/renderer.py
@@ -0,0 +1,26 @@
+from abc import ABC, abstractmethod
+
+
+class Renderer(ABC):
+    def __init__(self):
+        pass
+
+    @abstractmethod
+    def init_scene(self):
+        pass
+
+    @abstractmethod
+    def before_render(self):
+        pass
+
+    @abstractmethod
+    def render(self):
+        pass
+
+    @abstractmethod
+    def after_render(self):
+        pass
+
+    @abstractmethod
+    def get_frame(self):
+        pass
