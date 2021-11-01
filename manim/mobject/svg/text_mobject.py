39598b19e34d8e9ecea2b916193e88b32a9ce297
hydrobeam <laithbahodi@gmail.com>
2021-11-01 11:54:40 -0400

    fix the docs

--- manim/mobject/svg/text_mobject.py
+++ manim/mobject/svg/text_mobject.py
@@ -352,7 +352,7 @@ class Text(SVGMobject):
                         'Google',
                         t2c={'[:1]': '#3174f0', '[1:2]': '#e53125',
                              '[2:3]': '#fbb003', '[3:4]': '#3174f0',
-                             '[4:5]': '#269a43', '[5:]': '#e53125'}, size=1.2).scale(3)
+                             '[4:5]': '#269a43', '[5:]': '#e53125'}, font_size=58).scale(3)
                     self.add(text1)
 
     As :class:`Text` uses Pango to render text, rendering non-English
