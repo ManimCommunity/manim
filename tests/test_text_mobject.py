acbe5244a3afc3fcb9fb46b0a16a05eb6c37bda6
citrusmunch <cnmne@protonmail.com>
2021-12-19 20:46:44 -0500

    add singleline and multline tests for Paragraph

--- tests/test_text_mobject.py
+++ tests/test_text_mobject.py
@@ -1,6 +1,6 @@
 import pytest
 
-from manim.mobject.svg.text_mobject import MarkupText, Text
+from manim.mobject.svg.text_mobject import MarkupText, Paragraph, Text
 
 
 def test_font_size():
@@ -11,3 +11,47 @@ def test_font_size():
 
     assert round(text_string.font_size, 5) == 14.4
     assert round(markuptext_string.font_size, 5) == 14.4
+
+
+def test_paragraph_singleline():
+    """Test a Paragraph with a single line."""
+    # need `__eq__` for `Text` to make some tests more useful
+    # going to hold off for the `Text` api rework
+    # e.g. `assert par[0] == Text("first")`
+
+    par = Paragraph("first")
+
+    # flatten
+    assert par.lines_text.text == "first"
+
+    # lines
+    assert par[0].text == "first"
+
+    # alignment
+    assert par.lines[1] == [None]
+
+    # shape
+    assert len(par) == 1
+    assert list(map(len, par)) == [4]  # ligature "fi"
+
+
+def test_paragraph_multiline():
+    """Test a Paragraph with multiple lines."""
+    # see note under `test_paragraph_singleline` about better tests
+
+    par = Paragraph("first", "second", "third")
+
+    # flatten
+    assert par.lines_text.text == "firstsecondthird"
+
+    # lines
+    assert par[0].text == "first"
+    assert par[1].text == "second"
+    assert par[2].text == "third"
+
+    # alignment
+    assert par.lines[1] == [None, None, None]
+
+    # shape
+    assert len(par) == 3
+    assert list(map(len, par)) == [4, 6, 5]  # ligature "fi"
