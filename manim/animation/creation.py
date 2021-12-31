c457e56278278ea1e4f926f52c650f4fe9f0e0ac
danielkwalsh <dan@crossnokaye.com>
2021-12-30 20:07:51 -0800

    Added docstring

--- manim/animation/creation.py
+++ manim/animation/creation.py
@@ -399,6 +399,26 @@ class Unwrite(Write):
 
 
 class SpiralIn(Animation):
+    """Create the Mobject with sub-Mobjects flying in on spiral trajectories.
+
+        Parameters
+        ----------
+        scale_factor
+            The factor used for scaling the effect.
+
+        Examples
+        --------
+        .. manim :: SpiralInExample
+
+        class SpiralInExample(Scene):
+            def construct(self):
+                pi = MathTex(r"\pi").scale(7)
+                pi.shift(2.25 * LEFT + 1.5 * UP)
+                circle = Circle(color=GREEN_C, fill_opacity=1).shift(LEFT)
+                square = Square(color=BLUE_D, fill_opacity=1).shift(UP)
+                shapes = VGroup(pi, circle, square)
+                self.play(SpiralIn(shapes))
+        """
     def __init__(
         self,
         shapes: Mobject,
