dee65cf011e470ce7c1add5a52fbfc1a0006382b
Marcin Serwin <marcin.serwin0@protonmail.com>
2022-01-03 19:35:27 +0100

    Update examples

--- docs/source/tutorials/using_text.rst
+++ docs/source/tutorials/using_text.rst
@@ -144,7 +144,7 @@ as explained in :ref:`iterating-text`.
 
     class Textt2cExample(Scene):
         def construct(self):
-            t2cindices = Text('Hello', t2c={'[1:-1]': BLUE}).move_to(LEFT)
+            t2cindices = Text('Hello', t2c={(1, -1): BLUE}).move_to(LEFT)
             t2cwords = Text('World',t2c={'rl':RED}).next_to(t2cindices, RIGHT)
             self.add(t2cindices, t2cwords)
 
--- manim/mobject/svg/text_mobject.py
+++ manim/mobject/svg/text_mobject.py
@@ -354,9 +354,9 @@ class Text(SVGMobject):
                 def construct(self):
                     text1 = Text(
                         'Google',
-                        t2c={'[:1]': '#3174f0', '[1:2]': '#e53125',
-                             '[2:3]': '#fbb003', '[3:4]': '#3174f0',
-                             '[4:5]': '#269a43', '[5:]': '#e53125'}, font_size=58).scale(3)
+                        t2c={(0, 1): '#3174f0', (1, 2): '#e53125',
+                             (2, 3): '#fbb003', (3, 4): '#3174f0',
+                             (4, 5): '#269a43', (5, None): '#e53125'}, font_size=58).scale(3)
                     self.add(text1)
 
     As :class:`Text` uses Pango to render text, rendering non-English
