0591c7833457930b399f4125958f81d038c96e69
Darylgolden <darylgolden@gmail.com>
2022-01-08 13:13:40 +0800

    Properly clip color

--- manim/utils/color.py
+++ manim/utils/color.py
@@ -547,6 +547,4 @@ def get_shaded_rgb(
     factor = 0.5 * np.dot(unit_normal_vect, to_sun) ** 3
     if factor < 0:
         factor *= 0.5
-    result = rgb + factor
-    clip_in_place(rgb + factor, 0, 1)
-    return result
+    return clip_in_place(rgb + factor, 0, 1)
