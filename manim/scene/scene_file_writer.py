44aea659f880688ebe0adc027ed83d5093921b5c
Kiran-Raj-Dev <90276965+Kiran-Raj-Dev@users.noreply.github.com>
2022-01-21 14:13:57 +0530

    remove version name from gifs

--- manim/scene/scene_file_writer.py
+++ manim/scene/scene_file_writer.py
@@ -606,7 +606,7 @@ class SceneFileWriter:
         # determine output path
         movie_file_path = self.movie_file_path
         if is_gif_format() and not config["output_file"]:
-            movie_file_path = str(add_version_before_extension(self.gif_file_path))
+            movie_file_path = self.gif_file_path
         logger.info("Combining to Movie file.")
         self.combine_files(
             partial_movie_files,
