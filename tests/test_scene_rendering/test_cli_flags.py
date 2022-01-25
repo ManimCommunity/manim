fd7d0b25a7bcc5d120b8cfd3ce7a895d457aeb07
Kiran-raj <santoshrajpurohit89@gmail.com>
2022-01-25 17:08:01 +0530

    Incorporated changes

--- tests/test_scene_rendering/test_cli_flags.py
+++ tests/test_scene_rendering/test_cli_flags.py
@@ -323,7 +323,7 @@ def test_gif_format_output(tmp_path, manim_cfg_file, simple_scenes_path):
     )
 
     expected_gif_path = (
-        tmp_path / "videos" / "simple_scenes" / "480p15" / "SquareToCircle.gif"
+        tmp_path / "videos" / "simple_scenes" / "480p15" / add_version_before_extension("SquareToCircle.gif")
     )
     assert expected_gif_path.exists(), "gif file not found at " + str(expected_gif_path)
 
