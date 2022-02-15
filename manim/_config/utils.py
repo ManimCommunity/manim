4a23fe170b15934df1e4078409c4b894fb92f37b
Kiran-raj <santoshrajpurohit89@gmail.com>
2022-02-15 11:01:05 +0530

    Changes to manim/_config/utils.py
    Fixed some type hints in manim/_config/utils.py
    Removed an unused import of set_file_logger
    Removed a redundant check in meth:digest_file
    Removed the double declaration of dry_run

--- manim/_config/utils.py
+++ manim/_config/utils.py
@@ -19,9 +19,9 @@ import logging
 import os
 import re
 import sys
-import typing
 from collections.abc import Mapping, MutableMapping
 from pathlib import Path
+from typing import Any, Iterable, Iterator
 
 import colour
 import numpy as np
@@ -29,7 +29,6 @@ import numpy as np
 from .. import constants
 from ..utils.tex import TexTemplate, TexTemplateFromFile
 from ..utils.tex_templates import TexTemplateLibrary
-from .logger_utils import set_file_logger
 
 
 def config_file_paths() -> list[Path]:
@@ -299,11 +298,11 @@ class ManimConfig(MutableMapping):
         "force_window",
     }
 
-    def __init__(self) -> dict[str, None]:
+    def __init__(self) -> None:
         self._d = {k: None for k in self._OPTS}
 
     # behave like a dict
-    def __iter__(self) -> typing.Iterator[str]:
+    def __iter__(self) -> Iterator[str]:
         return iter(self._d)
 
     def __len__(self) -> int:
@@ -319,7 +318,7 @@ class ManimConfig(MutableMapping):
     def __getitem__(self, key) -> Any:
         return getattr(self, key)
 
-    def __setitem__(self, key: str, val: typing.Any) -> None:
+    def __setitem__(self, key: str, val: Any) -> None:
         getattr(ManimConfig, key).fset(self, val)  # fset is the property's setter
 
     def update(self, obj: ManimConfig | dict) -> None:
@@ -394,7 +393,7 @@ class ManimConfig(MutableMapping):
         """See ManimConfig.copy()."""
         return copy.deepcopy(self)
 
-    def __deepcopy__(self, memo: dict[str, typing.Any]) -> ManimConfig:
+    def __deepcopy__(self, memo: dict[str, Any]) -> ManimConfig:
         """See ManimConfig.copy()."""
         c = ManimConfig()
         # Deepcopying the underlying dict is enough because all properties
@@ -404,14 +403,14 @@ class ManimConfig(MutableMapping):
         return c
 
     # helper type-checking methods
-    def _set_from_list(self, key: str, val: typing.Any, values: list) -> None:
+    def _set_from_list(self, key: str, val: Any, values: list) -> None:
         """Set ``key`` to ``val`` if ``val`` is contained in ``values``."""
         if val in values:
             self._d[key] = val
         else:
             raise ValueError(f"attempted to set {key} to {val}; must be in {values}")
 
-    def _set_boolean(self, key: str | int, val: typing.Any) -> None:
+    def _set_boolean(self, key: str | int, val: Any) -> None:
         """Set ``key`` to ``val`` if ``val`` is Boolean."""
         if val in [True, False]:
             self._d[key] = val
@@ -424,7 +423,7 @@ class ManimConfig(MutableMapping):
         else:
             raise ValueError(f"{key} must be tuple")
 
-    def _set_str(self, key: str, val: typing.Any) -> None:
+    def _set_str(self, key: str, val: Any) -> None:
         """Set ``key`` to ``val`` if ``val`` is a string."""
         if isinstance(val, str):
             self._d[key] = val
@@ -829,8 +828,7 @@ class ManimConfig(MutableMapping):
                 filename,
             )
 
-        if filename:
-            return self.digest_parser(make_config_parser(filename))
+        return self.digest_parser(make_config_parser(filename))
 
     # config options are properties
     preview = property(
@@ -913,12 +911,6 @@ class ManimConfig(MutableMapping):
         lambda self: self._d["force_window"],
         lambda self, val: self._set_boolean("force_window", val),
         doc="Set to force window when using the opengl renderer",
-    )
-
-    dry_run = property(
-        lambda self: self._d["dry_run"],
-        lambda self, val: self._set_boolean("dry_run", val),
-        doc="Enable dry_run so that no output files are generated and window is disabled.",
     )
 
     @property
@@ -1304,7 +1296,7 @@ class ManimConfig(MutableMapping):
         doc="PNG zero padding. A number between 0 (no zero padding) and 9 (9 columns minimum).",
     )
 
-    def get_dir(self, key: str, **kwargs: str) -> Path:
+    def get_dir(self, key: str, **kwargs: str) -> Path | None:
         """Resolve a config option that stores a directory.
 
         Config options that store directories may depend on one another.  This
@@ -1616,7 +1608,7 @@ class ManimFrame(Mapping):
         self.__dict__["_c"] = c
 
     # there are required by parent class Mapping to behave like a dict
-    def __getitem__(self, key: str | int) -> typing.Any:
+    def __getitem__(self, key: str | int) -> Any:
         if key in self._OPTS:
             return self._c[key]
         elif key in self._CONSTANTS:
@@ -1624,7 +1616,7 @@ class ManimFrame(Mapping):
         else:
             raise KeyError(key)
 
-    def __iter__(self) -> typing.Iterable:
+    def __iter__(self) -> Iterable:
         return iter(list(self._OPTS) + list(self._CONSTANTS))
 
     def __len__(self) -> int:
