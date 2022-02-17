eea4f229b75ad349a8a3d89856b45302c9985a89
Laith Bahodi <laithbahodi@gmail.com>
2022-02-17 12:24:10 -0500

    Set font for text_color_inheritance test

--- tests/test_graphical_units/test_text.py
+++ tests/test_graphical_units/test_text.py
@@ -14,20 +14,22 @@ __module_test__ = "text"
 def test_Text2Color(scene):
     scene.add(Text("this is  a text  with spaces!", t2c={"spaces": RED}))
 
-# generated control data does not match control data produced locally on arch linux.
-# possibly due to pango version mismatch?
+@pytest.mark.skipif(
+    not sys.platform.startswith("linux"),
+    reason="MacOS and Windows render fonts differently, so they need separate comparison data.",
+)
+@frames_comparison
+def test_text_color_inheritance(scene):
+    """Test that Text and MarkupText correctly inherit colour from
+    their parent class."""
 
-# @frames_comparison
-# def test_text_color_inheritance(scene):
-#     """Test that Text and MarkupText correctly inherit colour from
-#     their parent class."""
-#
-#     VMobject.set_default(color=RED)
-#     text = Text("test_color_inheritance")
-#     markup_text = MarkupText("test_color_inheritance")
-#     vgr = VGroup(text, markup_text).arrange()
-#
-#     # reset the default color so that future tests aren't affected by this change.
-#     VMobject.set_default()
-#
-#     scene.add(vgr)
+    VMobject.set_default(color=RED)
+    # set both to a singular font so that the tests agree.
+    text = Text("test_color_inheritance", font="Dejavu Sans")
+    markup_text = MarkupText("test_color_inheritance", font="Dejavu Sans")
+    vgr = VGroup(text, markup_text).arrange()
+
+    # reset the default color so that future tests aren't affected by this change.
+    VMobject.set_default()
+
+    scene.add(vgr)
