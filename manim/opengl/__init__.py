0588563e29d91be27c6848bfb6e3be3dd90543b7
Laith Bahodi <laithbahodi@gmail.com>
2022-02-23 10:29:57 -0500

    re-add imports to `manim/opengl/__init__.py`

--- manim/opengl/__init__.py
+++ manim/opengl/__init__.py
@@ -4,3 +4,13 @@ try:
     from dearpygui import dearpygui as dpg
 except ImportError:
     pass
+
+
+from manim.mobject.opengl.opengl_mobject import *
+from manim.mobject.opengl.opengl_three_dimensions import *
+from manim.mobject.opengl.opengl_vectorized_mobject import *
+from manim.mobject.opengl.opengl_surface import *
+from manim.mobject.opengl.opengl_point_cloud_mobject import *
+from manim.mobject.opengl.dot_cloud import *
+from ..renderer.shader import *
+from ..utils.opengl import *
