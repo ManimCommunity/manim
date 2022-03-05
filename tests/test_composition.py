6016834337dcf7c3490f435631fbde2563be54d0
Benjamin Hackl <devel@benjamin-hackl.at>
2022-03-05 22:40:57 +0100

    fixed some tests

--- tests/test_composition.py
+++ tests/test_composition.py
@@ -1,6 +1,6 @@
 from __future__ import annotations
 
-from unittest.mock import Mock
+from unittest.mock import MagicMock
 
 import pytest
 
@@ -22,9 +22,7 @@ def test_succession_timing():
     animation_4s = FadeOut(line, shift=DOWN, run_time=4.0)
     succession = Succession(animation_1s, animation_4s)
     assert succession.get_run_time() == 5.0
-    scene = Mock()
-    scene.mobjects = []
-    succession._setup_scene(scene)
+    succession._setup_scene(MagicMock())
     succession.begin()
     assert succession.active_index == 0
     # The first animation takes 20% of the total run time.
@@ -56,9 +54,7 @@ def test_succession_in_succession_timing():
     )
     assert nested_succession.get_run_time() == 5.0
     assert succession.get_run_time() == 10.0
-    scene = Mock()
-    scene.mobjects = []
-    succession._setup_scene(scene)
+    succession._setup_scene(MagicMock())
     succession.begin()
     succession.interpolate(0.1)
     assert succession.active_index == 0
