63ed75f834021b0d055633ca63f23788c460a421
Amanda Krohn <33667375+amandakrohn@users.noreply.github.com>
2022-03-07 14:16:33 +0100

    Makes color constants instances of Color-class (#9)
    * Converts values in setting_args dict in _get_settings_from_t2xs to string to make sure that all values are of type string.
    
    * Makes color constants instances of Color-class from colour-module and updates color_gradient to handle gradient with one color, fixes #7.

--- manim/utils/color.py
+++ manim/utils/color.py
@@ -29,87 +29,171 @@ from colour import Color
 from ..utils.bezier import interpolate
 from ..utils.space_ops import normalize
 
-WHITE: str = "#FFFFFF"
-GRAY_A: str = "#DDDDDD"
-GREY_A: str = "#DDDDDD"
-GRAY_B: str = "#BBBBBB"
-GREY_B: str = "#BBBBBB"
-GRAY_C: str = "#888888"
-GREY_C: str = "#888888"
-GRAY_D: str = "#444444"
-GREY_D: str = "#444444"
-GRAY_E: str = "#222222"
-GREY_E: str = "#222222"
-BLACK: str = "#000000"
-LIGHTER_GRAY: str = "#DDDDDD"
-LIGHTER_GREY: str = "#DDDDDD"
-LIGHT_GRAY: str = "#BBBBBB"
-LIGHT_GREY: str = "#BBBBBB"
-GRAY: str = "#888888"
-GREY: str = "#888888"
-DARK_GRAY: str = "#444444"
-DARK_GREY: str = "#444444"
-DARKER_GRAY: str = "#222222"
-DARKER_GREY: str = "#222222"
-BLUE_A: str = "#C7E9F1"
-BLUE_B: str = "#9CDCEB"
-BLUE_C: str = "#58C4DD"
-BLUE_D: str = "#29ABCA"
-BLUE_E: str = "#236B8E"
-PURE_BLUE: str = "#0000FF"
-BLUE: str = "#58C4DD"
-DARK_BLUE: str = "#236B8E"
-TEAL_A: str = "#ACEAD7"
-TEAL_B: str = "#76DDC0"
-TEAL_C: str = "#5CD0B3"
-TEAL_D: str = "#55C1A7"
-TEAL_E: str = "#49A88F"
-TEAL: str = "#5CD0B3"
-GREEN_A: str = "#C9E2AE"
-GREEN_B: str = "#A6CF8C"
-GREEN_C: str = "#83C167"
-GREEN_D: str = "#77B05D"
-GREEN_E: str = "#699C52"
-PURE_GREEN: str = "#00FF00"
-GREEN: str = "#83C167"
-YELLOW_A: str = "#FFF1B6"
-YELLOW_B: str = "#FFEA94"
-YELLOW_C: str = "#FFFF00"
-YELLOW_D: str = "#F4D345"
-YELLOW_E: str = "#E8C11C"
-YELLOW: str = "#FFFF00"
-GOLD_A: str = "#F7C797"
-GOLD_B: str = "#F9B775"
-GOLD_C: str = "#F0AC5F"
-GOLD_D: str = "#E1A158"
-GOLD_E: str = "#C78D46"
-GOLD: str = "#F0AC5F"
-RED_A: str = "#F7A1A3"
-RED_B: str = "#FF8080"
-RED_C: str = "#FC6255"
-RED_D: str = "#E65A4C"
-RED_E: str = "#CF5044"
-PURE_RED: str = "#FF0000"
-RED: str = "#FC6255"
-MAROON_A: str = "#ECABC1"
-MAROON_B: str = "#EC92AB"
-MAROON_C: str = "#C55F73"
-MAROON_D: str = "#A24D61"
-MAROON_E: str = "#94424F"
-MAROON: str = "#C55F73"
-PURPLE_A: str = "#CAA3E8"
-PURPLE_B: str = "#B189C6"
-PURPLE_C: str = "#9A72AC"
-PURPLE_D: str = "#715582"
-PURPLE_E: str = "#644172"
-PURPLE: str = "#9A72AC"
-PINK: str = "#D147BD"
-LIGHT_PINK: str = "#DC75CD"
-ORANGE: str = "#FF862F"
-LIGHT_BROWN: str = "#CD853F"
-DARK_BROWN: str = "#8B4513"
-GRAY_BROWN: str = "#736357"
-GREY_BROWN: str = "#736357"
+WHITE: Color = Color("#FFFFFF")
+GRAY_A: Color = Color("#DDDDDD")
+GREY_A: Color = Color("#DDDDDD")
+GRAY_B: Color = Color("#BBBBBB")
+GREY_B: Color = Color("#BBBBBB")
+GRAY_C: Color = Color("#888888")
+GREY_C: Color = Color("#888888")
+GRAY_D: Color = Color("#444444")
+GREY_D: Color = Color("#444444")
+GRAY_E: Color = Color("#222222")
+GREY_E: Color = Color("#222222")
+BLACK: Color = Color("#000000")
+LIGHTER_GRAY: Color = Color("#DDDDDD")
+LIGHTER_GREY: Color = Color("#DDDDDD")
+LIGHT_GRAY: Color = Color("#BBBBBB")
+LIGHT_GREY: Color = Color("#BBBBBB")
+GRAY: Color = Color("#888888")
+GREY: Color = Color("#888888")
+DARK_GRAY: Color = Color("#444444")
+DARK_GREY: Color = Color("#444444")
+DARKER_GRAY: Color = Color("#222222")
+DARKER_GREY: Color = Color("#222222")
+BLUE_A: Color = Color("#C7E9F1")
+BLUE_B: Color = Color("#9CDCEB")
+BLUE_C: Color = Color("#58C4DD")
+BLUE_D: Color = Color("#29ABCA")
+BLUE_E: Color = Color("#236B8E")
+PURE_BLUE: Color = Color("#0000FF")
+BLUE: Color = Color("#58C4DD")
+DARK_BLUE: Color = Color("#236B8E")
+TEAL_A: Color = Color("#ACEAD7")
+TEAL_B: Color = Color("#76DDC0")
+TEAL_C: Color = Color("#5CD0B3")
+TEAL_D: Color = Color("#55C1A7")
+TEAL_E: Color = Color("#49A88F")
+TEAL: Color = Color("#5CD0B3")
+GREEN_A: Color = Color("#C9E2AE")
+GREEN_B: Color = Color("#A6CF8C")
+GREEN_C: Color = Color("#83C167")
+GREEN_D: Color = Color("#77B05D")
+GREEN_E: Color = Color("#699C52")
+PURE_GREEN: Color = Color("#00FF00")
+GREEN: Color = Color("#83C167")
+YELLOW_A: Color = Color("#FFF1B6")
+YELLOW_B: Color = Color("#FFEA94")
+YELLOW_C: Color = Color("#FFFF00")
+YELLOW_D: Color = Color("#F4D345")
+YELLOW_E: Color = Color("#E8C11C")
+YELLOW: Color = Color("#FFFF00")
+GOLD_A: Color = Color("#F7C797")
+GOLD_B: Color = Color("#F9B775")
+GOLD_C: Color = Color("#F0AC5F")
+GOLD_D: Color = Color("#E1A158")
+GOLD_E: Color = Color("#C78D46")
+GOLD: Color = Color("#F0AC5F")
+RED_A: Color = Color("#F7A1A3")
+RED_B: Color = Color("#FF8080")
+RED_C: Color = Color("#FC6255")
+RED_D: Color = Color("#E65A4C")
+RED_E: Color = Color("#CF5044")
+PURE_RED: Color = Color("#FF0000")
+RED: Color = Color("#FC6255")
+MAROON_A: Color = Color("#ECABC1")
+MAROON_B: Color = Color("#EC92AB")
+MAROON_C: Color = Color("#C55F73")
+MAROON_D: Color = Color("#A24D61")
+MAROON_E: Color = Color("#94424F")
+MAROON: Color = Color("#C55F73")
+PURPLE_A: Color = Color("#CAA3E8")
+PURPLE_B: Color = Color("#B189C6")
+PURPLE_C: Color = Color("#9A72AC")
+PURPLE_D: Color = Color("#715582")
+PURPLE_E: Color = Color("#644172")
+PURPLE: Color = Color("#9A72AC")
+PINK: Color = Color("#D147BD")
+LIGHT_PINK: Color = Color("#DC75CD")
+ORANGE: Color = Color("#FF862F")
+LIGHT_BROWN: Color = Color("#CD853F")
+DARK_BROWN: Color = Color("#8B4513")
+GRAY_BROWN: Color = Color("#736357")
+GREY_BROWN: Color = Color("#736357")
+
+ALL_COLORS = {
+    "WHITE": WHITE,
+    "GRAY_A": GRAY_A,
+    "GREY_A": GREY_B,
+    "GRAY_B": GRAY_B,
+    "GREY_B": GREY_B,
+    "GRAY_C": GRAY_C,
+    "GREY_C": GREY_C,
+    "GRAY_D": GRAY_D,
+    "GREY_D": GREY_D,
+    "GRAY_E": GRAY_E,
+    "GREY_E": GREY_E,
+    "BLACK": BLACK,
+    "LIGHTER_GRAY": LIGHTER_GRAY,
+    "LIGHTER_GREY": LIGHTER_GREY,
+    "LIGHT_GRAY": LIGHT_GRAY,
+    "LIGHT_GREY": LIGHT_GREY,
+    "GRAY": GRAY,
+    "GREY": GREY,
+    "DARK_GRAY": DARK_GRAY,
+    "DARK_GREY": DARK_GREY,
+    "DARKER_GRAY": DARKER_GRAY,
+    "DARKER_GREY": DARKER_GREY,
+    "BLUE_A": BLUE_A,
+    "BLUE_B": BLUE_B,
+    "BLUE_C": BLUE_C,
+    "BLUE_D": BLUE_D,
+    "BLUE_E": BLUE_E,
+    "PURE_BLUE": PURE_BLUE,
+    "BLUE": BLUE,
+    "DARK_BLUE": DARK_BLUE,
+    "TEAL_A": TEAL_A,
+    "TEAL_B": TEAL_B,
+    "TEAL_C": TEAL_C,
+    "TEAL_D": TEAL_D,
+    "TEAL_E": TEAL_E,
+    "TEAL": TEAL,
+    "GREEN_A": GREEN_A,
+    "GREEN_B": GREEN_B,
+    "GREEN_C": GREEN_C,
+    "GREEN_D": GREEN_D,
+    "GREEN_E": GREEN_E,
+    "PURE_GREEN": PURE_GREEN,
+    "GREEN": GREEN,
+    "YELLOW_A": YELLOW_A,
+    "YELLOW_B": YELLOW_B,
+    "YELLOW_C": YELLOW_C,
+    "YELLOW_D": YELLOW_D,
+    "YELLOW_E": YELLOW_E,
+    "YELLOW": YELLOW,
+    "GOLD_A": GOLD_A,
+    "GOLD_B": GOLD_B,
+    "GOLD_C": GOLD_C,
+    "GOLD_D": GOLD_D,
+    "GOLD_E": GOLD_E,
+    "GOLD": GOLD,
+    "RED_A": RED_A,
+    "RED_B": RED_B,
+    "RED_C": RED_C,
+    "RED_D": RED_D,
+    "RED_E": RED_E,
+    "PURE_RED": PURE_RED,
+    "RED": RED,
+    "MAROON_A": MAROON_A,
+    "MAROON_B": MAROON_B,
+    "MAROON_C": MAROON_C,
+    "MAROON_D": MAROON_D,
+    "MAROON_E": MAROON_E,
+    "MAROON": MAROON,
+    "PURPLE_A": PURPLE_A,
+    "PURPLE_B": PURPLE_B,
+    "PURPLE_C": PURPLE_C,
+    "PURPLE_D": PURPLE_D,
+    "PURPLE_E": PURPLE_E,
+    "PURPLE": PURPLE,
+    "PINK": PINK,
+    "LIGHT_PINK": LIGHT_PINK,
+    "ORANGE": ORANGE,
+    "LIGHT_BROWN": LIGHT_BROWN,
+    "DARK_BROWN": DARK_BROWN,
+    "GRAY_BROWN": GRAY_BROWN,
+    "GREY_BROWN": GREY_BROWN,
+}
 
 __all__ += [
     "WHITE",
@@ -193,13 +277,9 @@ __all__ += [
     "DARK_BROWN",
     "GRAY_BROWN",
     "GREY_BROWN",
+    "ALL_COLORS",
 ]
 
-color_map = dict(((colorName, value) for colorName, value in locals().items() if str(value).startswith("#")))
-all_colors = [c for colors, c in locals().items() if str(c).startswith("#")]
-
-def get_all_colors():
-    return color_map
 
 def color_to_rgb(color: Color | str) -> np.ndarray:
     if isinstance(color, str):
@@ -248,11 +328,13 @@ def color_to_int_rgba(color: Color, opacity: float = 1.0) -> np.ndarray:
 
 
 def color_gradient(
-    reference_colors: Iterable[Color],
+    reference_colors: Color | Iterable[Color],
     length_of_output: int,
 ) -> list[Color]:
     if length_of_output == 0:
         return reference_colors[0]
+    if isinstance(reference_colors, Color):
+        return list(reference_colors.range_to(reference_colors, length_of_output))
     rgbs = list(map(color_to_rgb, reference_colors))
     alphas = np.linspace(0, (len(rgbs) - 1), length_of_output)
     floors = alphas.astype("int")
@@ -285,7 +367,8 @@ def random_bright_color() -> Color:
 
 
 def random_color() -> Color:
-    return random.choice(all_colors)
+    return random.choice(list(ALL_COLORS.values()))
+
 
 def get_shaded_rgb(
     rgb: np.ndarray,
