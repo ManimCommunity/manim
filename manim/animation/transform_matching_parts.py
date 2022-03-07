7a811ff2691a3a8496314f3ca2c156c51d569edc
Nicolai Weitkemper <kontakt@nicolaiweitkemper.de>
2022-03-07 14:47:11 +0100

    update example for `TransformMatchingTex` (2)

--- manim/animation/transform_matching_parts.py
+++ manim/animation/transform_matching_parts.py
@@ -247,12 +247,15 @@ class TransformMatchingTex(TransformMatchingAbstractBase):
             def construct(self):
                 variables = VGroup(MathTex("a"), MathTex("b"), MathTex("c")).arrange_submobjects().shift(UP)
 
-                eq1 = MathTex("{{x}}^2 + {{y}}^2 = {{z}}^2")
-                eq2 = MathTex("{{a}}^2 + {{b}}^2 = {{c}}^2")
+                eq1 = MathTex("{{x}}^2", "+", "{{y}}^2", "=", "{{z}}^2")
+                eq2 = MathTex("{{a}}^2", "+", "{{b}}^2", "=", "{{c}}^2")
+                eq3 = MathTex("{{a}}^2", "=", "{{c}}^2", "-", "{{b}}^2")
 
                 self.add(eq1)
                 self.wait(0.5)
                 self.play(TransformMatchingTex(Group(eq1, variables), eq2))
+                self.wait(0.5)
+                self.play(TransformMatchingTex(eq2, eq3))
                 self.wait(0.5)
 
     """
