087932e42db27bd8c33d69d3ff2b7a97ce7dc696
tameTNT <30503695+tametnt@users.noreply.github.com>
2022-04-01 12:41:38 +0100

    🎨 Reorder functions alphabetically
    Makes it easier to reference documentation alongside source code.

--- manim/utils/iterables.py
+++ manim/utils/iterables.py
@@ -18,10 +18,114 @@ __all__ = [
     "listify",
 ]
 
-
 import itertools as it
 
 import numpy as np
+
+
+def adjacent_n_tuples(objects, n):
+    return zip(*([*objects[k:], *objects[:k]] for k in range(n)))
+
+
+def adjacent_pairs(objects):
+    return adjacent_n_tuples(objects, 2)
+
+
+def all_elements_are_instances(iterable, Class):
+    return all([isinstance(e, Class) for e in iterable])
+
+
+def batch_by_property(items, property_func):
+    """
+    Takes in a list, and returns a list of tuples, (batch, prop)
+    such that all items in a batch have the same output when
+    put into property_func, and such that chaining all these
+    batches together would give the original list (i.e. order is
+    preserved)
+    """
+    batch_prop_pairs = []
+    curr_batch = []
+    curr_prop = None
+    for item in items:
+        prop = property_func(item)
+        if prop != curr_prop:
+            # Add current batch
+            if len(curr_batch) > 0:
+                batch_prop_pairs.append((curr_batch, curr_prop))
+            # Redefine curr
+            curr_prop = prop
+            curr_batch = [item]
+        else:
+            curr_batch.append(item)
+    if len(curr_batch) > 0:
+        batch_prop_pairs.append((curr_batch, curr_prop))
+    return batch_prop_pairs
+
+
+def concatenate_lists(*list_of_lists):
+    return [item for lst in list_of_lists for item in lst]
+
+
+def list_difference_update(l1, l2):
+    return [e for e in l1 if e not in l2]
+
+
+def list_update(l1, l2):
+    """
+    Used instead of list(set(l1).update(l2)) to maintain order,
+    making sure duplicates are removed from l1, not l2.
+    """
+    return [e for e in l1 if e not in l2] + list(l2)
+
+
+def listify(obj):
+    if isinstance(obj, str):
+        return [obj]
+    try:
+        return list(obj)
+    except TypeError:
+        return [obj]
+
+
+def make_even(iterable_1, iterable_2):
+    list_1, list_2 = list(iterable_1), list(iterable_2)
+    length = max(len(list_1), len(list_2))
+    return (
+        [list_1[(n * len(list_1)) // length] for n in range(length)],
+        [list_2[(n * len(list_2)) // length] for n in range(length)],
+    )
+
+
+def make_even_by_cycling(iterable_1, iterable_2):
+    length = max(len(iterable_1), len(iterable_2))
+    cycle1 = it.cycle(iterable_1)
+    cycle2 = it.cycle(iterable_2)
+    return (
+        [next(cycle1) for x in range(length)],
+        [next(cycle2) for x in range(length)],
+    )
+
+
+def remove_list_redundancies(lst):
+    """
+    Used instead of list(set(l)) to maintain order
+    Keeps the last occurrence of each element
+    """
+    reversed_result = []
+    used = set()
+    for x in reversed(lst):
+        if x not in used:
+            reversed_result.append(x)
+            used.add(x)
+    reversed_result.reverse()
+    return reversed_result
+
+
+def remove_nones(sequence):
+    return [x for x in sequence if x]
+
+
+# Note this is redundant with it.chain
 
 
 def resize_array(nparray, length):
@@ -52,90 +156,6 @@ def resize_with_interpolation(nparray, length):
     )
 
 
-def remove_list_redundancies(lst):
-    """
-    Used instead of list(set(l)) to maintain order
-    Keeps the last occurrence of each element
-    """
-    reversed_result = []
-    used = set()
-    for x in reversed(lst):
-        if x not in used:
-            reversed_result.append(x)
-            used.add(x)
-    reversed_result.reverse()
-    return reversed_result
-
-
-def list_update(l1, l2):
-    """
-    Used instead of list(set(l1).update(l2)) to maintain order,
-    making sure duplicates are removed from l1, not l2.
-    """
-    return [e for e in l1 if e not in l2] + list(l2)
-
-
-def list_difference_update(l1, l2):
-    return [e for e in l1 if e not in l2]
-
-
-def all_elements_are_instances(iterable, Class):
-    return all([isinstance(e, Class) for e in iterable])
-
-
-def adjacent_n_tuples(objects, n):
-    return zip(*([*objects[k:], *objects[:k]] for k in range(n)))
-
-
-def adjacent_pairs(objects):
-    return adjacent_n_tuples(objects, 2)
-
-
-def tuplify(obj):
-    if isinstance(obj, str):
-        return (obj,)
-    try:
-        return tuple(obj)
-    except TypeError:
-        return (obj,)
-
-
-def batch_by_property(items, property_func):
-    """
-    Takes in a list, and returns a list of tuples, (batch, prop)
-    such that all items in a batch have the same output when
-    put into property_func, and such that chaining all these
-    batches together would give the original list (i.e. order is
-    preserved)
-    """
-    batch_prop_pairs = []
-    curr_batch = []
-    curr_prop = None
-    for item in items:
-        prop = property_func(item)
-        if prop != curr_prop:
-            # Add current batch
-            if len(curr_batch) > 0:
-                batch_prop_pairs.append((curr_batch, curr_prop))
-            # Redefine curr
-            curr_prop = prop
-            curr_batch = [item]
-        else:
-            curr_batch.append(item)
-    if len(curr_batch) > 0:
-        batch_prop_pairs.append((curr_batch, curr_prop))
-    return batch_prop_pairs
-
-
-def listify(obj):
-    if isinstance(obj, str):
-        return [obj]
-    try:
-        return list(obj)
-    except TypeError:
-        return [obj]
-
-
 def stretch_array_to_length(nparray, length):
     curr_len = len(nparray)
     if curr_len > length:
@@ -145,34 +165,13 @@ def stretch_array_to_length(nparray, length):
     return nparray[indices.astype("int")]
 
 
-def make_even(iterable_1, iterable_2):
-    list_1, list_2 = list(iterable_1), list(iterable_2)
-    length = max(len(list_1), len(list_2))
-    return (
-        [list_1[(n * len(list_1)) // length] for n in range(length)],
-        [list_2[(n * len(list_2)) // length] for n in range(length)],
-    )
-
-
-def make_even_by_cycling(iterable_1, iterable_2):
-    length = max(len(iterable_1), len(iterable_2))
-    cycle1 = it.cycle(iterable_1)
-    cycle2 = it.cycle(iterable_2)
-    return (
-        [next(cycle1) for x in range(length)],
-        [next(cycle2) for x in range(length)],
-    )
-
-
-def remove_nones(sequence):
-    return [x for x in sequence if x]
-
-
-# Note this is redundant with it.chain
-
-
-def concatenate_lists(*list_of_lists):
-    return [item for lst in list_of_lists for item in lst]
+def tuplify(obj):
+    if isinstance(obj, str):
+        return (obj,)
+    try:
+        return tuple(obj)
+    except TypeError:
+        return (obj,)
 
 
 def uniq_chain(*args):
