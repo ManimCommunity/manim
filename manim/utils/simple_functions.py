987304aa55b26ebecd01638fa479d4e7c3d78089
tameTNT <30503695+tametnt@users.noreply.github.com>
2022-04-15 22:52:50 +0100

    📝 Add docstrings for functions in simple_functions.py

--- manim/utils/simple_functions.py
+++ manim/utils/simple_functions.py
@@ -20,20 +20,61 @@ from scipy import special
 
 
 def sigmoid(x: float) -> float:
+    """Returns the output of the logistic function (a very common sigmoid
+    function) defined as 1/(1+e^-x)
+
+    References
+    ----------
+    - https://en.wikipedia.org/wiki/Sigmoid_function
+    - https://en.wikipedia.org/wiki/Logistic_function
+    """
     return 1.0 / (1 + np.exp(-x))
 
 
 @lru_cache(maxsize=10)
 def choose(n: int, k: int) -> int:
+    """'n choose k' - the number of combinations of `n` things taken `k` at a time.
+
+    References
+    ----------
+    - https://en.wikipedia.org/wiki/Combination
+    - https://docs.scipy.org/doc/scipy/reference/generated/scipy.special.comb.html
+    """
     return special.comb(n, k, exact=True)
 
 
 def get_parameters(function: Callable) -> MappingProxyType[str, inspect.Parameter]:
+    """Return the parameters of `function` as an ordered mapping of parameters'
+    names to their corresponding `Parameter` objects.
+
+    Examples
+    --------
+    ::
+
+        >>> get_parameters(get_parameters)
+        mappingproxy(OrderedDict([('function', <Parameter "function: 'Callable'">)]))
+
+        >>> dict(get_parameters(choose))
+        {'n': <Parameter "n: 'int'">, 'k': <Parameter "k: 'int'">}
+    """
     return inspect.signature(function).parameters
 
 
 def clip(a, min_a, max_a):
-    """Any comparable objects (i.e. supports >,<)"""
+    """Accepts any comparable objects (i.e. those that support <, >).
+    Returns `a` if it is between `min_a` and `max_a`.
+    Otherwise, whichever of `min_a` and `max_a` is closest.
+
+    Examples
+    --------
+    Normal Usage::
+
+        >>>clip(15, 11, 20)
+        15
+
+        >>>clip('a', 'h', 'k')
+        'h'
+    """
     if a < min_a:
         return min_a
     elif a > max_a:
@@ -48,6 +89,24 @@ def binary_search(
     upper_bound: Union[int, float],
     tolerance: Union[int, float] = 1e-4,
 ) -> Union[int, float, None]:
+    """Performs a binary search to determine the input to `function`, between the bounds given,
+    that outputs `target` to within `tolerance` (default of 0.0001).
+    Returns None if no input can be found within the bounds.
+
+    Examples
+    --------
+    Normal Usage::
+
+        Observe that 0 <= 2 (solution) <= 5 and (2)^2 + 3(2) + 1 = 11 (target)
+        >>>binary_search(lambda x: x**2 + 3*x + 1, 11, 0, 5)
+        2.0000457763671875
+        >>>binary_search(lambda x: x**2 + 3*x + 1, 11, 0, 5, 0.01)
+        2.001953125
+
+        Here, observe that 7 (solution) > 5 (upper_bound) and (7)^2 + 3(7) + 1 = 71 (target)
+        >>>binary_search(lambda x: x**2 + 3*x + 1, 71, 0, 5)
+        None
+    """
 
     lh = lower_bound
     rh = upper_bound
