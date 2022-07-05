acd13a883cc46f47b71224ab4962c4245fbd7de6
Tristan Schulz <mrdiverlp@gmail.com>
2022-07-05 09:00:20 +0200

    Update manim/utils/bezier.py
    Co-authored-by: Benjamin Hackl <devel@benjamin-hackl.at>

--- manim/utils/bezier.py
+++ manim/utils/bezier.py
@@ -146,7 +146,10 @@ def split_quadratic_bezier(points: Iterable[float], t: float) -> np.ndarray:
 
 
 def subdivide_quadratic_bezier(points: Iterable[float], n: int) -> np.ndarray:
-    """Subdivide a quadratic bezier n times
+    """Subdivide a quadratic Bézier curve ``n`` times.
+    
+    The points at which the curve is split are located at the
+    arguments :math:`t = i/n` for :math:`i = 1, ..., n-1`. 
 
     Parameters
     ----------
