65038fce63d2979bd62c4bc96c53ee3a95cc0c7b
Benjamin Hackl <benjamin.hackl@aau.at>
2022-07-05 15:12:48 +0200

    reduced test run time and regenerated control data

--- tests/test_graphical_units/test_speed.py
+++ tests/test_graphical_units/test_speed.py
@@ -17,8 +17,8 @@ def test_SpeedModifier(scene):
     scene.play(
         ChangeSpeed(
             AnimationGroup(
-                a.animate(run_time=1, rate_func=linear).shift(RIGHT * 4),
-                b.animate(run_time=1, rate_func=rush_from).shift(RIGHT * 4),
+                a.animate(run_time=0.5, rate_func=linear).shift(RIGHT * 4),
+                b.animate(run_time=0.5, rate_func=rush_from).shift(RIGHT * 4),
             ),
             speedinfo={0.3: 1, 0.4: 0.1, 0.6: 0.1, 1: 1},
             affects_speed_updaters=False,
@@ -27,8 +27,8 @@ def test_SpeedModifier(scene):
     scene.play(
         ChangeSpeed(
             AnimationGroup(
-                a.animate(run_time=1, rate_func=linear).shift(LEFT * 4),
-                b.animate(run_time=1, rate_func=rush_into).shift(LEFT * 4),
+                a.animate(run_time=0.5, rate_func=linear).shift(LEFT * 4),
+                b.animate(run_time=0.5, rate_func=rush_into).shift(LEFT * 4),
             ),
             speedinfo={0.3: 1, 0.4: 0.1, 0.6: 0.1, 1: 1},
             rate_func=there_and_back,
