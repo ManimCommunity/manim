29bcfe79bde275478a33be24359d0c6423004315
Pedro Lamkowski <pedro.lamkowski@unesp.br>
2022-07-07 14:31:31 -0300

    Apply suggestions from code review
    Co-authored-by: Benjamin Hackl <devel@benjamin-hackl.at>

--- manim/mobject/graph.py
+++ manim/mobject/graph.py
@@ -392,21 +392,28 @@ class Graph(VMobject, metaclass=ConvertToOpenGL):
             def construct(self):
                 edges = []
                 partitions = []
-                c=0
-                layers = [2,3,3,2]         # the number of neurons on each layer
+                c = 0
+                layers = [2, 3, 3, 2]  # the number of neurons in each layer
 
                 for i in layers:
-                    partitions.append([j for j in range(c+1, c+i+1)])
+                    partitions.append(list(range(c + 1, c + i + 1)))
                     c += i
                 for i, v in enumerate(layers[1:]):
-                        last = int(np.sum(layers[:i+1]))
+                        last = sum(layers[:i+1])
                         for j in range(v):
-                            for k in range(last-layers[i], last):
-                                edges.append((k+1, j+last+1))
+                            for k in range(last - layers[i], last):
+                                edges.append((k + 1, j + last + 1))
 
-                vertices = np.arange(1, int(np.sum(layers))+1)
+                vertices = np.arange(1, sum(layers) + 1)
 
-                graph = Graph(vertices, edges, layout='partite', partitions=partitions, layout_scale=3, vertex_config={'radius':0.20})
+                graph = Graph(
+                    vertices,
+                    edges,
+                    layout='partite',
+                    partitions=partitions,
+                    layout_scale=3,
+                    vertex_config={'radius': 0.20},
+                )
                 self.add(graph)
 
     The custom tree layout can be used to show the graph
