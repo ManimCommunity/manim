9d1b29cda9cd7035c1e98de9c178d93dd3be1b98
Benjamin Hackl <devel@benjamin-hackl.at>
2022-11-09 17:43:05 +0100

    update added example in basic.py to reflect changed implementation

--- example_scenes/basic.py
+++ example_scenes/basic.py
@@ -164,11 +164,11 @@ class SpiralInExample(Scene):
 Triangle.set_default(stroke_width=20)
 
 
-class LineJoins(Scene):
+class LineJoints(Scene):
     def construct(self):
         t1 = Triangle()
-        t2 = Triangle(line_join=LINE_JOINS["ROUND"])
-        t3 = Triangle(line_join=LINE_JOINS["BEVEL"])
+        t2 = Triangle(line_join=LineJointType.ROUND)
+        t3 = Triangle(line_join=LineJointType.BEVEL)
 
         grp = VGroup(t1, t2, t3).arrange(RIGHT)
         grp.set(width=config.frame_width - 1)
