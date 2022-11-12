b8eebb45d2690c644a684c714ef3e1568421617f
Benjamin Hackl <devel@benjamin-hackl.at>
2022-11-12 21:38:41 +0100

    more documentation + doctests for mobject.utils

--- manim/mobject/utils.py
+++ manim/mobject/utils.py
@@ -4,6 +4,7 @@ from __future__ import annotations
 
 __all__ = [
     "get_mobject_class",
+    "get_point_mobject_class",
     "get_vectorized_mobject_class",
 ]
 
@@ -18,7 +19,25 @@ from .types.vectorized_mobject import VMobject
 
 
 def get_mobject_class() -> type:
-    """Returns the base mobject class, depending on the currently active renderer."""
+    """Gets the base mobject class, depending on the currently active renderer.
+    
+    .. NOTE::
+
+        This method is intended to be used in the code base of Manim itself
+        or in plugins where code should work independent of the selected
+        renderer.
+
+    Examples
+    --------
+
+    The function has to be explicitly imported. We test that
+    the name of the returned class is one of the known mobject
+    base classes::
+
+        >>> from manim.mobject.utils import get_mobject_class
+        >>> get_mobject_class().__name__ in ['Mobject', 'OpenGLMobject']
+        True
+    """
     if config.renderer == RendererType.CAIRO:
         return Mobject
     if config.renderer == RendererType.OPENGL:
@@ -29,6 +48,26 @@ def get_mobject_class() -> type:
 
 
 def get_vectorized_mobject_class() -> type:
+    """Gets the vectorized mobject class, depending on the currently
+    active renderer.
+
+    .. NOTE::
+
+        This method is intended to be used in the code base of Manim itself
+        or in plugins where code should work independent of the selected
+        renderer.
+
+    Examples
+    --------
+
+    The function has to be explicitly imported. We test that
+    the name of the returned class is one of the known mobject
+    base classes::
+
+        >>> from manim.mobject.utils import get_vectorized_mobject_class
+        >>> get_vectorized_mobject_class().__name__ in ['VMobject', 'OpenGLVMobject']
+        True
+    """
     if config.renderer == RendererType.CAIRO:
         return VMobject
     if config.renderer == RendererType.OPENGL:
@@ -39,6 +78,26 @@ def get_vectorized_mobject_class() -> type:
 
 
 def get_point_mobject_class() -> type:
+    """Gets the point cloud mobject class, depending on the currently
+    active renderer.
+
+    .. NOTE::
+
+        This method is intended to be used in the code base of Manim itself
+        or in plugins where code should work independent of the selected
+        renderer.
+
+    Examples
+    --------
+
+    The function has to be explicitly imported. We test that
+    the name of the returned class is one of the known mobject
+    base classes::
+
+        >>> from manim.mobject.utils import get_point_mobject_class
+        >>> get_point_mobject_class().__name__ in ['PMobject', 'OpenGLPMobject']
+        True
+    """
     if config.renderer == RendererType.CAIRO:
         return PMobject
     if config.renderer == RendererType.OPENGL:
