b54743e81d780931e7fb57c14676bbcee958c99f
Kevin Lubick <kjlubick@users.noreply.github.com>
2023-01-27 06:23:33 -0500

    Add scene.replace() and use in ReplacementTransform (#3042)
    * Add scene.replace() and use in ReplacementTransform
    
    This replace method allows one mobject to take the place (that is,
    the draw order) of another mobject.
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * Address Feedback
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * Update visual Replacement test and fix extract_frames.py
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * allow flake8 to be unhappy
    
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>
    Co-authored-by: Benjamin Hackl <devel@benjamin-hackl.at>

--- manim/scene/scene.py
+++ manim/scene/scene.py
@@ -519,6 +519,50 @@ class Scene:
                 self.restructure_mobjects(mobjects, list_name, False)
             return self
 
+    def replace(self, old_mobject: Mobject, new_mobject: Mobject) -> None:
+        """Replace one Mobject in the scene with another, preserving draw order.
+
+        If old_mobject is a submobject of some other Mobject (e.g. a Group),
+        the new_mobject will replace it inside the group, without otherwise
+        changing the parent mobject.
+
+        Parameters
+        ----------
+        old_mobject - A Mobject which must be in the scene. This method asserts if
+                      old_mobject is not in the scene
+        new_mobject - A Mobject which must not already be in the scene.
+
+        """
+        assert old_mobject is not None
+        assert new_mobject is not None
+
+        def replace_in_list(
+            mobj_list: list[Mobject], old_m: Mobject, new_m: Mobject
+        ) -> bool:
+            # We use breadth-first search because some Mobjects get very deep and
+            # we expect top-level elements to be the most common targets for replace.
+            for i in range(0, len(mobj_list)):
+                # Is this the old mobject?
+                if mobj_list[i] == old_m:
+                    # If so, write the new object to the same spot and stop looking.
+                    mobj_list[i] = new_m
+                    return True
+            # Now check all the children of all these mobs.
+            for mob in mobj_list:  # noqa: SIM110
+                if replace_in_list(mob.submobjects, old_m, new_m):
+                    # If we found it in a submobject, stop looking.
+                    return True
+            # If we did not find the mobject in the mobject list or any submobjects,
+            # (or the list was empty), indicate we did not make the replacement.
+            return False
+
+        # Make use of short-circuiting conditionals to check mobjects and then
+        # foreground_mobjects
+        replaced = replace_in_list(
+            self.mobjects, old_mobject, new_mobject
+        ) or replace_in_list(self.foreground_mobjects, old_mobject, new_mobject)
+        assert replaced, "Could not find old_mobject in Scene"
+
     def add_updater(self, func: Callable[[float], None]) -> None:
         """Add an update function to the scene.
 
