fc5f59ae0ea51d91a5a74edfcec5af342342e8c1
Benjamin Hackl <devel@benjamin-hackl.at>
2023-04-02 13:21:27 +0200

    black

--- manim/mobject/geometry/tips.py
+++ manim/mobject/geometry/tips.py
@@ -212,7 +212,7 @@ class StealthTip(ArrowTip):
     @property
     def length(self):
         """The length of the arrow tip.
-        
+
         In this case, the length is computed as the height of
         the triangle encompassing the stealth tip (otherwise,
         the tip is scaled too large).
