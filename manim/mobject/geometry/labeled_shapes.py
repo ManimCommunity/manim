7acdf0fa17405699447901ed38e58939aba218ff
Andres Berejnoi <andresberejnoi@gmail.com>
2023-07-27 14:57:45 -0400

    Update manim/mobject/geometry/labeled_shapes.py
    Co-authored-by: Jason Villanueva <a@jsonvillanueva.com>

--- manim/mobject/geometry/labeled_shapes.py
+++ manim/mobject/geometry/labeled_shapes.py
@@ -11,7 +11,9 @@ from manim.mobject.geometry.shape_matchers import (
     SurroundingRectangle,
 )
 from manim.utils.color import *
-
+from manim.mobject.text.tex_mobject import Tex, MathTex
+from manim.mobject.text.text_mobject import Text
+from manim.utils.color import Color
 
 class LabeledLine(Line):
     """Constructs a line containing a label box somewhere along its length.
