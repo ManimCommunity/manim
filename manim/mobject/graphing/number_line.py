4a796c58715f83dc246806d2a22f904ba3228c11
hari387 <narahari387@gmail.com>
2023-10-10 18:27:31 -0500

    forgot to subtract to create list of offsets

--- manim/mobject/graphing/number_line.py
+++ manim/mobject/graphing/number_line.py
@@ -277,7 +277,7 @@ class NumberLine(Line):
         via ``self.ticks``."""
         ticks = VGroup()
         elongated_tick_size = self.tick_size * self.longer_tick_multiple
-        elongated_tick_offsets = self.numbers_with_elongated_ticks
+        elongated_tick_offsets = self.numbers_with_elongated_ticks - self.x_min
         for x in self.get_tick_range():
             size = self.tick_size
             if np.any(np.isclose(x - self.x_min, elongated_tick_offsets)):
