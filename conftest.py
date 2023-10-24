ae7b0f4ffc58f6813695ebf95afa02a5717d98fd
Naveen M K <naveen521kk@gmail.com>
2023-10-24 13:52:49 +0530

    Display the cairo version when running pytest

--- conftest.py
+++ conftest.py
@@ -12,6 +12,7 @@ except ModuleNotFoundError:  # windows
     pass
 
 import moderngl
+import pycairo
 
 # If it is running Doctest the current directory
 # is changed because it also tests the config module
@@ -39,6 +40,7 @@ def pytest_report_header(config):
     info = ctx.info
     ctx.release()
     return (
+        f"\nCairo Version: {pycairo.cairo_version()}",
         "\nOpenGL information",
         "------------------",
         f"vendor: {info['GL_VENDOR'].strip()}",
