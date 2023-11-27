9b98fe5af25fae4688faa438d27e6e554653cc50
Francisco Manríquez Novoa <49853152+chopan050@users.noreply.github.com>
2023-11-27 01:10:52 +0100

    [EXPERIMENTAL] Mobject shader cleanup + Scene changes + Undo orientation input in vertex shader + Transparency Fix (#3474)
    * Updated Scene.remove
    
    * Updated Scene.replace
    
    * When shader file is missing, log its absolute path
    
    * Undo adding 'orientation' in vert.glsl, revert to 'unit_normal' in OpenGLVMobject
    
    * Added removed docstring to Scene.replace
    
    * Removed and rewrote multiple OpenGL(V)Mobject methods
    
    * Fixed OpenGLVMobject.get_style()
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * Fixing transparency and generator bug
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    ---------
    
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>
    Co-authored-by: MrDiver <mrdiverlp@gmail.com>

--- manim/scene/scene.py
+++ manim/scene/scene.py
@@ -29,6 +29,7 @@ from manim.utils.family_ops import (
     extract_mobject_family_members,
     recursive_mobject_remove,
 )
+from manim.utils.iterables import list_difference_update
 from manim.utils.module_ops import get_module
 
 if TYPE_CHECKING:
@@ -406,28 +407,15 @@ class Scene:
         For example, if the scene includes Group(m1, m2, m3), and we call scene.remove(m1),
         the desired behavior is for the scene to then include m2 and m3 (ungrouped).
         """
-        if config.renderer == RendererType.OPENGL:
-            mobjects_to_remove = []
-            meshes_to_remove = set()
-            for mobject_or_mesh in mobjects:
-                if isinstance(mobject_or_mesh, Object3D):
-                    meshes_to_remove.add(mobject_or_mesh)
-                else:
-                    mobjects_to_remove.append(mobject_or_mesh)
-            self.mobjects = restructure_list_to_exclude_certain_family_members(
-                self.mobjects,
-                mobjects_to_remove,
-            )
-            self.meshes = list(
-                filter(lambda mesh: mesh not in set(meshes_to_remove), self.meshes),
-            )
-            return self
-        elif config.renderer == RendererType.CAIRO:
-            for list_name in "mobjects", "foreground_mobjects":
-                self.restructure_mobjects(mobjects, list_name, False)
-            return self
+        for mob in mobjects_to_remove:
+            # First restructure self.mobjects so that parents/grandparents/etc. are replaced
+            # with their children, likewise for all ancestors in the extended family.
+            for ancestor in mob.get_ancestors(extended=True):
+                self.replace(ancestor, *ancestor.submobjects)
+            self.mobjects = list_difference_update(self.mobjects, mob.get_family())
+        return self
 
-    def replace(self, old_mobject: Mobject, new_mobject: Mobject) -> None:
+    def replace(self, mobject: Mobject, *replacements: Mobject):
         """Replace one mobject in the scene with another, preserving draw order.
 
         If ``old_mobject`` is a submobject of some other Mobject (e.g. a
@@ -442,37 +430,14 @@ class Scene:
             A mobject which must not already be in the scene.
 
         """
-        if old_mobject is None or new_mobject is None:
-            raise ValueError("Specified mobjects cannot be None")
-
-        def replace_in_list(
-            mobj_list: list[Mobject], old_m: Mobject, new_m: Mobject
-        ) -> bool:
-            # We use breadth-first search because some Mobjects get very deep and
-            # we expect top-level elements to be the most common targets for replace.
-            for i in range(0, len(mobj_list)):
-                # Is this the old mobject?
-                if mobj_list[i] == old_m:
-                    # If so, write the new object to the same spot and stop looking.
-                    mobj_list[i] = new_m
-                    return True
-            # Now check all the children of all these mobs.
-            for mob in mobj_list:  # noqa: SIM110
-                if replace_in_list(mob.submobjects, old_m, new_m):
-                    # If we found it in a submobject, stop looking.
-                    return True
-            # If we did not find the mobject in the mobject list or any submobjects,
-            # (or the list was empty), indicate we did not make the replacement.
-            return False
-
-        # Make use of short-circuiting conditionals to check mobjects and then
-        # foreground_mobjects
-        replaced = replace_in_list(
-            self.mobjects, old_mobject, new_mobject
-        ) or replace_in_list(self.foreground_mobjects, old_mobject, new_mobject)
-
-        if not replaced:
-            raise ValueError(f"Could not find {old_mobject} in scene")
+        if mobject in self.mobjects:
+            index = self.mobjects.index(mobject)
+            self.mobjects = [
+                *self.mobjects[:index],
+                *replacements,
+                *self.mobjects[index + 1 :],
+            ]
+        return self
 
     def add_updater(self, func: Callable[[float], None]) -> None:
         """Add an update function to the scene.
