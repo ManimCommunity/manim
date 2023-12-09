5fd208cbf92ee099d5ddbcbd0b5597007058e48f
MrDiver <mrdiverlp@gmail.com>
2023-12-09 23:45:51 +0100

    Added tests

--- tests/module/animation/test_composition.py
+++ tests/module/animation/test_composition.py
@@ -169,3 +169,11 @@ def test_animationgroup_is_passing_remover_to_nested_animationgroups():
     assert sqr_animation.remover
     assert circ_animation.remover
     assert polygon_animation.remover
+
+def test_empty_animation_group_fails():
+    with pytest.raises(ValueError):
+        AnimationGroup().begin()
+
+def test_empty_animation_fails():
+    with pytest.raises(ValueError):
+        FadeIn(None, run_time=0).begin()
