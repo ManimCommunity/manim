d4621e42a53ef854fb51ec72d8eaf41beb611f20
Tristan Schulz <mrdiverlp@gmail.com>
2023-12-10 18:10:16 +0100

    Update manim/animation/animation.py
    Co-authored-by: Jason Grace <110117391+JasonGrace2282@users.noreply.github.com>

--- manim/animation/animation.py
+++ manim/animation/animation.py
@@ -193,7 +193,7 @@ class Animation:
         """
         if self.run_time <= 0:
             raise ValueError(
-                f"{self} has a runtime of 0 seconds. Which cannot be rendered correctly! please set a runtime > 0"
+                f"{self} has a runtime of <= 0 seconds, which cannot be rendered correctly! please set a runtime > 0"
             )
         self.starting_mobject = self.create_starting_mobject()
         if self.suspend_mobject_updating:
