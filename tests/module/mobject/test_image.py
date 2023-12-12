2a2b90bf76c8b9cafd59ff881e5a30f9658fc291
Benjamin Hackl <devel@benjamin-hackl.at>
2023-12-12 13:25:08 +0100

    Update tests/module/mobject/test_image.py

--- tests/module/mobject/test_image.py
+++ tests/module/mobject/test_image.py
@@ -4,7 +4,7 @@ import pytest
 from manim import ImageMobject
 
 
-@pytest.mark.parametrize("dtype", (np.uint8, np.uint16))
+@pytest.mark.parametrize("dtype", [np.uint8, np.uint16])
 def test_invert_image(dtype):
     array = (255 * np.random.rand(10, 10, 4)).astype(dtype)
     image = ImageMobject(array, pixel_array_dtype=dtype, invert=True)
