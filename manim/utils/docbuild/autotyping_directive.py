1710faf1175db2bf00e8f6df2153e8586260cc9f
JasonGrace2282 <aarush.deshpande@gmail.com>
2023-12-22 22:38:58 -0500

    Got reST parser going

--- manim/utils/docbuild/autotyping_directive.py
+++ manim/utils/docbuild/autotyping_directive.py
@@ -1,12 +1,10 @@
 from __future__ import annotations
 
-import inspect
-
 from docutils import nodes
+from docutils.statemachine import ViewList
 from docutils.parsers.rst import Directive
 from sphinx.application import Sphinx
 
-from manim import ManimColor
 from manim.utils.docbuild.module_parsing import get_typing_docs
 
 __all__ = ["TypingModuleDocumenter"]
@@ -21,11 +19,9 @@ class TypingModuleDocumenter(Directive):
     required_arguments = 0
     has_content = True
 
-    def add_directive_header(self, sig: str) -> None:
-        super().add_directive_header(sig)
-
-    def run(self) -> List[nodes.Element]:
+    def run(self) -> list[nodes.Element]:
         content = nodes.container()
+        
 
         typing_docs_dict = get_typing_docs()
         for category_name, category_dict in typing_docs_dict.items():
@@ -37,12 +33,21 @@ class TypingModuleDocumenter(Directive):
             category_section += category_alias_container
 
             for alias_name, alias_dict in category_dict.items():
+                result = ViewList()
                 alias_section = nodes.topic(ids=[alias_name.lower().replace(" ", "_")])
                 category_alias_container += alias_section
                 alias_section += nodes.title(text=alias_name)
-                alias_section += nodes.paragraph(text=alias_dict["definition"])
+
+                doc = nodes.paragraph() # one paragraph for both
+                # add | to keep on different lines
+                # TODO: Figure out where to log sphinx errors (/tmp/sphinx-errs not availiable for non-linux)
+                result.append(f'| ``{alias_dict["definition"]}``', "/tmp/sphinx-errs.log", 10)
                 if "doc" in alias_dict:
-                    alias_section += nodes.paragraph(text=alias_dict["doc"])
+                    result.append("| %s" % alias_dict["doc"], "/tmp/sphinx-errs.log", 10)
+
+                # https://www.sphinx-doc.org/en/master/extdev/markupapi.html#parsing-directive-content-as-rest
+                self.state.nested_parse(result, 0, doc)
+                alias_section += doc
             content += category_section
 
         return [content]
