e0a35531fb64f65d67d7abf8b3ac713fc486e3c5
Viicos <65306057+Viicos@users.noreply.github.com>
2023-12-23 16:25:27 +0100

    fix affected tests

--- manim/plugins/__init__.py
+++ manim/plugins/__init__.py
@@ -1,8 +1,14 @@
 from __future__ import annotations
 
 from .plugins_flags import get_plugins, list_plugins
+from .. import config, logger
 
 __all__ = [
     "get_plugins",
     "list_plugins",
 ]
+
+requested_plugins: set[str] = set(config["plugins"])
+
+if not requested_plugins.issubset(get_plugins().keys()):
+    logger.warning("Missing Plugins: %s", requested_plugins)
