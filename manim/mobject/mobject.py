bfb694938d547e875f5584373658297f64e1d362
JasonGrace2282 <aarush.deshpande@gmail.com>
2024-02-01 16:27:34 -0500

    Add __class_getitem__ to Mobject

--- manim/mobject/mobject.py
+++ manim/mobject/mobject.py
@@ -2909,6 +2909,8 @@ class Mobject:
         self.set_z_index(z_coord)
         return self
 
+    def __class_getitem__(cls, item: type) -> str:
+        return f"{cls.__name__}[{item.__name__}]"
 
 class Group(Mobject, metaclass=ConvertToOpenGL):
     """Groups together multiple :class:`Mobjects <.Mobject>`.
