e31c2077cd15ed83f81974c2cc8729f093731da3
JasonGrace2282 <aarush.deshpande@gmail.com>
2024-04-10 11:44:25 -0400

    Attempt to fix windows test

--- manim/_config/__init__.py
+++ manim/_config/__init__.py
@@ -3,6 +3,7 @@
 from __future__ import annotations
 
 import logging
+import os
 from contextlib import contextmanager
 from typing import Any, Generator
 
@@ -19,6 +20,8 @@ __all__ = [
     "tempconfig",
     "cli_ctx_settings",
 ]
+
+os.environ["PYTHONIOENCODING"] = "utf-8"
 
 parser = make_config_parser()
 
