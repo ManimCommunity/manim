15abc8cc3d4c5139abb0b07a2fda122c9a0000ad
Benjamin Hackl <devel@benjamin-hackl.at>
2024-05-05 22:37:58 +0200

    added more run_time tests

---
+++ tests/module/animation/test_animation.py
@@ -0,0 +1,18 @@
+from __future__ import annotations
+
+import pytest
+
+from manim import FadeIn
+
+
+def test_empty_animation_fails():
+    with pytest.raises(ValueError, match="Please set the run_time to be positive"):
+        FadeIn(None, run_time=0).begin()
+
+def test_negative_run_time_fails():
+    with pytest.raises(ValueError, match="Please set the run_time to be positive"):
+        FadeIn(None, run_time=-1).begin()
+
+def test_run_time_shorter_than_frame_rate(caplog):
+    FadeIn(None, run_time=1e-9).begin()
+    assert "Original run time of FadeIn(Mobject) is shorter than current frame rate" in caplog.text
