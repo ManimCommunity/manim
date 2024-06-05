48013f4a30341f3933a11acfa68d898b7a654cf5
adeshpande <110117391+JasonGrace2282@users.noreply.github.com>
2024-06-05 07:21:49 -0400

    Nicer way of formatting
    Co-authored-by: Victorien <65306057+Viicos@users.noreply.github.com>

--- manim/mobject/graphing/scale.py
+++ manim/mobject/graphing/scale.py
@@ -182,7 +182,7 @@ class LogBase(_ScaleBase):
         tex_labels = [
             Integer(
                 self.base,
-                unit="^{%s}" % (f"{self.inverse_function(i):.{unit_decimal_places}f}"),  # noqa: UP031
+                unit="^{:.{prec}f}".format(self.inverse_function(i), prec=unit_decimal_places)
                 **base_config,
             )
             for i in val_range
