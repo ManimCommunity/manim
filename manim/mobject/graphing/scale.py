39f541ae72d756fb682174099c0ea774895a65a9
JasonGrace2282 <aarush.deshpande@gmail.com>
2024-06-05 07:47:26 -0400

    Revert "Nicer way of formatting"
    This reverts commit 48013f4a30341f3933a11acfa68d898b7a654cf5.

--- manim/mobject/graphing/scale.py
+++ manim/mobject/graphing/scale.py
@@ -182,10 +182,8 @@ class LogBase(_ScaleBase):
         tex_labels = [
             Integer(
                 self.base,
-                unit="^{:.{prec}f}".format(
-                    self.inverse_function(i), prec=unit_decimal_places
-                )
-                ** base_config,
+                unit="^{%s}" % (f"{self.inverse_function(i):.{unit_decimal_places}f}"),  # noqa: UP031
+                **base_config,
             )
             for i in val_range
         ]
