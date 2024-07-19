e8633fe1c8dd53c41482dfff3218f00664326b40
JasonGrace2282 <aarush.deshpande@gmail.com>
2024-07-18 23:13:36 -0400

    Changes to make pre-commit happy (Unrelated to PR)

--- manim/_config/utils.py
+++ manim/_config/utils.py
@@ -323,7 +323,7 @@ class ManimConfig(MutableMapping):
     }
 
     def __init__(self) -> None:
-        self._d: dict[str, Any | None] = {k: None for k in self._OPTS}
+        self._d: dict[str, Any | None] = dict.fromkeys(self._OPTS, None)
 
     # behave like a dict
     def __iter__(self) -> Iterator[str]:
