c48d4c27b7d44bc17a061068fa79465c8937d6de
JasonGrace2282 <aarush.deshpande@gmail.com>
2024-07-18 22:57:56 -0400

    Fixes for pre-commit

--- tests/test_scene_rendering/test_file_writer.py
+++ tests/test_scene_rendering/test_file_writer.py
@@ -81,7 +81,7 @@ def test_gif_writing(tmp_path, transparent):
 
 @pytest.mark.slow
 @pytest.mark.parametrize(
-    "format, transparent, codec, pixel_format",
+    ("format", "transparent", "codec", "pixel_format"),
     [
         ("mp4", False, "h264", "yuv420p"),
         ("mov", False, "h264", "yuv420p"),
