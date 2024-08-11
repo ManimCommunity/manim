c4378a8ad0bd7b3e94646153867bee268527e58e
christopher-hampson <christopher.hampson@kcl.ac.uk>
2024-08-11 12:19:10 +0100

    added test for preserving tex color

--- tests/test_graphical_units/test_tex_mobject.py
+++ tests/test_graphical_units/test_tex_mobject.py
@@ -24,3 +24,33 @@ def test_set_opacity_by_tex(scene):
     tex = MathTex("f(x) = y", substrings_to_isolate=["f(x)"])
     tex.set_opacity_by_tex("f(x)", 0.2, 0.5)
     scene.add(tex)
+
+
+def test_preserve_tex_color():
+    """Test that Tex preserves original tex colors."""
+    template = TexTemplate(preamble=r"\usepackage{xcolor}")
+    Tex.set_default(tex_template=template)
+
+    txt = Tex(r"\textcolor{red}{Hello} World")
+    assert len(txt[0].submobjects) == 10
+    assert all(char.fill_color.to_hex() == "#FF0000" for char in txt[0][:5])  # "Hello"
+    assert all(
+        char.fill_color.to_hex() == WHITE.to_hex() for char in txt[0][-5:]
+    )  # "World"
+
+    txt = Tex(r"\textcolor{red}{Hello} World", color=BLUE)
+    assert len(txt[0].submobjects) == 10
+    assert all(char.fill_color.to_hex() == "#FF0000" for char in txt[0][:5])  # "Hello"
+    assert all(
+        char.fill_color.to_hex() == BLUE.to_hex() for char in txt[0][-5:]
+    )  # "World"
+
+    Tex.set_default(color=GREEN)
+    txt = Tex(r"\textcolor{red}{Hello} World")
+    assert len(txt[0].submobjects) == 10
+    assert all(char.fill_color.to_hex() == "#FF0000" for char in txt[0][:5])  # "Hello"
+    assert all(
+        char.fill_color.to_hex() == GREEN.to_hex() for char in txt[0][-5:]
+    )  # "World"
+
+    Tex.set_default()
