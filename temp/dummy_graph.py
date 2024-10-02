810d7c7de6e0ad7e93c435f68360cc355cfb822f
ZynoXelek <enzo.brossier-secher@student.isae-supaero.fr>
2024-10-02 19:21:25 +0200

    Update temporary dummy graph scene

--- temp/dummy_graph.py
+++ temp/dummy_graph.py
@@ -9,15 +9,16 @@ class GraphScene(Scene):
     def construct(self):
         vertices = [1, 2, 3, 4, 5]
         edges = [(1, 2), (2, 3), (3, 5), (3, 1), (1, 5), (4, 4)]
-        layout = {
-            1: 2 * UP,
-            2: 2 * RIGHT,
-            3: 2 * DOWN,
-            4: 2 * LEFT + DOWN,
-            5: 2 * RIGHT + 2 * UP,
+        layout = "circular"
+        labels = True
+        weights = {
+            (1, 2): "4",
+            (2, 3): Tex("5", color=RED),
+            (3, 5): "3",
+            (3, 1): 0,
+            (1, 5): 3,
+            (4, 4): "2",
         }
-        labels = True  # {1: "1", 2: MathTex("2", stroke_color=BLACK, fill_color=RED), 3: "C", 4: Tex("D", color=BLACK), 5: "E"}
-        edge_config = {(4, 4): {"color": RED}}  # None # by default
 
         # Graph generation
 
@@ -25,14 +26,9 @@ class GraphScene(Scene):
             vertices=vertices,
             edges=edges,
             labels=labels,
+            weights=weights,
             layout=layout,
-            edge_config=edge_config,
-            label_fill_color=BLUE,
         )
-
-        # Inspect the generated labels
-        for vertex in g.vertices:
-            print(f"Vertex {vertex} has position {g.vertices[vertex].get_center()}")
 
         # Rendering the graphs
 
