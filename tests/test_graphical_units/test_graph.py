fbe3d7a9971d1d73a6c33b6d3a395670b6aed93f
ZynoXelek <enzo.brossier-secher@student.isae-supaero.fr>
2024-10-04 14:53:42 +0200

    Add graphical tests for self-loop edges and weighted edges for both DiGraph and Graph classes

---
+++ tests/test_graphical_units/test_graph.py
@@ -0,0 +1,81 @@
+from __future__ import annotations
+
+from manim import *
+from manim.utils.testing.frames_comparison import frames_comparison
+
+__module_test__ = "graph"
+
+
+@frames_comparison
+def test_GraphLoopEdge(scene):
+    vertices = [1, 2, 3, 4, 5]
+    edges = [(1, 2), (2, 3), (3, 1), (4, 5), (4, 1), (5, 1), (5, 2), (4, 4)]
+    labels = True
+    layout = "circular"
+    g = Graph(vertices, edges, labels=labels, layout=layout)
+    scene.add(g)
+
+
+@frames_comparison
+def test_DiGraphLoopEdge(scene):
+    vertices = [1, 2, 3, 4, 5]
+    edges = [(1, 2), (2, 4), (3, 2), (4, 5), (4, 1), (5, 1), (5, 3), (4, 4)]
+    labels = True
+    layout = "circular"
+    g = DiGraph(vertices, edges, labels=labels, layout=layout)
+    scene.add(g)
+
+
+@frames_comparison
+def test_WeightedGraph(scene):
+    vertices = [1, 2, 3, 4, 5]
+    edges = [(1, 3), (2, 3), (3, 4), (4, 2), (4, 5), (5, 1), (5, 3)]
+    labels = True
+    layout = "circular"
+    weights = {
+        (1, 3): "4",
+        (2, 3): Tex("5", color=RED),
+        (3, 4): 0,
+        (4, 2): "3",
+        (4, 5): 3,
+        (5, 1): "2",
+        (5, 3): "1",
+    }
+    g = Graph(
+        vertices,
+        edges,
+        labels=labels,
+        weights=weights,
+        layout=layout,
+    )
+    scene.add(g)
+
+
+@frames_comparison
+def test_WeightedDiGraph(scene):
+    vertices = [1, 2, 3, 4, 5]
+    edges = [(1, 4), (2, 3), (3, 4), (1, 3), (4, 2), (5, 4), (5, 1)]
+    labels = True
+    layout = "circular"
+    weights = {
+        (1, 4): "1",
+        (2, 3): -1,
+        (3, 4): MathTex("2"),
+        (1, 3): "2",
+        (4, 2): 4,
+        (5, 4): 1.5,
+        (5, 1): "7/2",
+    }
+    edge_config = {
+        (2, 3): {"label_background_color": WHITE, "label_text_color": BLACK},
+        (5, 1): {"label_text_color": YELLOW},
+    }
+    g = DiGraph(
+        vertices,
+        edges,
+        labels=labels,
+        weights=weights,
+        layout=layout,
+        edge_config=edge_config,
+    )
+    scene.add(g)
