e1ae4428c3000ee0bb40f327e51756654b82cd49
zeFresk <8807862+zeFresk@users.noreply.github.com>
2024-10-05 19:20:16 +0200

    Fix linting

--- tests/module/mobject/graphing/test_bar_chart.py
+++ tests/module/mobject/graphing/test_bar_chart.py
@@ -4,7 +4,7 @@ from manim.mobject.graphing.probability import BarChart
 def test_values_close_to_zero():
     """Checks that BarChart supports values/heights close to zero without crashing if ."""
     values = [1 / 10000 for _ in range(8)]
-    names = [i for i in range(len(values))]
+    names = list(range(len(values)))
     chart = BarChart(
         values=values,
         bar_names=["one", "two", "three", "four", "five"],
