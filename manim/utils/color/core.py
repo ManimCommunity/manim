9a6a86713399d80a446f227646fb951313660633
Sophia Wisdom <sophiawisdom1999@gmail.com>
2024-12-08 12:59:24 -0500

    add __hash__ to ManimColor

--- manim/utils/color/core.py
+++ manim/utils/color/core.py
@@ -1020,6 +1020,9 @@ class ManimColor:
         return self._construct_from_space(
             self._internal_from_integer(self.to_integer() ^ int(other), 1.0)
         )
+    
+    def __hash__(self) -> str:
+        return self.to_hex(with_alpha=True)
 
 
 RGBA = ManimColor
