9c3e4f575f114ae2d9e9574e34f4b0cf5acb78d4
JasonGrace2282 <aarush.deshpande@gmail.com>
2024-12-15 21:27:26 -0500

    PR review comments

--- manim/animation/composition.py
+++ manim/animation/composition.py
@@ -99,12 +99,11 @@ class AnimationGroup(Animation):
             anim._setup_scene(scene)
 
     def finish(self) -> None:
-        self.interpolate(1)
+        for anim in self.animations:
+            anim.finish()
         self.anims_begun[:] = True
         self.anims_finished[:] = True
         if self.suspend_mobject_updating:
-            for anim in self.animations:
-                anim.finish()
             self.group.resume_updating()
 
     def clean_up_from_scene(self, scene: Scene) -> None:
