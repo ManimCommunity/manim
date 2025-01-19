86a971940fd7ce8d956aff08f1f15d20e13ab08e
Sophia Wisdom <sophiawisdom1999@gmail.com>
2025-01-19 09:09:26 -0800

    Add __hash__ to ManimColor (#4051)
    * add __hash__ to ManimColor
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * Update core.py
    
    * add test
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * fix: correct typehint
    
    * fix test
    
    ---------
    
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>
    Co-authored-by: Benjamin Hackl <devel@benjamin-hackl.at>

--- manim/utils/color/core.py
+++ manim/utils/color/core.py
@@ -1030,6 +1030,9 @@ class ManimColor:
             self._internal_from_integer(self.to_integer() ^ int(other), 1.0)
         )
 
+    def __hash__(self) -> int:
+        return hash(self.to_hex(with_alpha=True))
+
 
 RGBA = ManimColor
 """RGBA Color Space"""
