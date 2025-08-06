c887b51d63acb40e2abaa5fd8f7110680db6d1f5
Henrik Skov Midtiby <hemi@mmmi.sdu.dk>
2025-08-06 22:38:59 +0200

    Add type annotations to `composition.py` (#4366)
    Co-authored-by: Francisco Manríquez Novoa <49853152+chopan050@users.noreply.github.com>

--- manim/animation/animation.py
+++ manim/animation/animation.py
@@ -129,7 +129,7 @@ class Animation:
 
     def __init__(
         self,
-        mobject: Mobject | None,
+        mobject: Mobject | OpenGLMobject | None,
         lag_ratio: float = DEFAULT_ANIMATION_LAG_RATIO,
         run_time: float = DEFAULT_ANIMATION_RUN_TIME,
         rate_func: Callable[[float], float] = smooth,
@@ -266,7 +266,7 @@ class Animation:
         # Keep track of where the mobject starts
         return self.mobject.copy()
 
-    def get_all_mobjects(self) -> Sequence[Mobject]:
+    def get_all_mobjects(self) -> Sequence[Mobject | OpenGLMobject]:
         """Get all mobjects involved in the animation.
 
         Ordering must match the ordering of arguments to interpolate_submobject
