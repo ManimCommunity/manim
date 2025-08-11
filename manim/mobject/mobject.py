0ec7b7bb67a6e1e357bf794e5dce75e6aaacc0a1
Henrik Skov Midtiby <hemi@mmmi.sdu.dk>
2025-08-11 08:17:36 +0200

    Fix bug introduced with the type annotations.

--- manim/mobject/mobject.py
+++ manim/mobject/mobject.py
@@ -2771,7 +2771,7 @@ class Mobject:
         def init_sizes(
             sizes: list | None, num: int, measures: list, name: str
         ) -> list[float]:
-            if sizes is None:
+            if len(sizes) == 0:
                 sizes = [None] * num
             if len(sizes) != num:
                 raise ValueError(f"{name} has a mismatching size.")
