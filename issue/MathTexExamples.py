73a08e4a8e2a0ff1b92d458df8e0a86868ff44d1
Henrik Skov Midtiby <hemi@mmmi.sdu.dk>
2025-12-16 11:03:16 +0100

    Added a lot of test cases

---
+++ issue/MathTexExamples.py
@@ -0,0 +1,159 @@
+from __future__ import annotations
+
+from manim import *
+
+
+class ExampleScene2(Scene):
+    def construct(self):
+        formula = MathTex(
+            r"P(X=k) = 0.5^k (1-0.5)^{12-k}",
+        ).scale(1.3)
+        print(formula.id_to_vgroup_dict.keys())
+        # formula.id_to_vgroup_dict['unique002'].set_color(RED)
+        # formula.set_color_by_tex("k", ORANGE)
+        self.add(formula)
+
+
+class ExampleScene3(Scene):
+    def construct(self):
+        formula = MathTex(
+            r"P(X=k) =",
+            r"\binom{12}{k}",
+            r"0.5^{k}",
+            r"(1-0.5)^{12-k}",
+            substrings_to_isolate=["k"],
+        ).scale(1.3)
+        for k in formula.id_to_vgroup_dict:
+            print(k)
+        for key in formula.id_to_vgroup_dict:
+            if key[-2:] == "ss":
+                formula.id_to_vgroup_dict[key].set_color(GREEN)
+
+        # formula.id_to_vgroup_dict['unique000ss'].set_color(RED)
+        # formula.id_to_vgroup_dict['unique001ss'].set_color(GREEN)
+        # formula.id_to_vgroup_dict['unique002ss'].set_color(BLUE)
+        # formula.id_to_vgroup_dict['unique003ss'].set_color(YELLOW)
+        # formula.set_color_by_tex("k", ORANGE)
+        self.add(formula)
+
+
+class ExampleScene4a(Scene):
+    def construct(self):
+        formula = MathTex(
+            r"a^2 + b^2 = c^2 + a^2",
+            substrings_to_isolate=["a", "b"],
+        ).scale(1.3)
+        for k in formula.id_to_vgroup_dict:
+            print(k)
+        for key in formula.id_to_vgroup_dict:
+            if key[-2:] == "ss":
+                formula.id_to_vgroup_dict[key].set_color(GREEN)
+
+        self.add(formula)
+
+
+class ExampleScene4b(Scene):
+    def construct(self):
+        formula = MathTex(
+            r"a^2 + b^2 = c^2 + a^2",
+            substrings_to_isolate=["c", "a"],
+        ).scale(1.3)
+        print("Hejsa")
+        for k in formula.id_to_vgroup_dict:
+            print(k)
+        for key in formula.id_to_vgroup_dict:
+            if key[-2:] == "ss":
+                formula.id_to_vgroup_dict[key].set_color(GREEN)
+
+        self.add(formula)
+
+
+class ExampleScene5(Scene):
+    def construct(self):
+        formula = MathTex(
+            r"a^2 + b^2 = c^2 + d^2 - a^2",
+            substrings_to_isolate=["[acd]"],
+        ).scale(1.3)
+        for k in formula.id_to_vgroup_dict:
+            print(k)
+        for key in formula.id_to_vgroup_dict:
+            if key[-2:] == "ss":
+                formula.id_to_vgroup_dict[key].set_color(GREEN)
+
+        self.add(formula)
+
+
+# TODO:
+# When all scenes are rendered with a single command line call
+# uv run manim render MathTexExamples.py --write_all
+# ExampleScene6 fails with the following error
+# KeyError: 'unique001ss'
+# I think it is related to a caching issue, because the error vanishes
+# when the scene is rendered by itself.
+# uv run manim render MathTexExamples.py ExampleScene6
+class ExampleScene6(Scene):
+    def construct(self):
+        formula = MathTex(
+            r"a^2 + b^2 = c^2 + d^2 - a^2",
+            substrings_to_isolate=["[acd]"],
+        ).scale(1.3)
+
+        for k in formula.id_to_vgroup_dict:
+            print(k)
+
+        def set_color_by_tex(mathtex, tex, color):
+            print(mathtex.matched_strings_and_ids)
+            for match in mathtex.matched_strings_and_ids:
+                if match[0] == tex:
+                    mathtex.id_to_vgroup_dict[match[1]].set_color(color)
+
+        set_color_by_tex(formula, "c", ORANGE)
+        set_color_by_tex(formula, "a", RED)
+
+        self.add(formula)
+
+
+class ExampleScene7(Scene):
+    def construct(self):
+        formula = MathTex(
+            r"a^2 + b^2 = c^2 + d^2 - 2 a^2",
+            substrings_to_isolate=["[acd]"],
+        ).scale(1.3)
+
+        for k in formula.id_to_vgroup_dict:
+            print(k)
+
+        def set_color_by_tex(mathtex, tex, color):
+            print(mathtex.matched_strings_and_ids)
+            for match in mathtex.matched_strings_and_ids:
+                if match[0] == tex:
+                    mathtex.id_to_vgroup_dict[match[1]].set_color(color)
+
+        set_color_by_tex(formula, "c", GREEN)
+        set_color_by_tex(formula, "a", RED)
+
+        self.add(formula)
+
+
+class ExampleScene8(Scene):
+    def construct(self):
+        formula = MathTex(
+            r"P(X=k) =",
+            r"\binom{12}{k}",
+            r"0.5^{k}",
+            r"(1-0.5)^{12-k}",
+            substrings_to_isolate=["k", "1", "12", "0.5"],
+        ).scale(1.3)
+
+        def set_color_by_tex(
+            mathtex: MathTex, tex: str, color: ParsableManimColor
+        ) -> None:
+            for match in mathtex.matched_strings_and_ids:
+                if match[0] == tex:
+                    mathtex.id_to_vgroup_dict[match[1]].set_color(color)
+
+        set_color_by_tex(formula, "k", GREEN)
+        set_color_by_tex(formula, "12", RED)
+        set_color_by_tex(formula, "1", YELLOW)
+        set_color_by_tex(formula, "0.5", BLUE_D)
+        self.add(formula)
