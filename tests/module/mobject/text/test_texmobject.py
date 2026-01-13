e928a0cde42a92d4ac6b5841ffd8fee8ae49c74a
Henrik Skov Midtiby <hemi@mmmi.sdu.dk>
2026-01-13 09:12:40 +0100

    Adjust paths the generated artefacts in tests that rely on MathTex

--- tests/module/mobject/text/test_texmobject.py
+++ tests/module/mobject/text/test_texmobject.py
@@ -10,7 +10,7 @@ from manim import MathTex, SingleStringMathTex, Tex, TexTemplate, tempconfig
 
 def test_MathTex(config):
     MathTex("a^2 + b^2 = c^2")
-    assert Path(config.media_dir, "Tex", "e4be163a00cf424f.svg").exists()
+    assert Path(config.media_dir, "Tex", "05bb0a41ed575f00.svg").exists()
 
 
 def test_SingleStringMathTex(config):
@@ -29,7 +29,7 @@ def test_double_braces_testing(text_input, length_sub):
 
 def test_tex(config):
     Tex("The horse does not eat cucumber salad.")
-    assert Path(config.media_dir, "Tex", "c3945e23e546c95a.svg").exists()
+    assert Path(config.media_dir, "Tex", "5384b41741a246bd.svg").exists()
 
 
 def test_tex_temp_directory(tmpdir, monkeypatch):
@@ -42,12 +42,12 @@ def test_tex_temp_directory(tmpdir, monkeypatch):
     with tempconfig({"media_dir": "media"}):
         Tex("The horse does not eat cucumber salad.")
         assert Path("media", "Tex").exists()
-        assert Path("media", "Tex", "c3945e23e546c95a.svg").exists()
+        assert Path("media", "Tex", "5384b41741a246bd.svg").exists()
 
 
 def test_percent_char_rendering(config):
     Tex(r"\%")
-    assert Path(config.media_dir, "Tex", "4a583af4d19a3adf.tex").exists()
+    assert Path(config.media_dir, "Tex", "32509dd0ea993961.tex").exists()
 
 
 def test_tex_whitespace_arg():
@@ -218,11 +218,11 @@ def test_tex_garbage_collection(tmpdir, monkeypatch, config):
     Path(tmpdir, "media").mkdir()
     config.media_dir = "media"
 
-    tex_without_log = Tex("Hello World!")  # d771330b76d29ffb.tex
-    assert Path("media", "Tex", "d771330b76d29ffb.tex").exists()
-    assert not Path("media", "Tex", "d771330b76d29ffb.log").exists()
+    tex_without_log = Tex("Hello World!")  # 058a4e242c57db6d.tex
+    assert Path("media", "Tex", "058a4e242c57db6d.tex").exists()
+    assert not Path("media", "Tex", "058a4e242c57db6d.log").exists()
 
     config.no_latex_cleanup = True
 
-    tex_with_log = Tex("Hello World, again!")  # da27670a37b08799.tex
-    assert Path("media", "Tex", "da27670a37b08799.log").exists()
+    tex_with_log = Tex("Hello World, again!")  # 45b4e7819cc20cb1.tex
+    assert Path("media", "Tex", "45b4e7819cc20cb1.log").exists()
