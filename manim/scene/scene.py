6ba455da47606747383f69ce43616eeef0a98d55
arnaud-ma <arnaudma.code@gmail.com>
2026-01-17 18:09:27 +0100

    feat: 'seed' option for reproducible outputs

--- manim/scene/scene.py
+++ manim/scene/scene.py
@@ -219,9 +219,9 @@ class Scene:
         self.mobjects: list[Mobject] = []
         # TODO, remove need for foreground mobjects
         self.foreground_mobjects: list[Mobject] = []
-        if self.random_seed is not None:
-            random.seed(self.random_seed)
-            np.random.default_rng(self.random_seed)
+
+        random.seed(self.random_seed)
+        np.random.seed(self.random_seed)  # noqa: NPY002 (only way to set seed globally)
 
     @property
     def camera(self) -> Camera | OpenGLCamera:
@@ -1163,13 +1163,11 @@ class Scene:
             and threading.current_thread().name != "MainThread"
         ):
             # TODO: are these actually being used?
-            kwargs.update(
-                {
-                    "subcaption": subcaption,
-                    "subcaption_duration": subcaption_duration,
-                    "subcaption_offset": subcaption_offset,
-                }
-            )
+            kwargs.update({
+                "subcaption": subcaption,
+                "subcaption_duration": subcaption_duration,
+                "subcaption_offset": subcaption_offset,
+            })
             self.queue.put(SceneInteractRerun("play", **kwargs))
             return
 
