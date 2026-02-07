b68c9c6c95e5b7f5fd3ed6c43b6c993ce201d9ff
Miguel Ángel Rodríguez Vidal <unmoldequimica@gmail.com>
2026-02-07 03:48:45 +0100

    test: Added test to verify duplication of references in multiple groups when replacing

--- tests/module/animation/test_transform.py
+++ tests/module/animation/test_transform.py
@@ -1,6 +1,6 @@
 from __future__ import annotations
 
-from manim import Circle, Manager, ReplacementTransform, Scene, Square, VGroup
+from manim import Circle, Manager, ReplacementTransform, Scene, Square, Triangle, VGroup
 
 
 def test_no_duplicate_references():
@@ -27,3 +27,23 @@ def test_duplicate_references_in_group():
     submobs = vg.submobjects
     assert len(submobs) == 1
     assert submobs[0] is sq
+
+
+def test_duplicate_references_in_multiple_groups():
+    manager = Manager(Scene)
+    scene = manager.scene
+    c = Circle()
+    sq = Square()
+    tr = Triangle()
+    vg_1 = VGroup(c, sq)
+    vg_2 = VGroup(c, tr)
+    scene.add(vg_1, vg_2)
+
+    scene.play(ReplacementTransform(c, sq))
+    submobs_1 = vg_1.submobjects
+    submobs_2 = vg_2.submobjects
+    assert len(submobs_1) == 1
+    assert submobs_1[0] is sq
+    assert len(submobs_2) == 2
+    assert submobs_2[0] is sq
+    assert submobs_2[1] is tr
