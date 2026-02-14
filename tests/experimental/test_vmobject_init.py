33884470e466420de2bd45884bd04e69e9b410c6
Francisco Manríquez Novoa <francisco.manriquezn@usm.cl>
2026-02-14 15:59:38 -0300

    Fix various tests and reveal underlying issues by using get_fill_color() instead of fill_color and related

--- tests/experimental/test_vmobject_init.py
+++ tests/experimental/test_vmobject_init.py
@@ -4,11 +4,11 @@ from manim.mobject.opengl.opengl_vectorized_mobject import OpenGLVMobject as VMo
 
 def test_vmobject_init():
     vm = VMobject()
-    assert vm.fill_color == [col.WHITE]
-    assert vm.stroke_color == [col.WHITE]
+    assert vm.get_fill_color() == col.WHITE
+    assert vm.get_stroke_color() == col.WHITE
     vm = VMobject(color=col.RED)
-    assert vm.fill_color == [col.RED]
-    assert vm.stroke_color == [col.RED]
+    assert vm.get_fill_color() == col.RED
+    assert vm.get_stroke_color() == col.RED
     vm = VMobject(fill_color=col.GREEN, stroke_color=col.YELLOW)
-    assert vm.fill_color == [col.GREEN]
-    assert vm.stroke_color == [col.YELLOW]
+    assert vm.get_fill_color() == col.GREEN
+    assert vm.get_stroke_color() == col.YELLOW
