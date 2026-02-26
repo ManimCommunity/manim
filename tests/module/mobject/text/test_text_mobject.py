255a1c129b0691b0b520cc95a966993ab98400d8
hangeer <hangeerh@sina.com>
2026-02-26 16:37:33 -0600

    tests: add unit tests for Paragraph alignment (#2839)

--- tests/module/mobject/text/test_text_mobject.py
+++ tests/module/mobject/text/test_text_mobject.py
@@ -3,7 +3,8 @@ from __future__ import annotations
 from contextlib import redirect_stdout
 from io import StringIO
 
-from manim.mobject.text.text_mobject import MarkupText, Text
+from manim.constants import LEFT, RIGHT
+from manim.mobject.text.text_mobject import MarkupText, Paragraph, Text
 
 
 def test_font_size():
@@ -32,3 +33,31 @@ def test_font_warnings():
 
     # check random string (should be warning)
     assert warning_printed("Manim!" * 3, warn_missing_font=True)
+
+
+def test_paragraph_alignment():
+    def float_eq(a: float, b: float, delta: float = 1e-2):
+        return abs(a - b) <= delta
+
+    # check paragraph left alignment
+    par_left = Paragraph("This is", "a left-aligned", "paragraph", alignment="left")
+    assert float_eq(par_left[0][0].get_x(LEFT), par_left[1][0].get_x(LEFT))
+    assert float_eq(par_left[0][0].get_x(LEFT), par_left[2][0].get_x(LEFT))
+
+    # check paragraph right alignment
+    par_right = Paragraph("This is", "a right-aligned", "paragraph", alignment="right")
+    assert float_eq(par_right[0][-1].get_x(RIGHT), par_right[1][-1].get_x(RIGHT))
+    assert float_eq(par_right[0][-1].get_x(RIGHT), par_right[2][-1].get_x(RIGHT))
+
+    # check paragraph center alignment
+    par_center = Paragraph(
+        "This is", "a center-aligned", "paragraph", alignment="center"
+    )
+    assert float_eq(
+        (par_center[0][0].get_x(LEFT) + par_center[0][-1].get_x(RIGHT)) / 2,
+        (par_center[1][0].get_x(LEFT) + par_center[1][-1].get_x(RIGHT)) / 2,
+    )
+    assert float_eq(
+        (par_center[0][0].get_x(LEFT) + par_center[0][-1].get_x(RIGHT)) / 2,
+        (par_center[2][0].get_x(LEFT) + par_center[2][-1].get_x(RIGHT)) / 2,
+    )
