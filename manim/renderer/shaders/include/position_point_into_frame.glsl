3ca0c6ac611f42f1eb4e363478b4a88831a53904
Colin Rubow <colinwrubow@gmail.com>
2026-06-20 17:08:49 -0600

    proper transformations

--- manim/renderer/shaders/include/position_point_into_frame.glsl
+++ manim/renderer/shaders/include/position_point_into_frame.glsl
@@ -4,15 +4,6 @@
 #include "./camera_uniform_declarations.glsl"
 #include "./mobject_uniform_declarations.glsl"
 
-vec3 rotate_point_into_frame(vec3 point)
-{
-    if (bool(is_fixed_in_frame))
-    {
-        return point;
-    }
-    return camera_rotation * point;
-}
-
 vec3 position_point_into_frame(vec3 point)
 {
     if (bool(is_fixed_in_frame))
@@ -21,9 +12,8 @@ vec3 position_point_into_frame(vec3 point)
     }
     if (bool(is_fixed_orientation))
     {
-        vec3 new_center = rotate_point_into_frame(fixed_orientation_center);
-        return point + (new_center - fixed_orientation_center);
+	return point - camera_rotation * camera_center + camera_rotation * fixed_orientation_center - fixed_orientation_center;
     }
-    return rotate_point_into_frame(point - camera_center);
+    return camera_rotation*(point - camera_center);
 }
 #endif // POSITION_POINT_INTO_FRAME_GLSL
