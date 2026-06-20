c01a81addf721d97d9b93ee005c556a47ee4564c
Colin Rubow <colinwrubow@gmail.com>
2026-06-20 17:32:09 -0600

    whoops still need that function

--- manim/renderer/shaders/include/position_point_into_frame.glsl
+++ manim/renderer/shaders/include/position_point_into_frame.glsl
@@ -4,6 +4,15 @@
 #include "./camera_uniform_declarations.glsl"
 #include "./mobject_uniform_declarations.glsl"
 
+vec3 rotate_point_into_frame(vec3 point)
+{
+    if (bool(is_fixed_in_frame))
+    {
+        return point;
+    }
+    return camera_rotation * point;
+}
+
 vec3 position_point_into_frame(vec3 point)
 {
     if (bool(is_fixed_in_frame))
