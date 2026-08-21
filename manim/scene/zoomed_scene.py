d0597b3b810f38ed2616b5c29ce9148a058da4de
PlayfulMathematician <me@playfulmathematician.com>
2026-08-20 18:32:09 -0600

    Added deactivate zooming

--- manim/scene/zoomed_scene.py
+++ manim/scene/zoomed_scene.py
@@ -154,6 +154,13 @@ class ZoomedScene(MovingCameraScene):
             self.zoomed_camera.frame,
             self.zoomed_display,
         )
+        
+    def deactivate_zooming(self) -> None:
+        """Do documentation later"""
+        self.remove(
+            self.zoomed_camera.frame,
+            self.zoomed_display,
+        )
 
     def get_zoom_in_animation(self, run_time: float = 2, **kwargs: Any) -> ApplyMethod:
         """Returns the animation of camera zooming in.
