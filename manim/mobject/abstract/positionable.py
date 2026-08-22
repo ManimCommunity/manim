4da832e6dd312a54945b9ce74200f43b977dd943
GniLudio <50866361+GniLudio@users.noreply.github.com>
2026-08-22 10:34:56 +0200

    Fix scale parameters

--- manim/mobject/abstract/positionable.py
+++ manim/mobject/abstract/positionable.py
@@ -247,6 +247,7 @@ class Positionable:
         self,
         # TODO: Rename to `factor`
         scale_factor: float,
+        scale_stroke: bool = False,
         *,
         about_point: Point3DLike | None = None,
         about_edge: Vector3DLike | None = None,
