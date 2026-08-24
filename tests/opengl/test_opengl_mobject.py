e65bb7b99a31fc521e9ffa493ceaead8d7e5dcda
Benjamin Hackl <devel@benjamin-hackl.at>
2026-08-24 22:28:46 +0200

    test: cover OpenGLMobject.get_boundary_point

--- tests/opengl/test_opengl_mobject.py
+++ tests/opengl/test_opengl_mobject.py
@@ -65,6 +65,15 @@ def test_opengl_mobject_remove(using_opengl_renderer):
     assert obj.remove(OpenGLMobject()) is obj
 
 
+def test_opengl_mobject_get_boundary_point(using_opengl_renderer):
+    """Test that get_boundary_point returns the furthest point in a direction."""
+    obj = OpenGLMobject().set_points(
+        np.array([[-2.0, 0.0, 0.0], [1.0, 0.0, 0.0], [2.0, 0.0, 0.0]]),
+    )
+
+    np.testing.assert_array_equal(obj.get_boundary_point([1, 0, 0]), [2, 0, 0])
+
+
 def test_opengl_rotate_about_vertex_view(using_opengl_renderer):
     """Test that rotating about a vertex obtained from get_vertices() works correctly.
 
