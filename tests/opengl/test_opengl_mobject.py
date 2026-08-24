b79fb6a94a81d309dfabec2f3a75664c3075ec3e
GniLudio <50866361+GniLudio@users.noreply.github.com>
2026-08-24 23:31:02 +0200

    Fix OpenGLMobject.get_boundary_point (#4956)
    * Fix OpenGLMobject.get_boundary_point
    
    * test: cover OpenGLMobject.get_boundary_point
    
    ---------
    
    Co-authored-by: Benjamin Hackl <devel@benjamin-hackl.at>

--- tests/opengl/test_opengl_mobject.py
+++ tests/opengl/test_opengl_mobject.py
@@ -65,6 +65,15 @@ def test_opengl_mobject_remove(using_opengl_renderer):
     assert obj.remove(OpenGLMobject()) is obj
 
 
+def test_opengl_mobject_get_boundary_point(using_opengl_renderer):
+    """Test that get_boundary_point returns the furthest point in a direction."""
+    obj = OpenGLMobject().set_points(
+        np.array([[-2.0, 0.0, 0.0], [1.0, 0.0, 0.0], [2.0, 0.0, 0.0]]),
+    )
+
+    np.testing.assert_array_equal(obj.get_boundary_point([1, 0, 0]), [2, 0, 0])
+
+
 def test_opengl_mobject_stretch_to_fit_depth(using_opengl_renderer):
     """Test that stretch_to_fit_depth changes the depth dimension."""
     obj = OpenGLMobject().set_points(
